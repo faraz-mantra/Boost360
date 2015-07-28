adc3d81df46e182120db591a25773b48f99590a8
vinaykg <vinay.k@nowfloats.com>
2015-07-28 17:21:48 +0530

    city auto complete

---
+++ Boost/app/src/main/java/com/nowfloats/signup/UI/Model/PlacesMainModel.java
@@ -0,0 +1,11 @@
+package com.nowfloats.signup.UI.Model;
+
+import java.util.ArrayList;
+
+/**
+ * Created by guru on 28/07/2015.
+ */
+public class PlacesMainModel {
+    public String description;
+    public ArrayList<PlacesModel>  terms;
+}
---
+++ Boost/app/src/main/java/com/nowfloats/signup/UI/Places/AutoCompleteAdapter.java
@@ -0,0 +1,78 @@
+package com.nowfloats.signup.UI.Places;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.nowfloats.signup.UI.Model.PlacesModel;
+import com.nowfloats.signup.UI.UI.PreSignUpActivity;
+import com.thinksity.R;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by guru on 28/07/2015.
+ */
+public class AutoCompleteAdapter extends ArrayAdapter<String> {
+
+    final String TAG = "AutocompleteCustomArrayAdapter.java";
+
+    Context mContext;
+    int layoutResourceId;
+    ArrayList<String> data = null;
+
+    public AutoCompleteAdapter(Context mContext, int layoutResourceId, ArrayList<String> data) {
+
+        super(mContext, layoutResourceId, data);
+
+        this.layoutResourceId = layoutResourceId;
+        this.mContext = mContext;
+        this.data = data;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        try{
+            /*
+             * The convertView argument is essentially a "ScrapView" as described is Lucas post
+             * http://lucasr.org/2012/04/05/performance-tips-for-androids-listview/
+             * It will have a non-null value when ListView is asking you recycle the row layout.
+             * So, when convertView is not null, you should simply update its contents instead of inflating a new row layout.
+             */
+            if(convertView==null){
+                // inflate the layout
+                LayoutInflater inflater = ((PreSignUpActivity) mContext).getLayoutInflater();
+                convertView = inflater.inflate(layoutResourceId, parent, false);
+            }
+
+//            String result = " ";
+//            try {
+//                // object item based on the position
+//                JSONObject jsonObject =new JSONObject(data.get(position));
+//                JSONArray objectItem = jsonObject.getJSONArray("terms");
+//                result = objectItem.getJSONObject(0).getString("value")+","+objectItem.getJSONObject(objectItem.length()-1).getString("value");
+//            }catch(Exception e){e.printStackTrace();}
+
+            // get the TextView and then set the text (item name) and tag (item ID) values
+            TextView textViewItem = (TextView) convertView.findViewById(R.id.autocomplete_info_tv);
+            textViewItem.setText(data.get(position));
+
+
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return convertView;
+
+    }
+}
