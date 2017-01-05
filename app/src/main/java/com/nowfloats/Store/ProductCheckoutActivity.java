8dfcd9663a127fc7e32179afce8a6b619f56ab2a
Romio <romio.ranjan@nowfloats.com>
2017-01-05 19:18:33 +0530

    Activity Added

---
+++ app/src/main/java/com/nowfloats/Store/ProductCheckoutActivity.java
@@ -0,0 +1,13 @@
+package com.nowfloats.Store;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProductCheckoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_checkout);
+    }
+}
