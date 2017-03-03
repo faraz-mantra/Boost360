1acccd268fe51a86e3dc79e1ea6066df9a4f0205
Abhishek Singh <abhishek.rajput@nowfloats.com>
2017-03-03 14:20:27 +0530

    before scroll logic

--- app/src/main/java/com/nowfloats/Analytics_Screen/Search_Query_Adapter/SubscribersAdapter.java
+++ app/src/main/java/com/nowfloats/Analytics_Screen/Search_Query_Adapter/SubscribersAdapter.java
@@ -2,12 +2,16 @@ package com.nowfloats.Analytics_Screen.Search_Query_Adapter;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.nowfloats.Analytics_Screen.model.SubscriberModel;
+import com.thinksity.R;
 
-import java.util.List;
+import java.util.ArrayList;
 
 /**
  * Created by Admin on 02-03-2017.
@@ -15,32 +19,41 @@ import java.util.List;
 
 public class SubscribersAdapter extends RecyclerView.Adapter<SubscribersAdapter.MyHolder> {
 
-    List<SubscriberModel> mSubscriberList;
+    ArrayList<SubscriberModel> mSubscriberList;
     Context mContext;
-    public SubscribersAdapter(Context context,List<SubscriberModel> subscriberModelList){
+    public SubscribersAdapter(Context context,ArrayList<SubscriberModel> subscriberModelList){
         mContext= context;
         mSubscriberList = subscriberModelList;
     }
     @Override
     public MyHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(mContext).inflate(R.layout.adapter_subscribers_item,parent,false);
+        return new MyHolder(view);
     }
 
     @Override
     public void onBindViewHolder(MyHolder holder, int position) {
+        if (holder == null){
+            return;
+        }
+        holder.mTextView.setText(mSubscriberList.get(position).getUserMobile());
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mSubscriberList.size();
     }
 
     class MyHolder extends RecyclerView.ViewHolder{
 
-
+        ImageView radioButton, arrowImage;
+        TextView mTextView;
         public MyHolder(View itemView) {
             super(itemView);
+            arrowImage = (ImageView) itemView.findViewById(R.id.arrowImage);
+            radioButton = (ImageView) itemView.findViewById(R.id.radioButton);
+            mTextView = (TextView) itemView.findViewById(R.id.subscriber_text);
         }
 
     }
--- app/src/main/java/com/nowfloats/Analytics_Screen/SubscribersActivity.java
+++ app/src/main/java/com/nowfloats/Analytics_Screen/SubscribersActivity.java
@@ -2,13 +2,17 @@ package com.nowfloats.Analytics_Screen;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.nowfloats.Analytics_Screen.API.SubscriberApis;
+import com.nowfloats.Analytics_Screen.Search_Query_Adapter.SubscribersAdapter;
 import com.nowfloats.Analytics_Screen.model.SubscriberModel;
 import com.nowfloats.Login.UserSessionManager;
 import com.nowfloats.util.BoostLog;
@@ -17,7 +21,7 @@ import com.nowfloats.util.Key_Preferences;
 import com.nowfloats.util.Methods;
 import com.thinksity.R;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit.Callback;
@@ -32,7 +36,9 @@ public class SubscribersActivity extends AppCompatActivity {
     private ProgressBar mProgressBar;
     private RecyclerView mRecyclerView;
     private Toolbar toolbar;
-    List<SubscriberModel> mSubscriberList = Collections.emptyList();
+    ArrayList<SubscriberModel> mSubscriberList = new ArrayList<>();
+    SubscribersAdapter mSubscriberAdapter;
+    private LinearLayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,9 +53,14 @@ public class SubscribersActivity extends AppCompatActivity {
         }
         TextView titleTextView = (TextView) toolbar.findViewById(R.id.titleTextView);
         titleTextView.setText(getResources().getString(R.string.subscribers));
-
         mProgressBar = (ProgressBar) findViewById(R.id.pb_subscriber);
         mRecyclerView = (RecyclerView) findViewById(R.id.lv_subscribers);
+
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mSubscriberAdapter = new SubscribersAdapter(this,mSubscriberList);
+        mRecyclerView.setAdapter(mSubscriberAdapter);
 
         mSessionManager = new UserSessionManager(getApplicationContext(), SubscribersActivity.this);
         if (mSessionManager.getFPDetails(Key_Preferences.GET_FP_DETAILS_TAG) != null) {
@@ -60,32 +71,44 @@ public class SubscribersActivity extends AppCompatActivity {
         mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
             @Override
             public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
-
+                int count = recyclerView.getChildCount();
+                int visiblePosition = mLayoutManager.findLastVisibleItemPosition();
+                /*if(visiblePosition>=count-2){
+                    mOffset+=10;
+                    getSubscribersList(String.valueOf(String.valueOf(mOffset)));
+                }*/
             }
         });
 
-
+        getSubscribersList(String.valueOf(mOffset));
         BoostLog.d("Test for Fp Tag: ", mSessionManager.getFPDetails(Key_Preferences.GET_FP_DETAILS_TAG));
 
     }
     private void getSubscribersList(String mOffset){
-
+        mProgressBar.setVisibility(View.VISIBLE);
         SubscriberApis mSubscriberApis = Constants.restAdapter.create(SubscriberApis.class);
         mSubscriberApis.getsubscribers(mSessionManager.getFpTag(), Constants.clientId, mOffset, new Callback<List<SubscriberModel>>() {
             @Override
             public void success(List<SubscriberModel> subscriberModels, Response response) {
+                mProgressBar.setVisibility(View.GONE);
                 if(subscriberModels == null){
                     return;
                 }
-                for (int i=0;i<subscriberModels.size();i++){
-                    mSubscriberList.add(subscriberModels.get(i));
+                int count = mSubscriberList.size();
 
+                for (int i=0;i<subscriberModels.size();i++){
+                    Log.v("ggg",subscriberModels.get(i).getUserMobile());
+                    mSubscriberList.add(subscriberModels.get(i));
+                    mSubscriberAdapter.notifyItemChanged(count+i);
                 }
+                //Log.v("ggg",count+"");
             }
 
             @Override
             public void failure(RetrofitError error) {
+                mProgressBar.setVisibility(View.GONE);
 
+                Methods.showSnackBarNegative(SubscribersActivity.this,getString(R.string.something_went_wrong));
             }
         });
     }
