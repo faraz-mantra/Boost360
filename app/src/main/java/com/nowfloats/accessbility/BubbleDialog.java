0c5577a646d565e38770ef89926a42a6b637a999
Vinay Yamusani <vinay.yamusani@nowfloats.com>
2017-05-24 15:11:32 +0530

    local-changes

--- app/src/main/java/com/nowfloats/accessbility/BubbleDialog.java
+++ app/src/main/java/com/nowfloats/accessbility/BubbleDialog.java
@@ -196,7 +196,7 @@ public class BubbleDialog extends AppCompatActivity {
         } else {
             super.onBackPressed();
         }
-        overridePendingTransition(R.anim.slide_in_up,R.anim.slide_out_down);
+        overridePendingTransition(R.anim.slide_in_up,R.anim.slide_out_up);
     }
 
     @Override
