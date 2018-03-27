c1a1c3dd56f44bd047f422f05d7919841d08d168
Abhishek Singh <abhishek.rajput@nowfloats.com>
2018-03-27 15:15:58 +0530

    keyboard ime option handle

--- boostkeyboard/src/main/java/nowfloats/nfkeyboard/util/KeyboardUtils.java
+++ boostkeyboard/src/main/java/nowfloats/nfkeyboard/util/KeyboardUtils.java
@@ -1,5 +1,7 @@
 package nowfloats.nfkeyboard.util;
 
+import android.view.inputmethod.EditorInfo;
+
 import nowfloats.nfkeyboard.R;
 
 /**
@@ -7,6 +9,7 @@ import nowfloats.nfkeyboard.R;
  */
 
 public class KeyboardUtils {
+    public static final int IME_ACTION_CUSTOM_LABEL = EditorInfo.IME_MASK_ACTION + 1;
     public enum KeyboardType{
         QWERTY_LETTERS, EMAIL_ADDRESS, NUMBERS, SYMBOLS, SYMBOLS_SHIFT, EMOJIS;
 
@@ -28,7 +31,23 @@ public class KeyboardUtils {
             }
         }
     }
-
+    public static int getImeOptionsActionIdFromEditorInfo(final EditorInfo editorInfo) {
+        if ((editorInfo.imeOptions & EditorInfo.IME_FLAG_NO_ENTER_ACTION) != 0) {
+            //IME_FLAG_NO_ENTER_ACTION:
+            // Flag of imeOptions: used in conjunction with one of the actions masked by IME_MASK_ACTION.
+            // If this flag is not set, IMEs will normally replace the "enter" key with the action supplied.
+            // This flag indicates that the action should not be available in-line as a replacement for the "enter" key.
+            // Typically this is because the action has such a significant impact or is not recoverable enough
+            // that accidentally hitting it should be avoided, such as sending a message.
+            // Note that TextView will automatically set this flag for you on multi-line text views.
+            return EditorInfo.IME_ACTION_NONE;
+        } else if (editorInfo.actionLabel != null) {
+            return IME_ACTION_CUSTOM_LABEL;
+        } else {
+            // Note: this is different from editorInfo.actionId, hence "ImeOptionsActionId"
+            return editorInfo.imeOptions & EditorInfo.IME_MASK_ACTION;
+        }
+    }
     public enum CandidateType{
         TEXT_LIST, BOOST_SHARE, NULL;
         public static int getXml(CandidateType type){
