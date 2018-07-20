9d25850e53452b022c5e22f5fe2a0a8d6d03b60f
Nicky <nicky.kumari@nowfloats.com>
2018-07-20 23:32:21 +0530

    hindi keyboard header text change on keyboard switch issue fixed

--- agahkeyboard/src/main/java/io.separ.neural.inputmethod/indic/LatinIME.java
+++ agahkeyboard/src/main/java/io.separ.neural.inputmethod/indic/LatinIME.java
@@ -762,6 +762,7 @@ public class LatinIME extends InputMethodService implements KeyboardActionListen
         DebugFlags.init(PreferenceManager.getDefaultSharedPreferences(this));
         RichInputMethodManager.init(this);
         mRichImm = RichInputMethodManager.getInstance();
+        PointerTracker.KEYBOARD_TYPED_KEY = null;
 
 
         InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
@@ -1074,13 +1075,18 @@ public class LatinIME extends InputMethodService implements KeyboardActionListen
         mResContext = setLocale(getApplicationContext(), locale);
         mHandler.onStartInput(editorInfo, restarting);
         CharSequence inputSequence = getImeCurrentInputConnection().getTextBeforeCursor(1, 0);
+
         if (inputSequence == null) {
             PointerTracker.KEYBOARD_TYPED_KEY = null;
         }
         if (inputSequence != null) {
             if (!inputSequence.toString().trim().isEmpty()) {
-                PointerTracker.KEYBOARD_TYPED_KEY = new Key(inputSequence.toString(), 0, 0, inputSequence.toString(),
-                        null, 0, 0, 0, 0, 0, 0, 0, 0, false);
+                if (Character.UnicodeBlock.of(inputSequence.charAt(0)) != Character.UnicodeBlock.DEVANAGARI) {
+                    PointerTracker.KEYBOARD_TYPED_KEY = null;
+                } else {
+                    PointerTracker.KEYBOARD_TYPED_KEY = new Key(inputSequence.toString(), 0, 0, inputSequence.toString(),
+                            null, 0, 0, 0, 0, 0, 0, 0, 0, false);
+                }
             } else {
                 PointerTracker.KEYBOARD_TYPED_KEY = null;
             }
