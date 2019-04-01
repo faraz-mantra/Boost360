218742e3d0c8dd2f1836d304a467d22b7920da80
CHIRANJIT BARDHAN <bardhan.jit@gmail.com>
2019-04-01 13:06:53 +0530

    minor changes

--- app/src/main/java/com/nowfloats/CustomPage/CustomPageFragment.java
+++ app/src/main/java/com/nowfloats/CustomPage/CustomPageFragment.java
@@ -131,16 +131,16 @@ public class CustomPageFragment extends Fragment {
         addProduct.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                if(session.getFPDetails(Key_Preferences.GET_FP_DETAILS_PAYMENTSTATE).equals("-1")) {
-//                    Methods.showFeatureNotAvailDialog(getContext());
-//                }else {
-//                    MixPanelController.track("AddCustomPage", null);
-//                    Intent intent = new Intent(activity, CreateCustomPageActivity.class);
-//                    startActivity(intent);
-//                    activity.overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
-//                }
+                if(session.getFPDetails(Key_Preferences.GET_FP_DETAILS_PAYMENTSTATE).equals("-1")) {
+                    Methods.showFeatureNotAvailDialog(getContext());
+                }else {
+                    MixPanelController.track("AddCustomPage", null);
+                    Intent intent = new Intent(activity, CreateCustomPageActivity.class);
+                    startActivity(intent);
+                    activity.overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                }
 
-                ActivePackage activePackage = Widget.getInstance().getActivePackage();
+                //ActivePackage activePackage = Widget.getInstance().getActivePackage();
             }
         });
     }
