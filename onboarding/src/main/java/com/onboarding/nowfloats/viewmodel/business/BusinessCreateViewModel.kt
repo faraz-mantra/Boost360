a8734bbcad1f00d4aab1e33035d5a6fdd82df3e9
Sarvare Alam <sarvare@cantech.co>
2020-04-11 22:36:52 +0530

    API suggest domain

--- onboarding/src/main/java/com/onboarding/nowfloats/viewmodel/business/BusinessCreateViewModel.kt
+++ onboarding/src/main/java/com/onboarding/nowfloats/viewmodel/business/BusinessCreateViewModel.kt
@@ -8,6 +8,7 @@ import com.onboarding.nowfloats.model.business.BusinessCreateRequest
 import com.onboarding.nowfloats.model.channel.request.UpdateChannelAccessTokenRequest
 import com.onboarding.nowfloats.model.channel.request.UpdateChannelActionDataRequest
 import com.onboarding.nowfloats.model.domain.BusinessDomainRequest
+import com.onboarding.nowfloats.model.domain.BusinessDomainSuggestRequest
 import com.onboarding.nowfloats.model.uploadfile.UploadFileBusinessRequest
 import com.onboarding.nowfloats.model.uploadfile.UploadFileProfileRequest
 import com.onboarding.nowfloats.rest.repositories.*
@@ -28,6 +29,10 @@ class BusinessCreateViewModel : BaseViewModel() {
 
     fun postCheckBusinessDomain(request: BusinessDomainRequest): LiveData<BaseResponse> {
         return BusinessDomainRepository.postCheckBusinessDomain(request).toLiveData()
+    }
+
+    fun postCheckBusinessDomainSuggest(request: BusinessDomainSuggestRequest): LiveData<BaseResponse> {
+        return BusinessDomainRepository.postCheckBusinessDomainSuggest(request).toLiveData()
     }
 
     fun putUploadImageBusiness(request: UploadFileBusinessRequest): LiveData<BaseResponse> {
