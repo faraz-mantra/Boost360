0b6a774116e913103ff66bc801f8eddd455fdeb5
Sarvare Alam <sarvare@cantech.co>
2020-04-13 12:53:29 +0530

    Ui bug and city place picker search screen

--- onboarding/src/main/java/com/onboarding/nowfloats/constant/PreferenceConstant.kt
+++ onboarding/src/main/java/com/onboarding/nowfloats/constant/PreferenceConstant.kt
@@ -3,5 +3,5 @@ package com.onboarding.nowfloats.constant
 object PreferenceConstant {
     const val NOW_FLOATS_PREFS = "nowfloatsPrefs"
     const val CLIENT_ID = "clientId"
-    const val USER_PROFILE_ID = "data"
+    const val USER_PROFILE_ID = "user_profile_id"
 }
