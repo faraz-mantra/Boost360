1a4423aaa83a1d700462a7d1b90c865c7293d903
Sarvare Alam <sarvare@cantech.co>
2020-04-16 23:59:40 +0530

    Api and home page bug fix

--- onboarding/src/main/java/com/onboarding/nowfloats/constant/PreferenceConstant.kt
+++ onboarding/src/main/java/com/onboarding/nowfloats/constant/PreferenceConstant.kt
@@ -1,10 +1,10 @@
 package com.onboarding.nowfloats.constant
 
 object PreferenceConstant {
-    const val NOW_FLOATS_PREFS = "nowfloatsPrefs"
-    const val CLIENT_ID = "clientId"
-    const val USER_PROFILE_ID = "user_profile_id"
-    const val AUTHORIZATION = "Authorization"
+  const val NOW_FLOATS_PREFS = "nowfloatsPrefs"
+  const val CLIENT_ID = "clientId"
+  const val USER_PROFILE_ID = "user_profile_id"
+  const val AUTHORIZATION = "Authorization"
   const val KEY_FP_ID = "fpid"
   const val KEY_sourceClientId = "source_clientid"
   const val GET_FP_EXPERIENCE_CODE = "GET_FP_EXPERIENCE_CODE"
