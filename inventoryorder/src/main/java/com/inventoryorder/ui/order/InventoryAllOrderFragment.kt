81f9de7b102375af7659e8d0a0cbf3997ed1e8b5
Sarvare Alam <sarvare@cantech.co>
2020-04-29 17:15:01 +0530

    fab icon bottom order screen

--- inventoryorder/src/main/java/com/inventoryorder/ui/order/InventoryAllOrderFragment.kt
+++ inventoryorder/src/main/java/com/inventoryorder/ui/order/InventoryAllOrderFragment.kt
@@ -1,6 +1,7 @@
 package com.inventoryorder.ui.order
 
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.inventoryorder.constant.FragmentType
 import com.inventoryorder.constant.RecyclerViewActionType
@@ -14,42 +15,55 @@ import com.inventoryorder.ui.startFragmentActivity
 
 class InventoryAllOrderFragment : BaseOrderFragment<FragmentInventoryAllOrderBinding>(), RecyclerItemClickListener {
 
-    companion object {
-        @JvmStatic
-        fun newInstance(bundle: Bundle? = null): InventoryAllOrderFragment {
-            val fragment = InventoryAllOrderFragment()
-            fragment.arguments = bundle
-            return fragment
-        }
-    }
+  var previousScrollY: Int = 0
 
-    override fun onCreateView() {
-        super.onCreateView()
-        setAdapterOrder()
+  companion object {
+    @JvmStatic
+    fun newInstance(bundle: Bundle? = null): InventoryAllOrderFragment {
+      val fragment = InventoryAllOrderFragment()
+      fragment.arguments = bundle
+      return fragment
     }
+  }
 
-    private fun setAdapterOrder() {
-        val typeList = OrderTypeModel().getOrderType()
-        val orderList = InventoryOrderModel().getOrderItem()
-        binding?.typeRecycler?.post {
-            val typeAdapter = AppBaseRecyclerViewAdapter(baseActivity, typeList, this)
-            binding?.typeRecycler?.layoutManager = LinearLayoutManager(baseActivity, LinearLayoutManager.HORIZONTAL, false)
-            binding?.typeRecycler?.adapter = typeAdapter
-            binding?.typeRecycler?.let { typeAdapter.runLayoutAnimation(it) }
-        }
-        binding?.orderRecycler?.post {
-            val orderAdapter = AppBaseRecyclerViewAdapter(baseActivity, orderList, this)
-            binding?.orderRecycler?.layoutManager = LinearLayoutManager(baseActivity)
-            binding?.orderRecycler?.adapter = orderAdapter
-            binding?.orderRecycler?.let { orderAdapter.runLayoutAnimation(it) }
-        }
-    }
+  override fun onCreateView() {
+    super.onCreateView()
+    setAdapterOrder()
+    scrollBehaviourSet()
+  }
 
-    override fun onItemClick(position: Int, item: BaseRecyclerViewItem?, actionType: Int) {
-        when (actionType) {
-            RecyclerViewActionType.ORDER_ITEM_CLICKED.ordinal -> {
-                startFragmentActivity(FragmentType.ORDER_DETAIL_VIEW, Bundle())
-            }
-        }
+  private fun setAdapterOrder() {
+    val typeList = OrderTypeModel().getOrderType()
+    val orderList = InventoryOrderModel().getOrderItem()
+    binding?.typeRecycler?.post {
+      val typeAdapter = AppBaseRecyclerViewAdapter(baseActivity, typeList, this)
+      binding?.typeRecycler?.layoutManager = LinearLayoutManager(baseActivity, LinearLayoutManager.HORIZONTAL, false)
+      binding?.typeRecycler?.adapter = typeAdapter
+      binding?.typeRecycler?.let { typeAdapter.runLayoutAnimation(it) }
     }
+    binding?.orderRecycler?.post {
+      val orderAdapter = AppBaseRecyclerViewAdapter(baseActivity, orderList, this)
+      binding?.orderRecycler?.layoutManager = LinearLayoutManager(baseActivity)
+      binding?.orderRecycler?.adapter = orderAdapter
+      binding?.orderRecycler?.let { orderAdapter.runLayoutAnimation(it) }
+    }
+  }
+
+  override fun onItemClick(position: Int, item: BaseRecyclerViewItem?, actionType: Int) {
+    when (actionType) {
+      RecyclerViewActionType.ORDER_ITEM_CLICKED.ordinal -> {
+        startFragmentActivity(FragmentType.ORDER_DETAIL_VIEW, Bundle())
+      }
+    }
+  }
+
+  private fun scrollBehaviourSet() {
+    binding?.nestedScroll?.viewTreeObserver?.addOnScrollChangedListener {
+      binding?.nestedScroll?.scrollY?.let {
+        if (it > previousScrollY && binding?.btnAdd?.visibility === View.VISIBLE) binding?.btnAdd?.hide()
+        else if (it < previousScrollY && binding?.btnAdd?.visibility !== View.VISIBLE) binding?.btnAdd?.show()
+        previousScrollY = it
+      }
+    }
+  }
 }
