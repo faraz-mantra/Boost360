9b4ed002bcb4352d99b324820c8db0a4d6800ad5
Jwala Kumar <jkkj004@gmail.com>
2020-04-30 19:53:05 +0530

    set data on InventoryOrderDetailsFragment

--- inventoryorder/src/main/java/com/inventoryorder/holders/OrderItemDetailsViewHolder.kt
+++ inventoryorder/src/main/java/com/inventoryorder/holders/OrderItemDetailsViewHolder.kt
@@ -1,15 +1,22 @@
 package com.inventoryorder.holders
 
+import android.graphics.Paint
+import com.framework.glide.GlideImageLoader
 import com.inventoryorder.databinding.ItemOrderDetailsBinding
 import com.inventoryorder.model.ordersdetails.ItemX
+import com.inventoryorder.model.ordersdetails.OrderItem
 import com.inventoryorder.recyclerView.AppBaseRecyclerViewHolder
 import com.inventoryorder.recyclerView.BaseRecyclerViewItem
 
 class OrderItemDetailsViewHolder(binding: ItemOrderDetailsBinding) : AppBaseRecyclerViewHolder<ItemOrderDetailsBinding>(binding) {
 
+    var order : OrderItem? = null
+
     override fun bind(position: Int, item: BaseRecyclerViewItem) {
         super.bind(position, item)
         val data = item as ItemX
+
+//        order = item as OrderItem
 
 /*//        var string = data.itemDiscountedPrice
         var spannableString = SpannableString(data.itemDiscountedPrice)
@@ -47,6 +54,27 @@ class OrderItemDetailsViewHolder(binding: ItemOrderDetailsBinding) : AppBaseRecy
 //            binding.tvDishQuantity.text = data.itemQuantity
 ////        binding.tvDishAmountDiscountedPrice.text = data.itemDiscountedPrice
 //        }
+
+
+        setDataResponseForOrderDetails(order)
+
+    }
+
+    private fun setDataResponseForOrderDetails(order: OrderItem?) {
+
+        binding?.tvDishName?.text = order?.Items?.get(0)?.Product?.Name
+        binding?.tvDishQuantity.text = order?.Items?.get(0)?.Quantity.toString()
+        binding?.tvDishAmount.text = order?.Items?.get(0)?.Product?.DiscountAmount.toString()
+        binding?.tvDishAmountDiscountedPrice.paintFlags = binding.tvDishAmountDiscountedPrice.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+        binding.tvDishAmountDiscountedPrice.text = order?.Items?.get(0)?.Product?.DiscountAmount.toString()
+
+
+        val url : String = order?.Items?.get(0)?.Product?.ImageUri.toString()
+
+//        binding.ivDishItem.setImageResource()
+
+
+
     }
 
 }
--- inventoryorder/src/main/java/com/inventoryorder/model/ordersdetails/CancellationDetails.kt
+++ inventoryorder/src/main/java/com/inventoryorder/model/ordersdetails/CancellationDetails.kt
@@ -2,7 +2,7 @@ package com.inventoryorder.model.ordersdetails
 
 import java.io.Serializable
 
-data class CancellationDetails(
+data class  CancellationDetails(
     val CancelledBy: String? = null,
     val CancelledOn: String? = null
 ) : Serializable
