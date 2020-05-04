561801b42d11711c6c000e2574f59d5d7833b57b
Jwala Kumar <jkkj004@gmail.com>
2020-05-04 18:46:09 +0530

    commit

--- inventoryorder/src/main/java/com/inventoryorder/model/bottomsheet/DeliveryOptionsModel.kt
+++ inventoryorder/src/main/java/com/inventoryorder/model/bottomsheet/DeliveryOptionsModel.kt
@@ -1,5 +1,6 @@
 package com.inventoryorder.model.bottomsheet
 
+import com.inventoryorder.R
 import com.inventoryorder.constant.RecyclerViewItemType
 import com.inventoryorder.recyclerView.AppBaseRecyclerViewItem
 
@@ -9,6 +10,19 @@ class DeliveryOptionsModel (val deliveryOptionSelectedIcon: Int? = null,
 
     override fun getViewType(): Int {
         return RecyclerViewItemType.ITEM_DELIVERY_OPTIONS.getLayout()
+    }
+
+    private fun getData() : ArrayList<DeliveryOptionsModel>{
+
+        val list = ArrayList<DeliveryOptionsModel>()
+        list.add(DeliveryOptionsModel(R.drawable.ic_option_unselected,"Option comes here"))
+        list.add(DeliveryOptionsModel(R.drawable.ic_option_unselected,"Option comes here"))
+        list.add(DeliveryOptionsModel(R.drawable.ic_option_unselected,"Subtitle 2"))
+        list.add(DeliveryOptionsModel(R.drawable.ic_option_unselected,"Option comes here"))
+        list.add(DeliveryOptionsModel(R.drawable.ic_option_unselected,"Option comes here"))
+
+        return list
+
     }
 
 }
