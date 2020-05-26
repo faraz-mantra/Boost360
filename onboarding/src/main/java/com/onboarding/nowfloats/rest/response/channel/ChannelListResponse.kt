e2d17752de8f97bb838fc743252e59703e180ec5
Sarvare Alam <sarvare@cantech.co>
2020-05-27 02:29:46 +0530

    content sharing setting update

--- onboarding/src/main/java/com/onboarding/nowfloats/rest/response/channel/ChannelListResponse.kt
+++ onboarding/src/main/java/com/onboarding/nowfloats/rest/response/channel/ChannelListResponse.kt
@@ -2,7 +2,8 @@ package com.onboarding.nowfloats.rest.response.channel
 
 import com.framework.base.BaseResponse
 import com.onboarding.nowfloats.model.channel.ChannelModel
+import java.io.Serializable
 
 data class ChannelListResponse(
     var data: ArrayList<ChannelModel> = ArrayList()
-) : BaseResponse()
+) : BaseResponse(), Serializable
--- onboarding/src/main/java/com/onboarding/nowfloats/viewmodel/business/BusinessCreateViewModel.kt
+++ onboarding/src/main/java/com/onboarding/nowfloats/viewmodel/business/BusinessCreateViewModel.kt
@@ -12,34 +12,46 @@ import com.onboarding.nowfloats.model.domain.BusinessDomainSuggestRequest
 import com.onboarding.nowfloats.model.uploadfile.UploadFileBusinessRequest
 import com.onboarding.nowfloats.model.uploadfile.UploadFileProfileRequest
 import com.onboarding.nowfloats.rest.repositories.*
+import org.json.JSONObject
 
 class BusinessCreateViewModel : BaseViewModel() {
 
-    fun putCreateBusinessOnboarding(profileId: String?, request: BusinessCreateRequest): LiveData<BaseResponse> {
-        return BusinessCreateRepository.putCreateBusinessOnboarding(profileId, request).toLiveData()
-    }
+  fun putCreateBusinessOnboarding(profileId: String?, request: BusinessCreateRequest): LiveData<BaseResponse> {
+    return BusinessCreateRepository.putCreateBusinessOnboarding(profileId, request).toLiveData()
+  }
 
-    fun updateChannelAccessToken(request: UpdateChannelAccessTokenRequest): LiveData<BaseResponse> {
-        return ChannelRepository.updateChannelAccessTokens(request).toLiveData()
-    }
+  fun updateChannelAccessToken(request: UpdateChannelAccessTokenRequest): LiveData<BaseResponse> {
+    return ChannelRepository.updateChannelAccessTokens(request).toLiveData()
+  }
 
-    fun postUpdateWhatsappRequest(request: UpdateChannelActionDataRequest, auth: String): LiveData<BaseResponse> {
-        return WhatsAppRepository.postUpdateWhatsappRequest(request, auth).toLiveData()
-    }
+  fun postUpdateWhatsappRequest(request: UpdateChannelActionDataRequest, auth: String): LiveData<BaseResponse> {
+    return WhatsAppRepository.postUpdateWhatsappRequest(request, auth).toLiveData()
+  }
 
-    fun postCheckBusinessDomain(request: BusinessDomainRequest): LiveData<BaseResponse> {
-        return BusinessDomainRepository.postCheckBusinessDomain(request).toLiveData()
-    }
+  fun getWhatsappBusiness(request: String?, auth: String): LiveData<BaseResponse> {
+    return WhatsAppRepository.getWhatsappBusiness(getJsonRequest(request), auth).toLiveData()
+  }
 
-    fun postCheckBusinessDomainSuggest(request: BusinessDomainSuggestRequest): LiveData<BaseResponse> {
-        return BusinessDomainRepository.postCheckBusinessDomainSuggest(request).toLiveData()
-    }
+  fun postCheckBusinessDomain(request: BusinessDomainRequest): LiveData<BaseResponse> {
+    return BusinessDomainRepository.postCheckBusinessDomain(request).toLiveData()
+  }
 
-    fun putUploadImageBusiness(request: UploadFileBusinessRequest): LiveData<BaseResponse> {
-        return UploadImageRepository.putUploadImageBusiness(request).toLiveData()
-    }
+  fun postCheckBusinessDomainSuggest(request: BusinessDomainSuggestRequest): LiveData<BaseResponse> {
+    return BusinessDomainRepository.postCheckBusinessDomainSuggest(request).toLiveData()
+  }
 
-    fun putUploadImageProfile(request: UploadFileProfileRequest): LiveData<BaseResponse> {
-        return UploadImageRepository.putUploadImageProfile(request).toLiveData()
-    }
+  fun putUploadImageBusiness(request: UploadFileBusinessRequest): LiveData<BaseResponse> {
+    return UploadImageRepository.putUploadImageBusiness(request).toLiveData()
+  }
+
+  fun putUploadImageProfile(request: UploadFileProfileRequest): LiveData<BaseResponse> {
+    return UploadImageRepository.putUploadImageProfile(request).toLiveData()
+  }
+
+}
+
+fun getJsonRequest(fpTag: String?): String {
+  val jsonObject = JSONObject()
+  jsonObject.put("WebsiteId", fpTag)
+  return jsonObject.toString()
 }
