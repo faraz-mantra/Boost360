afae3cc3ebd0533185ce3725c9229e041603eb52
Jwala Kumar <jkkj004@gmail.com>
2020-05-26 15:39:46 +0530

    email address field bug fixed

--- inventoryorder/src/main/java/com/inventoryorder/ui/createappointment/NewBookingFragmentTwo.kt
+++ inventoryorder/src/main/java/com/inventoryorder/ui/createappointment/NewBookingFragmentTwo.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import android.text.TextUtils
 import android.util.Patterns
 import android.view.View
-import android.widget.Toast
 import androidx.core.content.ContextCompat
-import androidx.core.view.get
-import androidx.recyclerview.widget.RecyclerView
 import com.framework.views.customViews.CustomTextView
 import com.inventoryorder.R
 import com.inventoryorder.constant.FragmentType
@@ -15,88 +12,83 @@ import com.inventoryorder.databinding.FragmentNewBookingTwoBinding
 import com.inventoryorder.model.bottomsheet.GenderSelectionModel
 import com.inventoryorder.ui.BaseInventoryFragment
 import com.inventoryorder.ui.startFragmentActivity
-import kotlinx.android.synthetic.main.bottom_sheet_select_gender.*
 import kotlinx.android.synthetic.main.fragment_new_booking_two.*
-import kotlinx.android.synthetic.main.item_bottom_sheet_select_gender.*
-import java.util.regex.Pattern
 
 class NewBookingFragmentTwo : BaseInventoryFragment<FragmentNewBookingTwoBinding>() {
 
-  private var selectGenderBottomSheetDialog: SelectGenderBottomSheetDialog? = null
-  private var selectGenderList = GenderSelectionModel().getData()
-  private var recyclerViewSetGender : RecyclerView? = null
+    private var selectGenderBottomSheetDialog: SelectGenderBottomSheetDialog? = null
+    private var selectGenderList = GenderSelectionModel().getData()
 
-  companion object {
-    fun newInstance(bundle: Bundle? = null): NewBookingFragmentTwo {
-      val fragment = NewBookingFragmentTwo()
-      fragment.arguments = bundle
-      return fragment
-    }
-  }
-
-  override fun onCreateView() {
-    super.onCreateView()
-    setOnClickListener(binding?.buttonCreateBooking, binding?.tvBack, binding?.buttonPayAtClinic, binding?.buttonPayOnline, binding?.llSelectGender)
-  }
-
-  override fun onClick(v: View) {
-    super.onClick(v)
-    when (v) {
-      binding?.tvBack -> baseActivity.onBackPressed()
-      binding?.buttonCreateBooking -> validateDetails(eTName.text.toString(),eTPhoneNumber.text.toString(),etEmail.text.toString(),tvSetGender.text.toString())
-      binding?.buttonPayAtClinic -> setBacUI(binding?.buttonPayAtClinic, binding?.buttonPayOnline, R.drawable.payment_bg_right)
-      binding?.buttonPayOnline -> setBacUI(binding?.buttonPayOnline, binding?.buttonPayAtClinic, R.drawable.payment_bg_left)
-      binding?.llSelectGender -> showBottomSheetDialogSelectGender()
+    companion object {
+        fun newInstance(bundle: Bundle? = null): NewBookingFragmentTwo {
+            val fragment = NewBookingFragmentTwo()
+            fragment.arguments = bundle
+            return fragment
+        }
     }
 
-  }
-
-  private fun setBacUI(btn1: CustomTextView?, btn2: CustomTextView?, paymentBgType: Int) {
-    btn1?.background = ContextCompat.getDrawable(baseActivity, R.color.colorAccent)
-    btn2?.background = ContextCompat.getDrawable(baseActivity, paymentBgType)
-    btn1?.setTextColor(ContextCompat.getColor(baseActivity, R.color.warm_grey_10))
-    btn2?.setTextColor(ContextCompat.getColor(baseActivity, R.color.primary_grey))
-  }
-
-  private fun showBottomSheetDialogSelectGender() {
-    selectGenderBottomSheetDialog = SelectGenderBottomSheetDialog()
-    selectGenderBottomSheetDialog?.onDoneClicked = { selectGenderFromList(it) }
-    selectGenderBottomSheetDialog?.setList(selectGenderList)
-    selectGenderBottomSheetDialog?.show(this.parentFragmentManager, SelectGenderBottomSheetDialog::class.java.name)
-  }
-
-  private fun selectGenderFromList(list: GenderSelectionModel?) {
-    selectGenderList.forEach { it.isSelected = (it.genderType == list?.genderType) }
-    binding?.tvSetGender?.text = list?.genderType
-  }
-
-  private fun validateDetails( customerName: String, customerPhoneNumber : String, customerEmail : String, selectGender : String  ){
-
-    var regExpName = "[a-z, A-Z]*"
-
-    if(TextUtils.isEmpty(customerName)){
-      showShortToast("Name field must not be empty.")
-      return
-    }
-    else if (!eTName.text.toString().matches(regExpName.toRegex())) {
-      showShortToast("Please enter valid name.")
-      return
-    }
-    if(customerPhoneNumber.length < 10){
-      showShortToast("Please enter valid mobile number")
-      return
-    }
-    if( TextUtils.isEmpty(customerEmail) || !Patterns.EMAIL_ADDRESS.matcher(customerEmail).matches()){
-      showShortToast("Please Enter valid email")
-      return
-    }
-    if(TextUtils.isEmpty(selectGender)){
-      showShortToast("Please select gender")
-      return
-    }
-    if (!TextUtils.isEmpty(customerName) && !TextUtils.isEmpty(customerPhoneNumber) && !TextUtils.isEmpty(customerEmail) && !TextUtils.isEmpty(selectGender)){
-      startFragmentActivity(FragmentType.BOOKING_SUCCESSFUL, Bundle())
+    override fun onCreateView() {
+        super.onCreateView()
+        setOnClickListener(binding?.buttonCreateBooking, binding?.tvBack, binding?.buttonPayAtClinic, binding?.buttonPayOnline, binding?.llSelectGender)
     }
 
-  }
+    override fun onClick(v: View) {
+        super.onClick(v)
+        when (v) {
+            binding?.tvBack -> baseActivity.onBackPressed()
+            binding?.buttonCreateBooking -> validateDetails(eTName.text.toString(), eTPhoneNumber.text.toString(), etEmail.text.toString(), tvSetGender.text.toString())
+            binding?.buttonPayAtClinic -> setBacUI(binding?.buttonPayAtClinic, binding?.buttonPayOnline, R.drawable.payment_bg_right)
+            binding?.buttonPayOnline -> setBacUI(binding?.buttonPayOnline, binding?.buttonPayAtClinic, R.drawable.payment_bg_left)
+            binding?.llSelectGender -> showBottomSheetDialogSelectGender()
+        }
+
+    }
+
+    private fun setBacUI(btn1: CustomTextView?, btn2: CustomTextView?, paymentBgType: Int) {
+        btn1?.background = ContextCompat.getDrawable(baseActivity, R.color.colorAccent)
+        btn2?.background = ContextCompat.getDrawable(baseActivity, paymentBgType)
+        btn1?.setTextColor(ContextCompat.getColor(baseActivity, R.color.warm_grey_10))
+        btn2?.setTextColor(ContextCompat.getColor(baseActivity, R.color.primary_grey))
+    }
+
+    private fun showBottomSheetDialogSelectGender() {
+        selectGenderBottomSheetDialog = SelectGenderBottomSheetDialog()
+        selectGenderBottomSheetDialog?.onDoneClicked = { selectGenderFromList(it) }
+        selectGenderBottomSheetDialog?.setList(selectGenderList)
+        selectGenderBottomSheetDialog?.show(this.parentFragmentManager, SelectGenderBottomSheetDialog::class.java.name)
+    }
+
+    private fun selectGenderFromList(list: GenderSelectionModel?) {
+        selectGenderList.forEach { it.isSelected = (it.genderType == list?.genderType) }
+        binding?.tvSetGender?.text = list?.genderType
+    }
+
+    private fun validateDetails(customerName: String, customerPhoneNumber: String, customerEmail: String, selectGender: String) {
+
+        var regExpName = "[a-z, A-Z]*"
+
+        if (TextUtils.isEmpty(customerName)) {
+            showShortToast("Name field must not be empty.")
+            return
+        } else if (!eTName.text.toString().matches(regExpName.toRegex())) {
+            showShortToast("Please enter valid name.")
+            return
+        }
+        if (customerPhoneNumber.length < 10) {
+            showShortToast("Please enter valid mobile number")
+            return
+        }
+        if (TextUtils.isEmpty(customerEmail) || !Patterns.EMAIL_ADDRESS.matcher(customerEmail).matches()) {
+            showShortToast("Please Enter valid email")
+            return
+        }
+        if (TextUtils.isEmpty(selectGender)) {
+            showShortToast("Please select gender")
+            return
+        }
+        if (!TextUtils.isEmpty(customerName) && !TextUtils.isEmpty(customerPhoneNumber) && !TextUtils.isEmpty(customerEmail) && !TextUtils.isEmpty(selectGender)) {
+            startFragmentActivity(FragmentType.BOOKING_SUCCESSFUL, Bundle())
+        }
+
+    }
 }
