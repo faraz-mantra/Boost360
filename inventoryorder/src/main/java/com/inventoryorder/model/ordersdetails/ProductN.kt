b910b42f44d86545174097c7f6d8a761746e8220
Sarvare Alam <sarvare@cantech.co>
2020-05-27 21:22:34 +0530

    video consultation list and detail page ui

--- inventoryorder/src/main/java/com/inventoryorder/model/ordersdetails/ProductN.kt
+++ inventoryorder/src/main/java/com/inventoryorder/model/ordersdetails/ProductN.kt
@@ -1,7 +1,5 @@
 package com.inventoryorder.model.ordersdetails
 
-import com.framework.utils.convertStringToObj
-import com.inventoryorder.model.consultation.ExtraItemProductConsultation
 import java.io.Serializable
 
 data class ProductN(
@@ -9,8 +7,7 @@ data class ProductN(
     val Description: String? = null,
     val Dimensions: DimensionsN? = null,
     val DiscountAmount: Double? = null,
-//    val ExtraProperties: ExtraPropertiesN? = null,
-    val ExtraProperties: Any? = null,
+    val ExtraProperties: ExtraPropertiesN? = null,
     val ImageUri: String? = null,
     val IsAvailable: Boolean? = null,
     val Name: String? = null,
@@ -20,7 +17,7 @@ data class ProductN(
     val _id: String? = null
 ) : Serializable {
 
-  fun extraItemProductConsultation(): ExtraItemProductConsultation? {
-    return ExtraProperties?.toString()?.let { convertStringToObj(it, ExtraItemProductConsultation::class.java) }
+  fun extraItemProductConsultation(): ExtraPropertiesN? {
+    return ExtraProperties
   }
 }
