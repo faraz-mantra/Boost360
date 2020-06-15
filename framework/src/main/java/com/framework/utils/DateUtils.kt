28f0169711221d3585cd16a5a90b4d65e7d90d49
Sarvare Alam <sarvare@cantech.co>
2020-06-15 17:09:27 +0530

    Order/ Appointment and consultation api and ui bug fix

--- framework/src/main/java/com/framework/utils/DateUtils.kt
+++ framework/src/main/java/com/framework/utils/DateUtils.kt
@@ -11,6 +11,7 @@ import java.util.concurrent.TimeUnit
 object DateUtils {
 
   const val FORMAT_SERVER_DATE = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+  const val FORMAT_SERVER_1_DATE = "yyyy-MM-dd'T'HH:mm:ss'Z'"
   const val FORMAT_SERVER_TO_LOCAL = "dd-MM-yyyy HH:mm a"
   const val FORMAT_SERVER_TO_LOCAL_2 = "EEE dd-MMM-yyyy HH:mm a"
   const val FORMAT_DD_MM_YYYY = "dd-MM-yyyy"
