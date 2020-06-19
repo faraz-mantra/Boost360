9034ae292432817c8cfb135aa4f2f624cb64ad55
Sarvare Alam <sarvare@cantech.co>
2020-06-19 19:26:43 +0530

    channel update bug

--- inventoryorder/src/main/java/com/inventoryorder/ui/order/OrdersFragment.kt
+++ inventoryorder/src/main/java/com/inventoryorder/ui/order/OrdersFragment.kt
@@ -78,7 +78,10 @@ class OrdersFragment : BaseInventoryFragment<FragmentOrdersBinding>(), RecyclerI
   override fun onClick(v: View) {
     super.onClick(v)
     when (v) {
-      binding?.btnAdd -> startFragmentActivity(FragmentType.CREATE_NEW_BOOKING, Bundle())
+      binding?.btnAdd -> {
+        showLongToast("Coming soon...")
+//        startFragmentActivity(FragmentType.CREATE_NEW_BOOKING, Bundle())
+      }
     }
   }
 
