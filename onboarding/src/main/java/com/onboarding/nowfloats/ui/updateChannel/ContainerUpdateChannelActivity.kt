6756f50116133268bfb131d73a4476f2c804c2e3
Sarvare Alam <sarvare@cantech.co>
2020-06-19 19:13:18 +0530

    digital channel update ui

---
+++ onboarding/src/main/java/com/onboarding/nowfloats/ui/updateChannel/ContainerUpdateChannelActivity.kt
@@ -0,0 +1,173 @@
+package com.onboarding.nowfloats.ui.updateChannel
+
+import android.app.Activity
+import android.content.Intent
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.framework.base.BaseFragment
+import com.framework.base.FRAGMENT_TYPE
+import com.framework.databinding.ActivityFragmentContainerBinding
+import com.framework.exceptions.IllegalFragmentTypeException
+import com.framework.models.BaseViewModel
+import com.framework.utils.ConversionUtils
+import com.framework.views.customViews.CustomToolbar
+import com.onboarding.nowfloats.R
+import com.onboarding.nowfloats.base.AppBaseActivity
+import com.onboarding.nowfloats.constant.FragmentType
+import com.onboarding.nowfloats.ui.updateChannel.digitalChannel.MyDigitalChannelFragment
+
+open class ContainerUpdateChannelActivity : AppBaseActivity<ActivityFragmentContainerBinding, BaseViewModel>() {
+
+  private var type: FragmentType? = null
+  private var myDigitalChannelFragment: MyDigitalChannelFragment? = null
+
+  override fun getLayout(): Int {
+    return com.framework.R.layout.activity_fragment_container
+  }
+
+  override fun getViewModelClass(): Class<BaseViewModel> {
+    return BaseViewModel::class.java
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    intent?.extras?.getString(FRAGMENT_TYPE)?.let { type = FragmentType.valueOf(it) }
+    super.onCreate(savedInstanceState)
+  }
+
+  override fun onCreateView() {
+    super.onCreateView()
+    setFragment()
+  }
+
+  override fun customTheme(): Int? {
+    return when (type) {
+      else -> super.customTheme()
+    }
+  }
+
+  override fun getToolbar(): CustomToolbar? {
+    return binding?.appBarLayout?.toolbar
+  }
+
+
+  override fun getToolbarBackgroundColor(): Int? {
+    return when (type) {
+      FragmentType.MY_DIGITAL_CHANNEL -> ContextCompat.getColor(this, R.color.bg_dark_grey)
+      else -> super.getToolbarBackgroundColor()
+    }
+  }
+
+  override fun getToolbarTitleColor(): Int? {
+    return when (type) {
+      FragmentType.MY_DIGITAL_CHANNEL -> ContextCompat.getColor(this, R.color.white)
+      else -> super.getToolbarTitleColor()
+    }
+  }
+
+  override fun getToolbarTitleSize(): Float? {
+    return when (type) {
+      FragmentType.MY_DIGITAL_CHANNEL -> ConversionUtils.dp2px(16f).toFloat()
+      else -> return super.getToolbarTitleSize()
+    }
+  }
+
+  override fun isHideToolbar(): Boolean {
+    return when (type) {
+      else -> super.isHideToolbar()
+    }
+  }
+
+  override fun getToolbarTitle(): String? {
+    return when (type) {
+      FragmentType.MY_DIGITAL_CHANNEL -> "My Digital Channels"
+      else -> super.getToolbarTitle()
+    }
+  }
+
+
+  override fun getNavigationIcon(): Drawable? {
+    return when (type) {
+      FragmentType.MY_DIGITAL_CHANNEL -> ContextCompat.getDrawable(this, R.drawable.ic_left_arrow_white)
+      else -> super.getNavigationIcon()
+    }
+  }
+
+  override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    val toolbarMenu = menu ?: return super.onCreateOptionsMenu(menu)
+    val menuRes = getMenuRes() ?: return super.onCreateOptionsMenu(menu)
+    menuInflater.inflate(menuRes, toolbarMenu)
+    return true
+  }
+
+  open fun getMenuRes(): Int? {
+    return when (type) {
+      else -> null
+    }
+  }
+
+  private fun shouldAddToBackStack(): Boolean {
+    return when (type) {
+      else -> false
+    }
+  }
+
+  private fun setFragment() {
+    val fragment = getFragmentInstance(type)
+    fragment?.arguments = intent.extras
+    binding?.container?.id?.let { addFragmentReplace(it, fragment, shouldAddToBackStack()) }
+  }
+
+  private fun getFragmentInstance(type: FragmentType?): BaseFragment<*, *>? {
+    return when (type) {
+      FragmentType.MY_DIGITAL_CHANNEL -> {
+        myDigitalChannelFragment = MyDigitalChannelFragment.newInstance()
+        myDigitalChannelFragment
+      }
+      else -> throw IllegalFragmentTypeException()
+    }
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    when (item.itemId) {
+      android.R.id.home -> {
+        onBackPressed()
+        return true
+      }
+    }
+    return super.onOptionsItemSelected(item)
+  }
+
+}
+
+fun Fragment.startFragmentActivity(type: FragmentType, bundle: Bundle = Bundle(), clearTop: Boolean = false, isResult: Boolean = false) {
+  val intent = Intent(activity, ContainerUpdateChannelActivity::class.java)
+  intent.putExtras(bundle)
+  intent.setFragmentType(type)
+  if (clearTop) intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+  if (isResult.not()) startActivity(intent) else startActivityForResult(intent, 101)
+}
+
+fun startFragmentActivityNew(activity: Activity, type: FragmentType, bundle: Bundle = Bundle(), clearTop: Boolean) {
+  val intent = Intent(activity, ContainerUpdateChannelActivity::class.java)
+  intent.putExtras(bundle)
+  intent.setFragmentType(type)
+  if (clearTop) intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+  activity.startActivity(intent)
+}
+
+fun AppCompatActivity.startFragmentActivity(type: FragmentType, bundle: Bundle = Bundle(), clearTop: Boolean = false) {
+  val intent = Intent(this, ContainerUpdateChannelActivity::class.java)
+  intent.putExtras(bundle)
+  intent.setFragmentType(type)
+  if (clearTop) intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+  startActivity(intent)
+}
+
+fun Intent.setFragmentType(type: FragmentType): Intent {
+  return this.putExtra(FRAGMENT_TYPE, type.name)
+}
