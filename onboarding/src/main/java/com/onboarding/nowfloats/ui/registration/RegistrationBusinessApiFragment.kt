b25990f03416c5d1361e4bd3a92681d28d33b970
Ronak Kumar Samantray <1369203+rokrsa@users.noreply.github.com>
2020-07-03 14:07:49 +0530

    Updating onboardingJSON format
    Store multiple widget_keys against every section of a category.

--- onboarding/src/main/java/com/onboarding/nowfloats/ui/registration/RegistrationBusinessApiFragment.kt
+++ onboarding/src/main/java/com/onboarding/nowfloats/ui/registration/RegistrationBusinessApiFragment.kt
@@ -298,6 +298,7 @@ class RegistrationBusinessApiFragment : BaseRegistrationFragment<FragmentRegistr
     createRequest.fbPageName = requestFloatsModel?.channelAccessTokens?.firstOrNull { it.getType() == ChannelAccessToken.AccessTokenType.facebookpage }?.userAccountName
     createRequest.primaryCategory = requestFloatsModel?.categoryDataModel?.category_key
     createRequest.appExperienceCode = requestFloatsModel?.categoryDataModel?.experience_code
+    //TODO: [Ronak] pass the widgetkeys (split by ,)
     return createRequest
   }
 
