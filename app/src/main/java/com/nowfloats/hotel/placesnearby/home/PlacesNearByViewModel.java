30e483297573141940a51c1bbcde019760124d16
suhassasuke <suhassasuke4090@gmail.com>
2020-07-14 13:08:23 +0530

    place near by design and trip advisor UI design

---
+++ app/src/main/java/com/nowfloats/hotel/placesnearby/home/PlacesNearByViewModel.java
@@ -0,0 +1,7 @@
+package com.nowfloats.hotel.placesnearby.home;
+
+import androidx.lifecycle.ViewModel;
+
+public class PlacesNearByViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
