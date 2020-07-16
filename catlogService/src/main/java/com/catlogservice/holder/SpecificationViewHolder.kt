fe7bbf737804c07b1b65027880b9b7dcb92b87b3
Sarvare Alam <sarvare@cantech.co>
2020-07-16 16:49:37 +0530

    Service specification list add UI deep link bug fix

---
+++ catlogService/src/main/java/com/catlogservice/holder/SpecificationViewHolder.kt
@@ -0,0 +1,8 @@
+package com.catlogservice.holder
+
+import com.catlogservice.databinding.RowLayoutAddedSpecsBinding
+import com.catlogservice.recyclerView.AppBaseRecyclerViewHolder
+
+class SpecificationViewHolder(binding: RowLayoutAddedSpecsBinding) : AppBaseRecyclerViewHolder<RowLayoutAddedSpecsBinding>(binding) {
+
+}
