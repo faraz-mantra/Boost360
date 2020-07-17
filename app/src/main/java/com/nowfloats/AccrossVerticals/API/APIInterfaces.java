ecf8db806aa71b0991770596813c16927dcf8917
suhassasuke <suhassasuke4090@gmail.com>
2020-07-17 20:22:23 +0530

    Temp UI changes for custome Domain design

--- app/src/main/java/com/nowfloats/AccrossVerticals/API/APIInterfaces.java
+++ app/src/main/java/com/nowfloats/AccrossVerticals/API/APIInterfaces.java
@@ -4,6 +4,8 @@ import com.nowfloats.AccrossVerticals.API.model.testimonials.TestimonialModel;
 
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+
 import retrofit.Callback;
 import retrofit.http.GET;
 import retrofit.http.Headers;
@@ -14,6 +16,9 @@ public interface APIInterfaces {
     @Headers({"Authorization: 59ca26afdd30411900b6a8db"})
     @GET("/api/v1/testimonial/get-data")
     void getTestimonialsList(@Query("query") JSONObject query, @Query("skip") int skip, Callback<TestimonialModel> response);
+
+    @GET("/DomainService/v1/domains/supportedTypes")
+    void getDomainSupportType(@Query("clientId") String clientId, Callback<String[]> response);
 
 
 
--- app/src/main/java/com/nowfloats/AccrossVerticals/domain/DomainEmailActivity.java
+++ app/src/main/java/com/nowfloats/AccrossVerticals/domain/DomainEmailActivity.java
@@ -29,6 +29,7 @@ public class DomainEmailActivity extends AppCompatActivity {
     private Fragment currentFragment = null;
     private FragmentManager fragmentManager = null;
     private FragmentTransaction fragmentTransaction = null;
+    public String clientid = "2FA76D4AFCD84494BD609FDB4B3D76782F56AE790A3744198E6F517708CAAA21";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
