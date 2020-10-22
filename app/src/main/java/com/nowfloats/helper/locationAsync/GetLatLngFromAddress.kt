b25288e43dbb3821d9c920668f1a6220ba3962c8
Sarvare Alam <sarvare@cantech.co>
2020-10-22 19:00:19 +0530

    The issue with the city name in onboarding and business profile. Subscription History Renew view remove Call tracker API fix

---
+++ app/src/main/java/com/nowfloats/helper/locationAsync/GetLatLngFromAddress.kt
@@ -0,0 +1,41 @@
+package com.nowfloats.helper.locationAsync
+
+import android.content.Context
+import android.location.Address
+import android.location.Geocoder
+import android.os.AsyncTask
+import com.google.android.gms.maps.model.LatLng
+import java.util.*
+
+class GetLatLngFromAddress(private val contextRef: Context?, val listener: Listener) : AsyncTask<String, Context, LatLng>() {
+
+  override fun onPreExecute() {
+    super.onPreExecute()
+    contextRef?.let { ProgressUtility.showProgress(it) }
+  }
+
+  override fun doInBackground(vararg params: String?): LatLng {
+    var latLng = LatLng(0.0, 0.0)
+    try {
+      val addresses: List<Address>?
+      val geoCoder = Geocoder(contextRef, Locale.getDefault())
+      //get location from lat long if address string is null
+      addresses = geoCoder.getFromLocationName(params[0], 1)
+      if (addresses.isNullOrEmpty().not()) {
+        latLng = LatLng(addresses[0].latitude, addresses[0].longitude)
+      }
+    } catch (ignored: Exception) {
+    }
+    return latLng
+  }
+
+  override fun onPostExecute(result: LatLng?) {
+    super.onPostExecute(result)
+    ProgressUtility.hideProgress()
+    listener.getLatLngResult(result)
+  }
+
+  interface Listener {
+    fun getLatLngResult(latLong: LatLng?)
+  }
+}
