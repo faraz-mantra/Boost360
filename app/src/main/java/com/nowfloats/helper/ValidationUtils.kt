6b737d8d258b106dca54925b417be4ac03261509
Sarvare Alam <sarvare@cantech.co>
2020-10-26 14:01:59 +0530

    Contact information validation change

---
+++ app/src/main/java/com/nowfloats/helper/ValidationUtils.kt
@@ -0,0 +1,11 @@
+package com.nowfloats.helper
+
+import java.util.regex.Pattern
+
+fun isMobileNumberValid(mobile: String): Boolean {
+  return Pattern.matches("[6-9][0-9]{9}", mobile)
+}
+
+fun isEmailValid(email: String): Boolean {
+  return Pattern.matches("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$", email)
+}
