f3d0b44ca8a3e53f5d715e35b7e1c60363210c53
Sarvare Alam <sarvare@cantech.co>
2020-11-10 14:20:23 +0530

    Boost add on data show with expendable view

--- dashboard/src/main/java/com/dashboard/holder/BoostAddOnsViewHolder.kt
+++ dashboard/src/main/java/com/dashboard/holder/BoostAddOnsViewHolder.kt
@@ -1,9 +1,11 @@
 package com.dashboard.holder
 
 import android.view.View
+import androidx.core.content.ContextCompat
 import com.appservice.utils.INITIAL_POSITION
 import com.appservice.utils.ROTATED_POSITION
 import com.appservice.utils.rotateImage
+import com.dashboard.R
 import com.dashboard.databinding.ItemBoostAddOnsBinding
 import com.dashboard.model.AllBoostAddOnsData
 import com.dashboard.recyclerView.AppBaseRecyclerViewAdapter
@@ -20,26 +22,46 @@ class BoostAddOnsViewHolder(binding: ItemBoostAddOnsBinding) : AppBaseRecyclerVi
     val data = item as? AllBoostAddOnsData ?: return
     binding.txtTitle.text = data.title
     binding.txtSubTitle.text = data.subTitle
-    if (data.isLastSeen) {
-      binding.imgArrow.visibility = View.INVISIBLE
-    } else {
+
+    if (data.isLastSeen.not()) {
+      activity?.let { binding.imgSymbol.setTintColor(ContextCompat.getColor(it, R.color.greyish_brown)) }
+      setExpendStatusView(data)
       binding.imgArrow.rotation = if (data.isExpend) ROTATED_POSITION else INITIAL_POSITION
-      binding.rvManageBusiness.visibility = if (data.isExpend) View.VISIBLE else View.GONE
-      binding.contentMain.setOnClickListener {
+      binding.contentParent.setOnClickListener {
         data.isExpend = data.isExpend.not()
         binding.imgArrow.rotateImage(data.isExpend)
-        if (data.isExpend) binding.rvManageBusiness.visible() else binding.rvManageBusiness.gone()
+        setExpendStatusView(data)
       }
-    }
+    } else setLastSeenView(data)
+
     if (data.manageBusinessList.isNullOrEmpty().not()) {
       activity?.let {
         binding.rvManageBusiness.apply {
+          visible()
           val adapter1 = AppBaseRecyclerViewAdapter(it, data.manageBusinessList!!, this@BoostAddOnsViewHolder)
           adapter = adapter1
         }
       }
-    }
+    } else binding.rvManageBusiness.gone()
+
     binding.executePendingBindings()
+  }
+
+  private fun setExpendStatusView(data: AllBoostAddOnsData) {
+    activity?.let { binding.imgArrow.setTintColor(ContextCompat.getColor(it, if (data.isExpend) R.color.grey_dim_2 else R.color.colorAccent)) }
+    activity?.let { binding.txtTitle.setTextColor(ContextCompat.getColor(it, if (data.isExpend) R.color.greyish_brown else R.color.colorAccent)) }
+    activity?.let { binding.contentParent.setCardBackgroundColor(ContextCompat.getColorStateList(it, if (data.isExpend) R.color.white_smoke else R.color.white)) }
+    binding.contentParent.cardElevation = if (data.isExpend) 0F else 2F
+    binding.rvManageBusiness.visibility = if (data.isExpend) View.VISIBLE else View.GONE
+  }
+
+  private fun setLastSeenView(data: AllBoostAddOnsData) {
+    binding.imgArrow.visibility = View.INVISIBLE
+    binding.mainContent.background = activity?.let { ContextCompat.getDrawable(it, R.color.white) }
+    activity?.let { binding.contentParent.setCardBackgroundColor(ContextCompat.getColorStateList(it, R.color.white)) }
+    binding.contentParent.cardElevation = 0F
+    activity?.let { binding.imgSymbol.setTintColor(ContextCompat.getColor(it, R.color.colorAccent)) }
+    activity?.let { binding.txtTitle.setTextColor(ContextCompat.getColor(it, R.color.colorAccent)) }
   }
 
   override fun onItemClick(position: Int, item: BaseRecyclerViewItem?, actionType: Int) {
