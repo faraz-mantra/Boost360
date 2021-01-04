9cb4af0d42e2db1632920c08ba39129053673a7e
Ajay Rawat <ajayrawat.android@gmail.com>
2021-01-04 13:10:00 +0530

    Feature: AppsFlyer's Deep linking Added in DashboardActivity

--- dashboard/src/main/java/com/dashboard/utils/WebEngageController.kt
+++ dashboard/src/main/java/com/dashboard/utils/WebEngageController.kt
@@ -1,23 +1,24 @@
 package com.appservice.utils
 
+import com.appsflyer.AppsFlyerLib
 import com.webengage.sdk.android.User
 import com.webengage.sdk.android.WebEngage
-import java.util.HashMap
+import java.util.*
 
 object WebEngageController {
     var weAnalytics = WebEngage.get().analytics()
     var weUser: User = WebEngage.get().user()
     var isUserLogedIn = false
 
-    fun initiateUserLogin(userId: String?){
-        if(!userId.isNullOrEmpty()) {
+    fun initiateUserLogin(userId: String?) {
+        if (!userId.isNullOrEmpty()) {
             weUser.login(userId)
             isUserLogedIn = true
         }
     }
 
-    fun setUserContactAttributes(email: String?, mobile: String?, name: String?){
-        if(isUserLogedIn) {
+    fun setUserContactAttributes(email: String?, mobile: String?, name: String?) {
+        if (isUserLogedIn) {
             if (!email.isNullOrEmpty()) {
                 weUser.setEmail(email)
             }
@@ -30,14 +31,15 @@ object WebEngageController {
         }
     }
 
-    fun setCategory(userCategory: String?){
-        try{
-            if(!userCategory.isNullOrEmpty())
+    fun setCategory(userCategory: String?) {
+        try {
+            if (!userCategory.isNullOrEmpty())
                 weUser.setAttribute("Category", userCategory)
-        }catch (e: Exception){}
+        } catch (e: Exception) {
+        }
     }
 
-    fun initiateUserLogout(){
+    fun initiateUserLogout() {
         weUser.logout()
         isUserLogedIn = false
     }
@@ -48,6 +50,9 @@ object WebEngageController {
         trackEvent["fptag/event_value"] = event_value
         trackEvent["event_label"] = event_label
         weAnalytics.track(event_name, trackEvent)
+
+        //AppsFlyerEvent...
+        AppsFlyerLib.getInstance().logEvent(weAnalytics.activity.get()?.applicationContext, event_name, trackEvent);
 
     }
 
--- upgrades/src/main/java/com/boost/upgrades/utils/WebEngageController.kt
+++ upgrades/src/main/java/com/boost/upgrades/utils/WebEngageController.kt
@@ -1,5 +1,6 @@
 package com.boost.upgrades.utils
 
+import com.appsflyer.AppsFlyerLib
 import com.framework.analytics.FirebaseAnalyticsUtils
 import com.framework.analytics.FirebaseEvent
 import com.webengage.sdk.android.WebEngage
@@ -64,11 +65,24 @@ object WebEngageController {
                 val event = FirebaseEvent(event_name)
                 event.putIntMap(event_attributes)
                 FirebaseAnalyticsUtils.logEvent(event.getName(), event.getBundle())
+
+                //AppsFlyerEvent...
+                try {
+                    AppsFlyerLib.getInstance().logEvent(weAnalytics.activity.get()?.applicationContext, event_name, event_attributes as Map<String, Any>?)
+                } catch (e: Exception) {
+                }
+
             } else {
                 weAnalytics.track(event_name)
 
                 //Firebase Analytics Event...
                 FirebaseAnalyticsUtils.logEvent(event_name, "", "")
+
+                //AppsFlyerEvent...
+                try {
+                    AppsFlyerLib.getInstance().logEvent(weAnalytics.activity.get()?.applicationContext, event_name, null)
+                } catch (e: Exception) {
+                }
             }
 
         }
@@ -81,6 +95,10 @@ object WebEngageController {
 
             if (event_attributes.size > 0) {
                 weAnalytics.track(event_name, event_attributes)
+
+
+                //Firebase Analytics Event...
+                FirebaseAnalyticsUtils.logEvent(event_name, "", "")
             } else {
                 weAnalytics.track(event_name)
             }
