1b61986178df48dbaa6eb8308aa9483ced18a64f
Sarvare Alam <sarvare@cantech.co>
2021-01-05 12:13:13 +0530

    merge production code to feature_appflyer

--- dashboard/src/main/java/com/dashboard/utils/WebEngageController.kt
+++ dashboard/src/main/java/com/dashboard/utils/WebEngageController.kt
@@ -1,4 +1,4 @@
-package com.appservice.utils
+package com.dashboard.utils
 
 import com.appsflyer.AppsFlyerLib
 import com.framework.analytics.FirebaseAnalyticsUtils
@@ -7,70 +7,94 @@ import com.webengage.sdk.android.WebEngage
 import java.util.*
 
 object WebEngageController {
-    var weAnalytics = WebEngage.get().analytics()
-    var weUser: User = WebEngage.get().user()
-    var isUserLogedIn = false
+  var weAnalytics = WebEngage.get().analytics()
+  var weUser: User = WebEngage.get().user()
+  var isUserLogedIn = false
 
-    fun initiateUserLogin(userId: String?) {
-        if (!userId.isNullOrEmpty()) {
-            weUser.login(userId)
-            isUserLogedIn = true
-        }
+  fun initiateUserLogin(userId: String?) {
+    if (!userId.isNullOrEmpty()) {
+      weUser.login(userId)
+      isUserLogedIn = true
     }
+  }
 
-    fun setUserContactAttributes(email: String?, mobile: String?, name: String?) {
-        if (isUserLogedIn) {
-            if (!email.isNullOrEmpty()) {
-                weUser.setEmail(email)
-            }
-            if (!mobile.isNullOrEmpty()) {
-                weUser.setPhoneNumber(mobile)
-            }
-            if (!name.isNullOrEmpty()) {
-                weUser.setFirstName(name)
-            }
-        }
+  fun setUserContactAttributes(email: String?, mobile: String?, name: String?, company: String?) {
+    if (isUserLogedIn) {
+      if (!email.isNullOrEmpty()) {
+        weUser.setEmail(email)
+
+        //Firebase Analytics User Property.
+        FirebaseAnalyticsUtils.setUserProperty("emailId", email)
+      }
+      if (!mobile.isNullOrEmpty()) {
+        weUser.setPhoneNumber(mobile)
+
+        //Firebase Analytics User Property.
+        FirebaseAnalyticsUtils.setUserProperty("mobile", mobile)
+      }
+      if (!name.isNullOrEmpty()) {
+        weUser.setFirstName(name)
+
+        //Firebase Analytics User Property.
+        FirebaseAnalyticsUtils.setUserProperty("name", name)
+      }
+      if (!company.isNullOrEmpty()) {
+        weUser.setCompany(company)
+
+        //Firebase Analytics User Property.
+        FirebaseAnalyticsUtils.setUserProperty("company", company)
+      }
     }
+  }
 
-    fun setCategory(userCategory: String?) {
-        try {
-            if (!userCategory.isNullOrEmpty())
-                weUser.setAttribute("Category", userCategory)
-        } catch (e: Exception) {
-        }
+  fun setFPTag(fpTag: String?) {
+    try {
+//      weUser.setAttribute("fpTag", fpTag)
+//      //Firebase Analytics User Property.
+//      FirebaseAnalyticsUtils.setUserProperty("fpTag", fpTag?:"")
+    } catch (e: java.lang.Exception) {
     }
+  }
 
-    fun initiateUserLogout() {
-        weUser.logout()
-        isUserLogedIn = false
+  fun setCategory(userCategory: String?) {
+    try {
+      if (!userCategory.isNullOrEmpty())
+        weUser.setAttribute("Category", userCategory)
+    } catch (e: Exception) {
     }
+  }
 
-    fun trackEvent(event_name: String, event_label: String, event_value: String) {
-        val trackEvent: MutableMap<String, Any> = HashMap()
-        trackEvent["event_name"] = event_name
-        trackEvent["fptag/event_value"] = event_value
-        trackEvent["event_label"] = event_label
-        weAnalytics.track(event_name, trackEvent)
+  fun initiateUserLogout() {
+    weUser.logout()
+    isUserLogedIn = false
+  }
 
-        //Firebase Analytics Event...
-        FirebaseAnalyticsUtils.logDefinedEvent(event_name, event_label, event_value)
+  fun trackEvent(event_name: String = "", event_label: String = "", event_value: String? = "") {
+    try {
+      val trackEvent: MutableMap<String, Any> = HashMap()
+      trackEvent["event_name"] = event_name
+      trackEvent["fptag/event_value"] = event_value?:""
+      trackEvent["event_label"] = event_label
+      weAnalytics.track(event_name, trackEvent)
 
-        //AppsFlyerEvent...
-        try {
-            AppsFlyerLib.getInstance().logEvent(weAnalytics.activity.get()?.applicationContext, event_name, trackEvent);
-        } catch (e: Exception) {
-        }
+      //Firebase Analytics Event...
+      FirebaseAnalyticsUtils.logDefinedEvent(event_name, event_label, event_value?:"")
 
+      //AppsFlyerEvent...
+      AppsFlyerLib.getInstance().logEvent(weAnalytics.activity.get()?.applicationContext, event_name, trackEvent);
+    } catch (e: Exception) {
     }
+  }
 
 
-    fun logout() {
-        weUser!!.logout()
-
-        //Reset Firebase Analytics User Session Event.
-        FirebaseAnalyticsUtils.resetIdentifyUser()
-
-        //End AppsFlyer Analytics User Session Event.
-        AppsFlyerLib.getInstance().setCustomerUserId(null)
+  fun logout() {
+    try {
+      weUser.logout()
+      //Reset Firebase Analytics User Session Event.
+      FirebaseAnalyticsUtils.resetIdentifyUser()
+      //End AppsFlyer Analytics User Session Event.
+      AppsFlyerLib.getInstance().setCustomerUserId(null)
+    } catch (e: Exception) {
     }
+  }
 }
