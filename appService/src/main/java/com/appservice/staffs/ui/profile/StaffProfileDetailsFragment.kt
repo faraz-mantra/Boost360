aff5b0ec1996ac3a583a33931e910079cef23d31
Azhar Ansari <ansari@patchinfotech.com>
2021-02-03 14:12:07 +0530

    staff profile details service update

--- appService/src/main/java/com/appservice/staffs/ui/profile/StaffProfileDetailsFragment.kt
+++ appService/src/main/java/com/appservice/staffs/ui/profile/StaffProfileDetailsFragment.kt
@@ -1,5 +1,6 @@
 package com.appservice.staffs.ui.profile
 
+import android.content.Context
 import android.content.Intent
 import android.graphics.Paint
 import android.os.Bundle
@@ -7,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.widget.PopupMenu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import com.appservice.R
 import com.appservice.base.AppBaseFragment
 import com.appservice.constant.FragmentType
@@ -20,15 +22,16 @@ import com.appservice.staffs.ui.Constants
 import com.appservice.staffs.ui.UserSession
 import com.appservice.staffs.ui.startStaffFragmentActivity
 import com.appservice.staffs.ui.viewmodel.StaffViewModel
+import com.appservice.ui.catalog.common.AppointmentModel
 import com.framework.extensions.observeOnce
 import com.framework.glide.util.glideLoad
+import com.webengage.sdk.android.WebEngage.getApplicationContext
 
 
 class StaffProfileDetailsFragment() : AppBaseFragment<FragmentStaffProfileBinding, StaffViewModel>(), RecyclerItemClickListener {
     private  var serviceIds: ArrayList<String>? = null
     private var staffDetails: StaffDetailsResult? = null
     var serviceAdapter: AppBaseRecyclerViewAdapter<ServiceTimingModel>? = null
-    private var servicesList: ArrayList<ServiceTimingModel>? = null
     override fun getLayout(): Int {
         return R.layout.fragment_staff_profile
     }
@@ -54,7 +57,7 @@ class StaffProfileDetailsFragment() : AppBaseFragment<FragmentStaffProfileBindin
     private fun updateStaffProfile() {
         viewModel?.updateStaffProfile(
                 StaffProfileUpdateRequest(isAvailable = staffDetails?.isAvailable, serviceIds = staffDetails?.serviceIds, gender = staffDetails?.gender, floatingPointTag = UserSession.fpId, name = staffDetails?.name, description = staffDetails
-                        ?.description, experience = staffDetails?.experience, staffId = staffDetails?.id, age = staffDetails?.age, specialisations = staffDetails?.specialisations))?.observe(viewLifecycleOwner, { t ->
+                        ?.description, experience = staffDetails?.experience, staffId = staffDetails?.id, age = staffDetails?.age, specialisations = staffDetails?.specialisations))?.observeOnce(viewLifecycleOwner, { t ->
             when (t.status) {
                 200 -> {
                     showShortToast(getString(R.string.updated))
@@ -74,7 +77,7 @@ class StaffProfileDetailsFragment() : AppBaseFragment<FragmentStaffProfileBindin
         binding!!.ctvEdit.text = getString(R.string.u_edit_info_u)
         val get = arguments?.get(IntentConstant.STAFF_DATA.name) as DataItem
         showProgress(getString(R.string.loading))
-        viewModel?.getStaffDetails(get.id)?.observe(viewLifecycleOwner, {
+        viewModel?.getStaffDetails(get.id)?.observeOnce(viewLifecycleOwner, {
             when (it.status) {
                 200 -> {
                     hideProgress()
@@ -85,9 +88,7 @@ class StaffProfileDetailsFragment() : AppBaseFragment<FragmentStaffProfileBindin
                     binding?.ctvAboutHeading?.text = "About ${staffDetails?.name}"
                     binding?.ctvAboutStaff?.text = staffDetails?.description
                     let { activity?.glideLoad(binding?.civStaffProfileImg!!, staffDetails?.image.toString(), R.drawable.placeholder_image) }
-                    val specialisations = StringBuilder()
-                    staffDetails?.specialisations?.forEachIndexed { _, specialisationsItem -> specialisations.append(specialisationsItem?.value) }
-                    binding?.ctvSpecialization?.text = specialisations
+                    binding?.ctvSpecialization?.text = staffDetails?.specialisations?.get(0)?.value
                     fetchServices()
                     fetchTimings()
                 }
@@ -105,17 +106,18 @@ class StaffProfileDetailsFragment() : AppBaseFragment<FragmentStaffProfileBindin
     }
 
     private fun fetchServices() {
+        var servicesList: ArrayList<ServiceTimingModel>? = null
         viewModel?.getServiceListing(ServiceListRequest(
-                filterBy = FilterBy("ALL", 0, 0), category = "", floatingPointTag = UserSession.fpId))?.observe(viewLifecycleOwner, { response ->
+                filterBy = FilterBy("ALL", 0, 0), category = "", floatingPointTag = UserSession.fpId))?.observeOnce(viewLifecycleOwner, { response ->
             when (response.status) {
                 200 -> {
                     val data = (response as ServiceListResponse).result?.data
                     if (staffDetails?.serviceIds.isNullOrEmpty().not()) {
                         val servicesProvided = data?.filter { item -> staffDetails?.serviceIds!!.contains(item?.id) } as ArrayList<DataItemService>
                         this.serviceIds = data.filter { item -> staffDetails?.serviceIds!!.contains(item?.id) } as ArrayList<String>
-                        val services = ArrayList<ServiceTimingModel>()
-                        servicesProvided.forEach { itemService -> services.add(ServiceTimingModel(itemService.name!!)) }
-                        this.serviceAdapter = AppBaseRecyclerViewAdapter(baseActivity, services)
+                        if (servicesList == null) servicesList = arrayListOf()
+                        servicesProvided.forEach { itemService -> servicesList?.add(ServiceTimingModel(itemService.name!!)) }
+                        this.serviceAdapter = AppBaseRecyclerViewAdapter(baseActivity, servicesList as ArrayList<ServiceTimingModel>)
                         binding?.rvServices?.adapter = serviceAdapter
                     }
 
@@ -149,10 +151,27 @@ class StaffProfileDetailsFragment() : AppBaseFragment<FragmentStaffProfileBindin
                 startStaffFragmentActivity(requireActivity(), FragmentType.STAFF_SELECT_SERVICES_FRAGMENT, bundle, clearTop = false, isResult = true, requestCode = Constants.REQUEST_CODE_SERVICES_PROVIDED)
             }
             binding!!.ctvEditTiming -> {
+                val bundle = Bundle()
+                if (staffDetails?.serviceIds.isNullOrEmpty().not()) {
+                    bundle.putSerializable(IntentConstant.STAFF_TIMINGS.name, staffDetails?.timings as ArrayList<AppointmentModel>)
+                }
+                startStaffFragmentActivity(requireActivity(), FragmentType.STAFF_TIMING_FRAGMENT, bundle, clearTop = false, isResult = true, requestCode = Constants.REQUEST_CODE_STAFF_TIMING)
 
             }
         }
     }
+     fun updateStaffTimings(){
+        viewModel?.updateStaffTiming(StaffTimingAddUpdateRequest(staffId = staffDetails?.id, staffDetails?.timings))?.observeOnce(viewLifecycleOwner, Observer {
+            when (it.status) {
+                200 -> {
+                    showShortToast(getString(R.string.staff_timings_updated))
+                }
+                else -> {
+                    showShortToast(getString(R.string.staff_timings_unable_to_update))
+                }
+            }
+        })
+     }
 
     private fun showPopUp(view: View) {
         val popupMenu = PopupMenu(activity, view)
@@ -160,50 +179,55 @@ class StaffProfileDetailsFragment() : AppBaseFragment<FragmentStaffProfileBindin
         // inflate the layout of the popup window
         val inflater = LayoutInflater.from(baseActivity)
         val popupView = inflater.inflate(R.layout.popup_window, null)
-//        inflater.inflate(R.layout.popup_window, popupMenu.menu)
         popupMenu.show()
 
         popupMenu.setOnMenuItemClickListener {
             when (it.itemId) {
                 R.id.action_menu_inactive -> {
                     staffDetails?.isAvailable = false
+                    updateStaffProfile()
                 }
                 R.id.action_menu_remove_staff -> {
                     showProgress(getString(R.string.removing_staff))
-                    viewModel?.deleteStaffProfile(StaffDeleteImageProfileRequest(staffDetails?.id, UserSession.fpId))?.observe(viewLifecycleOwner, { response ->
-                        when (response.status) {
-                            200 -> {
-                                baseActivity.setResult(AppCompatActivity.RESULT_OK)
-                                baseActivity.finish()
-                                hideProgress()
-                            }
-                            else -> {
-                                showShortToast(getString(R.string.unable_to_delete))
-                            }
-                        }
-                    })
+                    removeStaffProfile()
 
                 }
             }
-            viewModel?.updateStaffProfile(StaffProfileUpdateRequest(isAvailable = false))
-
             true
         }
+    }
+
+    private fun removeStaffProfile() {
+        viewModel?.deleteStaffProfile(StaffDeleteImageProfileRequest(staffDetails?.id, UserSession.fpId))?.observe(viewLifecycleOwner, { response ->
+            when (response.status) {
+                200 -> {
+                    baseActivity.setResult(AppCompatActivity.RESULT_OK)
+                    baseActivity.finish()
+                    hideProgress()
+                }
+                else -> {
+                    showShortToast(getString(R.string.unable_to_delete))
+                }
+            }
+        })
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         when {
             requestCode == Constants.REQUEST_CODE_SERVICES_PROVIDED && resultCode == AppCompatActivity.RESULT_OK -> {
+                val service: ArrayList<ServiceTimingModel> = arrayListOf()
                 val resultServices = data!!.extras!![IntentConstant.STAFF_SERVICES.name] as ArrayList<DataItemService>
-                resultServices.forEach { itemService -> servicesList?.add(ServiceTimingModel(itemService.name!!)) }
-                serviceAdapter?.clear()
-                servicesList?.let { serviceAdapter?.updateList(it) }
+                resultServices.forEach { itemService -> service.add(ServiceTimingModel(itemService.name!!)) }
                 staffDetails?.serviceIds = resultServices.map { it.id }
-                serviceAdapter?.notifyDataSetChanged()
+                serviceAdapter?.updateList(service)
                 updateStaffProfile()
             }
             requestCode == Constants.REQUEST_CODE_STAFF_TIMING && resultCode == AppCompatActivity.RESULT_OK -> {
+                val resultTimings = data!!.extras!![IntentConstant.STAFF_TIMINGS.name] as ArrayList<AppointmentModel>
+                staffDetails?.timings = resultTimings
+
+
             }
         }
 
