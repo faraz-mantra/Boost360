168ef7ebaac82abedcc42937af6392f21de9d9f5
Azhar Ansari <ansari@patchinfotech.com>
2021-02-09 00:46:55 +0530

    changes

---
+++ appService/src/main/java/com/appservice/holder/ServiceListingViewHolder.kt
@@ -0,0 +1,7 @@
+package com.appservice.holder
+
+import com.appservice.databinding.ItemPdfFileBinding
+import com.appservice.recyclerView.AppBaseRecyclerViewHolder
+
+class ServiceListingViewHolder(binding: ItemPdfFileBinding) : AppBaseRecyclerViewHolder<ItemPdfFileBinding>(binding) {
+}
