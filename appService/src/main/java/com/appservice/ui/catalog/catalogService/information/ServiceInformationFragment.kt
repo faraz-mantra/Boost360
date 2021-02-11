b4fc407852821c535d284cc039b972a7fd9bdfe9
Azhar Ansari <ansari@patchinfotech.com>
2021-02-11 12:51:03 +0530

    secondary image size

--- appService/src/main/java/com/appservice/ui/catalog/catalogService/information/ServiceInformationFragment.kt
+++ appService/src/main/java/com/appservice/ui/catalog/catalogService/information/ServiceInformationFragment.kt
@@ -315,6 +315,28 @@ class ServiceInformationFragment : AppBaseFragment<FragmentServiceInformationBin
         }
 
       }
+      RecyclerViewActionType.IMAGE_CHANGE.ordinal -> {
+        val data = item as? FileModel
+        if (isEdit && data?.pathUrl.isNullOrEmpty().not()) {
+          val dataImage = secondaryDataImage?.firstOrNull { it.ActualImage == data?.pathUrl }
+                  ?: return
+          showProgress(resources.getString(R.string.removing_image))
+          val request = DeleteSecondaryImageRequest(product?.productId, dataImage.ImageId);
+          viewModel?.deleteSecondaryImage(request)?.observeOnce(viewLifecycleOwner, {
+            if (it.status == 200 || it.status == 201 || it.status == 202) {
+              secondaryDataImage?.remove(dataImage)
+              secondaryImage.remove(data)
+              setAdapter()
+            } else showLongToast(resources.getString(R.string.removing_image_failed))
+            hideProgress()
+          })
+        } else {
+          secondaryImage.remove(data)
+          setAdapter()
+        }
+        openImagePicker()
+
+      }
       RecyclerViewActionType.CLEAR_SPECIFICATION_CLICK.ordinal -> {
         if (specList.size > 1) {
           val element = item as? KeySpecification ?: return
