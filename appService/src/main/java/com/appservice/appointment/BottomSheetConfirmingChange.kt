bebbca82816754e9fff428a45c78d5f629a992e6
Azhar Ansari <ansari@patchinfotech.com>
2021-02-25 23:56:22 +0530

    bottomsheets ui design

---
+++ appService/src/main/java/com/appservice/appointment/BottomSheetConfirmingChange.kt
@@ -0,0 +1,4 @@
+package com.appservice.appointment
+
+class BottomSheetConfirmingChange {
+}
---
+++ appService/src/main/java/com/appservice/appointment/FragmentCustomerPolicies.kt
@@ -0,0 +1,7 @@
+package com.appservice.appointment
+
+import com.appservice.base.AppBaseFragment
+import com.framework.base.BaseFragment
+
+class FragmentCustomerPolicies {
+}
