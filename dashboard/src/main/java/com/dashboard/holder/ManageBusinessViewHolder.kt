ccb27c134cf7a5dd7e4222d005db9e8473224ced
Sarvare Alam <sarvare@cantech.co>
2021-03-10 12:14:11 +0530

    Boost add-ons bug fixed

--- dashboard/src/main/java/com/dashboard/holder/ManageBusinessViewHolder.kt
+++ dashboard/src/main/java/com/dashboard/holder/ManageBusinessViewHolder.kt
@@ -6,6 +6,7 @@ import com.dashboard.databinding.ItemManageBusinessDBinding
 import com.dashboard.model.live.addOns.ManageBusinessData
 import com.dashboard.recyclerView.AppBaseRecyclerViewHolder
 import com.dashboard.recyclerView.BaseRecyclerViewItem
+import com.framework.glide.util.glideLoad
 
 class ManageBusinessViewHolder(binding: ItemManageBusinessDBinding) : AppBaseRecyclerViewHolder<ItemManageBusinessDBinding>(binding) {
 
@@ -13,7 +14,9 @@ class ManageBusinessViewHolder(binding: ItemManageBusinessDBinding) : AppBaseRec
     super.bind(position, item)
     val data = item as? ManageBusinessData ?: return
     binding.txtTitle.text = data.title
-    ManageBusinessData.BusinessType.fromName(data.businessType)?.let { binding.imgIcon.setImageResource(it.icon) }
+    ManageBusinessData.BusinessType.fromName(data.businessType)?.let {
+      activity?.glideLoad( binding.imgIcon,it.icon)
+    }
     binding.imgLock.visibility = if (data.isLock) View.VISIBLE else View.GONE
     binding.imgIcon.apply { if (data.isLock) this.makeGreyscale() else this.removeGreyscale() }
     binding.mainContent.setOnClickListener { listener?.onItemClick(position, data, RecyclerViewActionType.BUSINESS_ADD_ONS_CLICK.ordinal) }
