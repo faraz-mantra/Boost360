755ad8a4b8ee9f917f3813b328b403066ff878c0
Anand Choudhary <anandpc13@gmail.com>
2021-03-10 15:16:19 +0530

    added missing function

--- dashboard/src/main/java/com/dashboard/utils/WebEngageController.kt
+++ dashboard/src/main/java/com/dashboard/utils/WebEngageController.kt
@@ -12,5 +12,7 @@ object WebEngageController {
     fun trackEvent(event_name: String = "", event_label: String = "", event_value: String? = "") =
             NFWebEngageController.trackEvent(event_name, event_label, event_value ?: "")
 
+    fun setFPTag(fpTag: String?) = NFWebEngageController.setFPTag(fpTag ?: "")
+
     fun logout() = NFWebEngageController.logout()
 }
