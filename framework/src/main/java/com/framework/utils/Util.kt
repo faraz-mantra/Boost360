cbdd714453990bfeeae962af001104a05bf8bbad
Sarvare Alam <sarvare@cantech.co>
2021-04-28 16:35:40 +0530

    Add-ons Ui and filter fixed Service share whatsapp bug fixed My business card Ui and share image bug fixed Staff tutorial video  fixed permission  fixed pan card scan Subscription history remove in setting

--- framework/src/main/java/com/framework/utils/Util.kt
+++ framework/src/main/java/com/framework/utils/Util.kt
@@ -6,9 +6,12 @@ import android.os.Build
 import android.text.Html
 import android.text.Spanned
 import android.view.View
+import android.view.ViewConfiguration
 import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
 import java.text.NumberFormat
 import java.util.*
+
 
 fun Activity.hideKeyBoard() {
   val view = this.currentFocus
@@ -39,7 +42,24 @@ fun getNumberFormat(value: String): String {
   }
 }
 
-fun Double.roundTo(n : Int) : Double {
+fun Double.roundTo(n: Int) : Double {
   return "%.${n}f".format(this).toDouble()
 }
 
+
+fun AppCompatActivity.getStatusBarHeight(): Int {
+  var result = 0
+  val resourceId: Int = resources.getIdentifier("status_bar_height", "dimen", "android")
+  if (resourceId > 0) {
+    result = resources.getDimensionPixelSize(resourceId)
+  }
+  return result
+}
+
+fun AppCompatActivity.getNavigationBarHeight(): Int {
+  val hasMenuKey = ViewConfiguration.get(this).hasPermanentMenuKey()
+  val resourceId: Int = resources.getIdentifier("navigation_bar_height", "dimen", "android")
+  return if (resourceId > 0 && !hasMenuKey) {
+    resources.getDimensionPixelSize(resourceId)
+  } else 0
+}
