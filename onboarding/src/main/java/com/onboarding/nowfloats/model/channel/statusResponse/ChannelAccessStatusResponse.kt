31fd6e4c89ed170b4f716789bb03b23c57c0dc43
Sarvare Alam <sarvare@cantech.co>
2021-05-19 16:22:38 +0530

    Bug fixed

--- onboarding/src/main/java/com/onboarding/nowfloats/model/channel/statusResponse/ChannelAccessStatusResponse.kt
+++ onboarding/src/main/java/com/onboarding/nowfloats/model/channel/statusResponse/ChannelAccessStatusResponse.kt
@@ -1,6 +1,5 @@
 package com.onboarding.nowfloats.model.channel.statusResponse
 
-
 import android.widget.LinearLayout
 import androidx.core.view.children
 import com.framework.base.BaseResponse
@@ -10,6 +9,7 @@ import com.framework.utils.*
 import com.framework.views.customViews.CustomImageView
 import com.google.gson.annotations.SerializedName
 import java.io.Serializable
+
 
 data class ChannelAccessStatusResponse(
     @SerializedName("channels")
@@ -23,31 +23,30 @@ data class ChannelAccessStatusResponse(
     @SerializedName("success")
     var success: Boolean? = null,
 ) : BaseResponse(), Serializable {
-    companion object {
-        const val CONNECTED_CHANNELS = "connected_channels"
-        fun getConnectedChannel(): ArrayList<String> {
-            val resp = PreferencesUtils.instance.getData(CONNECTED_CHANNELS, "") ?: ""
-            return ArrayList(convertStringToList(resp) ?: ArrayList())
-        }
 
-        fun visibleChannels(view: LinearLayout) {
-            for (it in view.children) {
-                val customImageView = it as? CustomImageView
-                val tag = customImageView?.tag
-                if (getConnectedChannel().contains(tag)) {
-                    customImageView?.visible()
-                } else {
-                    customImageView?.gone()
-                }
-            }
-        }
+  companion object {
 
-        fun saveDataConnectedChannel(connectedChannels: ArrayList<String>?) {
-            PreferencesUtils.instance.saveData(
-                key = CONNECTED_CHANNELS,
-                convertListObjToString(connectedChannels ?: ArrayList()) ?: ""
-            )
-        }
+    const val CONNECTED_CHANNELS = "connected_channels"
+
+    fun getConnectedChannel(): ArrayList<String> {
+      val resp = PreferencesUtils.instance.getData(CONNECTED_CHANNELS, "") ?: ""
+      return ArrayList(convertStringToList(resp) ?: ArrayList())
     }
 
+    fun visibleChannels(view: LinearLayout) {
+      for (it in view.children) {
+        val customImageView = it as? CustomImageView
+        val tag = customImageView?.tag
+        if (getConnectedChannel().contains(tag)) {
+          customImageView?.visible()
+        } else {
+          customImageView?.gone()
+        }
+      }
+    }
+
+    fun saveDataConnectedChannel(connectedChannels: ArrayList<String>?) {
+      PreferencesUtils.instance.saveData(key = CONNECTED_CHANNELS, convertListObjToString(connectedChannels ?: ArrayList()) ?: "")
+    }
+  }
 }
