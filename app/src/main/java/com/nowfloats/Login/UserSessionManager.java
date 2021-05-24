34a80c521fba77a426c40e0a00a89dbd5c660cbe
Sarvare Alam <sarvare@cantech.co>
2021-05-24 12:36:21 +0530

    Bug fixed

--- app/src/main/java/com/nowfloats/Login/UserSessionManager.java
+++ app/src/main/java/com/nowfloats/Login/UserSessionManager.java
@@ -28,6 +28,7 @@ import com.nowfloats.util.DataBase;
 import com.nowfloats.util.Key_Preferences;
 import com.nowfloats.util.Methods;
 import com.nowfloats.util.MixPanelController;
+import com.nowfloats.util.WebEngageController;
 import com.thinksity.R;
 import com.webengage.sdk.android.WebEngage;
 
@@ -987,6 +988,8 @@ public class UserSessionManager implements Fetch_Home_Data.Fetch_Home_Data_Inter
 
     private void processUserSessionDataClear() {
         try {
+            WebEngageController.logout();
+            AnaCore.logoutUser(activity);
             setUserLogin(false);
             DataBase db = new DataBase(activity);
             DbController.getDbController(activity.getApplicationContext()).deleteDataBase();
@@ -995,8 +998,6 @@ public class UserSessionManager implements Fetch_Home_Data.Fetch_Home_Data_Inter
             activity.getSharedPreferences(TwitterConnection.PREF_NAME, Context.MODE_PRIVATE).edit().clear().apply();
             activity.getSharedPreferences("prefs", Context.MODE_PRIVATE).edit().clear().apply();
 
-            WebEngage.get().user().logout();
-            AnaCore.logoutUser(activity);
             AppController.getInstance().clearApplicationData();
             Date date = new Date(System.currentTimeMillis());
             String dateString = date.toString();
