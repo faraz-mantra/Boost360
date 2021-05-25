867d7750bc4fed35398bbfb7db9db472e2379487
Azhar Ansari <ansari@patchinfotech.com>
2021-05-25 15:01:01 +0530

    product service listing offer bug fixed

--- florisboard/src/main/java/dev/patrickgold/florisboard/customization/model/response/Product.kt
+++ florisboard/src/main/java/dev/patrickgold/florisboard/customization/model/response/Product.kt
@@ -4,126 +4,157 @@ import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 import dev.patrickgold.florisboard.customization.adapter.BaseRecyclerItem
 import dev.patrickgold.florisboard.customization.adapter.FeaturesEnum
+import java.lang.Exception
+import kotlin.math.roundToInt
 
 data class Product(
-        @SerializedName("ApplicationId")
-        @Expose
-        var applicationId: String? = null,
+    @SerializedName("ApplicationId")
+    @Expose
+    var applicationId: String? = null,
 
-        @SerializedName("BuyOnlineLink")
-        @Expose
-        var buyOnlineLink: String? = null,
+    @SerializedName("BuyOnlineLink")
+    @Expose
+    var buyOnlineLink: String? = null,
 
-        @SerializedName("CreatedOn")
-        @Expose
-        var createdOn: String? = null,
+    @SerializedName("CreatedOn")
+    @Expose
+    var createdOn: String? = null,
 
-        @SerializedName("CurrencyCode")
-        @Expose
-        var currencyCode: String? = null,
+    @SerializedName("CurrencyCode")
+    @Expose
+    var currencyCode: String? = null,
 
-        @SerializedName("CustomWidgets")
-        @Expose
-        var customWidgets: Any? = null,
+    @SerializedName("CustomWidgets")
+    @Expose
+    var customWidgets: Any? = null,
 
-        @SerializedName("Description")
-        @Expose
-        var description: String? = null,
+    @SerializedName("Description")
+    @Expose
+    var description: String? = null,
 
-        @SerializedName("DiscountAmount")
-        @Expose
-        var discountAmount: String? = null,
+    @SerializedName("DiscountAmount")
+    @Expose
+    var discountAmount: String? = null,
 
-        @SerializedName("ExternalSourceId")
-        @Expose
-        var externalSourceId: Any? = null,
+    @SerializedName("ExternalSourceId")
+    @Expose
+    var externalSourceId: Any? = null,
 
-        @SerializedName("FPTag")
-        @Expose
-        var fPTag: String? = null,
+    @SerializedName("FPTag")
+    @Expose
+    var fPTag: String? = null,
 
-        @SerializedName("GPId")
-        @Expose
-        var gPId: Any? = null,
+    @SerializedName("GPId")
+    @Expose
+    var gPId: Any? = null,
 
-        @SerializedName("GroupProductId")
-        @Expose
-        var groupProductId: Any? = null,
+    @SerializedName("GroupProductId")
+    @Expose
+    var groupProductId: Any? = null,
 
-        @SerializedName("ImageUri")
-        @Expose
-        var imageUri: String? = null,
+    @SerializedName("ImageUri")
+    @Expose
+    var imageUri: String? = null,
 
-        @SerializedName("Images")
-        @Expose
-        var images: Any? = null,
+    @SerializedName("Images")
+    @Expose
+    var images: Any? = null,
 
-        @SerializedName("IsArchived")
-        @Expose
-        var isArchived: Boolean? = null,
+    @SerializedName("IsArchived")
+    @Expose
+    var isArchived: Boolean? = null,
 
-        @SerializedName("IsAvailable")
-        @Expose
-        var isAvailable: Boolean? = null,
-        @SerializedName("IsFreeShipmentAvailable")
-        @Expose
-        var isFreeShipmentAvailable: Boolean? = null,
+    @SerializedName("IsAvailable")
+    @Expose
+    var isAvailable: Boolean? = null,
+    @SerializedName("IsFreeShipmentAvailable")
+    @Expose
+    var isFreeShipmentAvailable: Boolean? = null,
 
-        @SerializedName("MerchantName")
-        @Expose
-        var merchantName: String? = null,
+    @SerializedName("MerchantName")
+    @Expose
+    var merchantName: String? = null,
 
-        @SerializedName("Name")
-        @Expose
-        var name: String? = null,
+    @SerializedName("Name")
+    @Expose
+    var name: String? = null,
 
-        @SerializedName("Price")
-        @Expose
-        var price: String? = null,
+    @SerializedName("Price")
+    @Expose
+    var price: String? = null,
 
-        @SerializedName("Priority")
-        @Expose
-        var priority: Int? = null,
+    @SerializedName("Priority")
+    @Expose
+    var priority: Int? = null,
 
-        @SerializedName("ProductIndex")
-        @Expose
-        var productIndex: Int? = null,
+    @SerializedName("ProductIndex")
+    @Expose
+    var productIndex: Int? = null,
 
-        @SerializedName("ProductUrl")
-        @Expose
-        var productUrl: String? = null,
+    @SerializedName("ProductUrl")
+    @Expose
+    var productUrl: String? = null,
 
-        @SerializedName("ShipmentDuration")
-        @Expose
-        var shipmentDuration: Int? = null,
+    @SerializedName("ShipmentDuration")
+    @Expose
+    var shipmentDuration: Int? = null,
 
-        @SerializedName("TileImageUri")
-        @Expose
-        var tileImageUri: Any? = null,
+    @SerializedName("TileImageUri")
+    @Expose
+    var tileImageUri: Any? = null,
 
-        @SerializedName("TotalQueries")
-        @Expose
-        var totalQueries: Int? = null,
+    @SerializedName("TotalQueries")
+    @Expose
+    var totalQueries: Int? = null,
 
-        @SerializedName("UpdatedOn")
-        @Expose
-        var updatedOn: String? = null,
+    @SerializedName("UpdatedOn")
+    @Expose
+    var updatedOn: String? = null,
 
-        @SerializedName("_id")
-        @Expose
-        var id: String? = null,
+    @SerializedName("_id")
+    @Expose
+    var id: String? = null,
 
-        @SerializedName("_keywords")
-        @Expose
-        var keywords: List<String>? = null,
+    @SerializedName("_keywords")
+    @Expose
+    var keywords: List<String>? = null,
 
-        @SerializedName("availableUnits")
-        @Expose
-        var availableUnits: Int? = null,
+    @SerializedName("availableUnits")
+    @Expose
+    var availableUnits: Int? = null,
 
-        @SerializedName("sharedPlatforms")
-        @Expose
-        var sharedPlatforms: List<Any>? = null
+    @SerializedName("sharedPlatforms")
+    @Expose
+    var sharedPlatforms: List<Any>? = null
 ) : BaseRecyclerItem() {
     override fun getViewType(): Int = FeaturesEnum.PRODUCTS.ordinal
+
+    fun getProductPrice(): String {
+        return if (this.price == null || this.price?.isEmpty() == true)
+            ""
+        else "₹$price"
+    }
+
+    fun getProductDiscountPrice(): String {
+        return if (this.discountAmount == null || this.discountAmount?.isEmpty() == true)
+            ""
+        else "₹$discountAmount"
+    }
+
+    fun getProductOffPrice(): String {
+        return if (this.discountAmount == null || this.discountAmount?.isEmpty() == true || price.isNullOrEmpty())
+            ""
+        else {
+            return try {
+                val offPercent =
+                    ((discountAmount?.toDouble())?.div((price?.toDouble()!!)))?.times(100)
+                if (offPercent?.toInt() ?: 0 <= 0) {
+                    ""
+                } else
+                    "${offPercent?.roundToInt()}% OFF"
+            } catch (e: Exception) {
+                ""
+            }
+        }
+    }
 }
--- florisboard/src/main/java/dev/patrickgold/florisboard/customization/viewholder/ProductViewHolder.kt
+++ florisboard/src/main/java/dev/patrickgold/florisboard/customization/viewholder/ProductViewHolder.kt
@@ -1,36 +1,59 @@
 package dev.patrickgold.florisboard.customization.viewholder
 
+import android.graphics.Paint
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import com.bumptech.glide.Glide
+import com.framework.extensions.gone
+import com.framework.extensions.visible
+import com.framework.views.customViews.CustomTextView
 import dev.patrickgold.florisboard.R
 import dev.patrickgold.florisboard.customization.adapter.BaseRecyclerItem
 import dev.patrickgold.florisboard.customization.adapter.BaseRecyclerViewHolder
 import dev.patrickgold.florisboard.customization.adapter.OnItemClickListener
 import dev.patrickgold.florisboard.customization.model.response.Product
 
-class ProductViewHolder(itemView: View, val listener: OnItemClickListener?) : BaseRecyclerViewHolder(itemView) {
+class ProductViewHolder(itemView: View, val listener: OnItemClickListener?) :
+    BaseRecyclerViewHolder(itemView) {
 
     private val productImage: ImageView = itemView.findViewById(R.id.imageView)
-    private val productName: TextView = itemView.findViewById(R.id.tv_product_name)
-    private val productPrice: TextView = itemView.findViewById(R.id.tv_product_price)
-    private val productDiscount: TextView = itemView.findViewById(R.id.tv_discount)
-    private val productDescription: TextView = itemView.findViewById(R.id.tv_description)
+    private val productName: CustomTextView = itemView.findViewById(R.id.tv_name)
+    private val productPrice: CustomTextView = itemView.findViewById(R.id.tv_price)
+    private val productDiscount: CustomTextView = itemView.findViewById(R.id.tv_discount)
+    private val productDescription: CustomTextView = itemView.findViewById(R.id.tv_description)
     private val btnCopy: Button = itemView.findViewById(R.id.buttonCopy)
+    private val ctvDuration: CustomTextView = itemView.findViewById(R.id.ctv_duration)
+    private val ctvOffPercentage: CustomTextView = itemView.findViewById(R.id.tv_off_percent)
 
     override fun bindTo(position: Int, item: BaseRecyclerItem?) {
         val product = item as Product
         // bind views with data
         Glide.with(productImage).load(product.imageUri)
-                .placeholder(R.drawable.default_product_image).into(productImage)
-
+            .placeholder(R.drawable.default_product_image).into(productImage)
+        if (product.price?.toDouble() ?: 0.0 <= 0) {
+            productPrice.gone()
+        } else {
+            productPrice.visible()
+        }
+        if (product.discountAmount?.toDouble() ?: 0.0 <= 0) {
+            productDiscount.gone()
+        } else {
+            productDiscount.visible()
+        }
+        if (product.shipmentDuration ?: 0 <= 0) {
+            ctvDuration.gone()
+        } else {
+            ctvDuration.visible()
+        }
+        ctvDuration.text = "${product.shipmentDuration} min"
+        ctvOffPercentage.text = product.getProductOffPrice()
         productName.text = product.name
-        productPrice.text = product.price
-        productDiscount.text = product.discountAmount
+        productPrice.text = product.getProductPrice()
+        productDiscount.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
+        productDiscount.text = product.getProductDiscountPrice()
         productDescription.text = product.description
-
         btnCopy.setOnClickListener { listener?.onItemClick(position, product) }
     }
 }
