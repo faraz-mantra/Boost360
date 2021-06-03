8a2a58fbb6acd6a5b3f65ab43527d70e15976c36
Sarvare Alam <sarvare@cantech.co>
2021-06-03 15:36:56 +0530

    Multiple business mange and signup ui changes

--- app/src/main/java/com/nowfloats/helper/ApiReLoadActivity.java
+++ app/src/main/java/com/nowfloats/helper/ApiReLoadActivity.java
@@ -2,7 +2,10 @@ package com.nowfloats.helper;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowInsetsController;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,20 +17,33 @@ import com.onboarding.nowfloats.BaseBoardingApplication;
 
 public class ApiReLoadActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        BaseOrderApplication.apiInitialize();
-        BaseBoardingApplication.apiInitialize();
-        AppServiceApplication.apiInitialize();
-        AppDashboardApplication.apiInitialize();
-        AppPreSignInApplication.apiInitialize();
-        onBackPressed();
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setLightStatusBar(this);
+    BaseOrderApplication.apiInitialize();
+    BaseBoardingApplication.apiInitialize();
+    AppServiceApplication.apiInitialize();
+    AppDashboardApplication.apiInitialize();
+    AppPreSignInApplication.apiInitialize();
+    onBackPressed();
+  }
 
-    @Override
-    public void onBackPressed() {
-        setResult(Activity.RESULT_OK, new Intent());
-        finish();
+  @Override
+  public void onBackPressed() {
+    setResult(Activity.RESULT_OK, new Intent());
+    finish();
+  }
+
+  private void setLightStatusBar(Activity activity) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+      activity.getWindow().getInsetsController().setSystemBarsAppearance(WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS, WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS);
+    } else {
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        activity.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR | View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR);
+      } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        activity.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+      }
     }
+  }
 }
