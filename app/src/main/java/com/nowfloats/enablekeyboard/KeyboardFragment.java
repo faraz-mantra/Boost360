6394301b8c14e7b0cc74cf061b4bd8faf5848c3a
Sarvare Alam <sarvare@cantech.co>
2021-06-07 21:45:56 +0530

    Bug fixed

--- app/src/main/java/com/nowfloats/enablekeyboard/KeyboardFragment.java
+++ app/src/main/java/com/nowfloats/enablekeyboard/KeyboardFragment.java
@@ -106,8 +106,8 @@ public class KeyboardFragment extends Fragment implements View.OnTouchListener, 
     mainLayout = view.findViewById(R.id.main_layout);
     secondaryLayout = view.findViewById(R.id.secondary_layout);
     buyItemButton = view.findViewById(R.id.buy_item);
-    if ((Constants.StoreWidgets != null && Constants.StoreWidgets.contains("BOOSTKEYBOARD")) ||
-        (Constants.currentActivePackageId != null && Constants.currentActivePackageId.contains("59ce2ae56431a80b009cb1fa"))) {
+    if (session.getStoreWidgets().contains("BOOSTKEYBOARD") || (Constants.currentActivePackageId != null &&
+        Constants.currentActivePackageId.contains("59ce2ae56431a80b009cb1fa"))) {
       mainLayout.setVisibility(View.VISIBLE);
       secondaryLayout.setVisibility(View.GONE);
     } else {
