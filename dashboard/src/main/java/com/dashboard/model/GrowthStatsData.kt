211eb0c2cdd2148ca9c32bfdba0f1020611e5e80
Sarvare Alam <sarvare@cantech.co>
2021-06-11 16:43:58 +0530

    Bug fixes

--- dashboard/src/main/java/com/dashboard/model/GrowthStatsData.kt
+++ dashboard/src/main/java/com/dashboard/model/GrowthStatsData.kt
@@ -27,7 +27,7 @@ class GrowthStatsData(
     list.add(GrowthStatsData(title = "Unique visits", value = summary?.getNoOfUniqueViews(), icon1 = R.drawable.ic_uniqe_visit_d,type = GrowthType.UNIQUE_VISITS.name))
     list.add(GrowthStatsData(title = "Address views", value = mapVisitCount, icon1 = R.drawable.ic_address_view_d,type = GrowthType.ADDRESS_NEWS.name))
     list.add(GrowthStatsData(title = "Newsletter Subscribers", value = summary?.getNoOfSubscribers(), icon1 = R.drawable.ic_news_subcription_d,type = GrowthType.NEWSLETTER_SUBSCRIPTION.name))
-    list.add(GrowthStatsData(title = "Search Queries", value = searchCount, icon1 = R.drawable.ic_search_queries_d,type = GrowthType.SEARCH_QUERIES.name))
+//    list.add(GrowthStatsData(title = "Search Queries", value = searchCount, icon1 = R.drawable.ic_search_queries_d,type = GrowthType.SEARCH_QUERIES.name))
     return list
   }
 
