b7ecf4a298ed0aad36fa1c1d5ec1e323ec1e10c1
Azhar Ansari <ansari@patchinfotech.com>
2021-06-15 21:02:12 +0530

    need help alert dialog in signin/signup module

--- presignin/src/main/java/com/boost/presignin/base/AppBaseFragment.kt
+++ presignin/src/main/java/com/boost/presignin/base/AppBaseFragment.kt
@@ -1,6 +1,7 @@
 package com.boost.presignin.base
 
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
@@ -10,6 +11,7 @@ import android.text.util.Linkify
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.view.ContextThemeWrapper
@@ -19,13 +21,19 @@ import com.framework.base.BaseFragment
 import com.framework.models.BaseViewModel
 import com.onboarding.nowfloats.constant.PreferenceConstant
 
-abstract class AppBaseFragment<Binding : ViewDataBinding, ViewModel : BaseViewModel> : BaseFragment<Binding, ViewModel>() {
+
+abstract class AppBaseFragment<Binding : ViewDataBinding, ViewModel : BaseViewModel> :
+  BaseFragment<Binding, ViewModel>() {
 
 
   protected var appBaseActivity: AppBaseActivity<*, *>? = null
   private var progressView: ProgressDialog? = null
 
-  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
     appBaseActivity = activity as? AppBaseActivity<*, *>
     progressView = ProgressDialog.newInstance()
     return super.onCreateView(inflater, container, savedInstanceState)
@@ -34,14 +42,21 @@ abstract class AppBaseFragment<Binding : ViewDataBinding, ViewModel : BaseViewMo
   override fun onCreateView() {
 
   }
+
   protected val pref: SharedPreferences?
     get() {
-      return baseActivity.getSharedPreferences(PreferenceConstant.NOW_FLOATS_PREFS, Context.MODE_PRIVATE)
+      return baseActivity.getSharedPreferences(
+        PreferenceConstant.NOW_FLOATS_PREFS,
+        Context.MODE_PRIVATE
+      )
     }
 
   protected val prefReferral: SharedPreferences?
     get() {
-      return baseActivity.getSharedPreferences(PreferenceConstant.PREF_NAME_REFERRAL, Context.MODE_PRIVATE)
+      return baseActivity.getSharedPreferences(
+        PreferenceConstant.PREF_NAME_REFERRAL,
+        Context.MODE_PRIVATE
+      )
     }
 
   protected fun getToolbarTitle(): String? {
@@ -52,13 +67,16 @@ abstract class AppBaseFragment<Binding : ViewDataBinding, ViewModel : BaseViewMo
     progressView?.hideProgress()
   }
 
-  protected open fun showProgress(title: String? = "Please wait...", cancelable: Boolean? = false) {
+  protected open fun showProgress(
+    title: String? = "Please wait...",
+    cancelable: Boolean? = false
+  ) {
     title?.let { progressView?.setTitle(it) }
     cancelable?.let { progressView?.isCancelable = it }
     activity?.let { progressView?.showProgress(it.supportFragmentManager) }
   }
 
- protected fun logout() {
+  protected fun logout() {
     try {
       val i = Intent(baseActivity, Class.forName("com.nowfloats.helper.LogoutActivity"))
       startActivity(i)
@@ -66,14 +84,16 @@ abstract class AppBaseFragment<Binding : ViewDataBinding, ViewModel : BaseViewMo
       e.printStackTrace()
     }
   }
-  protected fun needHelp(){
+
+  protected fun needHelp() {
     val s = SpannableString(resources.getString(R.string.need_help_desc))
     Linkify.addLinks(s, Linkify.ALL)
-    AlertDialog.Builder(ContextThemeWrapper(baseActivity, R.style.AlertDialogCustom))
-      .setTitle(getString(R.string.need_help_title))
-      .setMessage(s)
-      .setPositiveButton(resources.getString(R.string.okay), null).show()
-      .findViewById<TextView>(android.R.id.message)?.movementMethod = LinkMovementMethod.getInstance()
+    val alertDialog = AlertDialog.Builder(ContextThemeWrapper(baseActivity, R.style.AlertDialogCustom))
+    alertDialog.setTitle(getString(R.string.need_help_title)).setMessage(s).setPositiveButton(resources.getString(R.string.okay), null)
+    val alert = alertDialog.create()
+    alert.show()
+    alert.findViewById<TextView>(android.R.id.message)?.movementMethod = LinkMovementMethod.getInstance()
+    alert.getButton(DialogInterface.BUTTON_POSITIVE).setTextColor(getColor(R.color.colorAccent))
   }
 
 }
