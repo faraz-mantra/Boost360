4f3a7cbd8d8fd3dce829dd639bf03e7c74543a5c
Sarvare Alam <sarvare@cantech.co>
2021-07-08 16:14:28 +0530

    Bug fixed

--- dashboard/src/main/java/com/dashboard/holder/ManageBusinessViewHolder.kt
+++ dashboard/src/main/java/com/dashboard/holder/ManageBusinessViewHolder.kt
@@ -14,9 +14,7 @@ class ManageBusinessViewHolder(binding: ItemManageBusinessDBinding) : AppBaseRec
     super.bind(position, item)
     val data = item as? ManageBusinessData ?: return
     binding.txtTitle.text = data.title
-    ManageBusinessData.BusinessType.fromName(data.businessType)?.let {
-      activity?.glideLoad( binding.imgIcon,it.icon)
-    }
+    ManageBusinessData.BusinessType.fromName(data.businessType)?.let { binding.imgIcon.setImageResource(it.icon) }
     binding.imgLock.visibility = if (data.isLock) View.VISIBLE else View.GONE
     binding.imgIcon.apply { if (data.isLock) this.makeGreyscale() else this.removeGreyscale() }
     binding.mainContent.setOnClickListener { listener?.onItemClick(position, data, RecyclerViewActionType.BUSINESS_ADD_ONS_CLICK.ordinal) }
