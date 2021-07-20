0dae1126d060cad26e91cc4c047a631d0d12c083
Sarvare Alam <sarvare@cantech.co>
2021-07-20 19:39:06 +0530

    Bug fixed and styling

--- upgrades/src/main/java/com/boost/upgrades/ui/freeaddons/FreeAddonsFragment.kt
+++ upgrades/src/main/java/com/boost/upgrades/ui/freeaddons/FreeAddonsFragment.kt
@@ -126,7 +126,7 @@ class FreeAddonsFragment : BaseFragment(), MyAddonsListener {
 
 
       if (totalFreeItemList != null) {
-        paid_title.setText(totalFreeItemList!!.size.toString() + " Free add-ons")
+        paid_title.setText(totalFreeItemList!!.size.toString() + " Free Add-ons")
 //                if (totalFreeItemList!!.size > 6) {
         val lessList = totalFreeItemList
         updateFreeAddonsRecycler(lessList)
