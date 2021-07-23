a09557d0ffa3f97e1bcd0df11d12ce379dd92e57
Azhar Ansari <ansari@patchinfotech.com>
2021-07-23 11:29:37 +0530

    click events handling in more settings page

--- dashboard/src/main/java/com/dashboard/controller/ui/more/MoreFragment.kt
+++ dashboard/src/main/java/com/dashboard/controller/ui/more/MoreFragment.kt
@@ -1,6 +1,5 @@
 package com.dashboard.controller.ui.more
 
-import android.graphics.Color
 import com.dashboard.R
 import com.dashboard.base.AppBaseFragment
 import com.dashboard.constant.RecyclerViewActionType
@@ -11,12 +10,16 @@ import com.dashboard.databinding.FragmentMoreBinding
 import com.dashboard.recyclerView.AppBaseRecyclerViewAdapter
 import com.dashboard.recyclerView.BaseRecyclerViewItem
 import com.dashboard.recyclerView.RecyclerItemClickListener
+import com.dashboard.utils.startAboutBoostActivity
+import com.dashboard.utils.startBusinessKycBoost
 import com.dashboard.viewmodel.DashboardViewModel
 import com.framework.extensions.observeOnce
-import com.framework.models.BaseViewModel
-import java.util.ArrayList
+import com.framework.pref.UserSessionManager
+import java.util.*
 
-class MoreFragment : AppBaseFragment<FragmentMoreBinding, DashboardViewModel>(),RecyclerItemClickListener {
+class MoreFragment : AppBaseFragment<FragmentMoreBinding, DashboardViewModel>(), RecyclerItemClickListener {
+  private var session: UserSessionManager? = null
+
   override fun getLayout(): Int {
     return R.layout.fragment_more
   }
@@ -26,28 +29,43 @@ class MoreFragment : AppBaseFragment<FragmentMoreBinding, DashboardViewModel>(),
   }
 
   override fun onCreateView() {
-   getData()
+    getData()
+    this.session = UserSessionManager(baseActivity)
   }
 
   private fun getData() {
-    viewModel?.getMoreSettings(baseActivity)?.observeOnce(viewLifecycleOwner,{
+    viewModel?.getMoreSettings(baseActivity)?.observeOnce(viewLifecycleOwner, {
       val data = it as? MoreSettingsResponse
-      binding?.rvUsefulLinks?.adapter = AppBaseRecyclerViewAdapter(baseActivity,data?.usefulLinks as ArrayList<UsefulLinksItem>)
+      binding?.rvUsefulLinks?.adapter = AppBaseRecyclerViewAdapter(baseActivity, data?.usefulLinks as ArrayList<UsefulLinksItem>)
       binding?.rvAbout?.adapter = AppBaseRecyclerViewAdapter(baseActivity, data.aboutAppSection as ArrayList<AboutAppSectionItem>)
     })
   }
 
   override fun onItemClick(position: Int, item: BaseRecyclerViewItem?, actionType: Int) {
-    when(actionType) {
+    when (actionType) {
       RecyclerViewActionType.ABOUT_VIEW_CLICK.ordinal -> {
         val aboutAppSectionItem = item as? AboutAppSectionItem
-        aboutAppSectionItem?.icon.get(position)
+        aboutAppSectionItem?.type?.let { AboutAppSectionItem.IconType.fromName(it) }?.let { clickAppActionButton(it) }
       }
       RecyclerViewActionType.USEFUL_LINKS_CLICK.ordinal -> {
         val usefulLinksItem = item as? UsefulLinksItem
+        usefulLinksItem?.type?.let { UsefulLinksItem.IconType.fromName(it) }?.let { clickUsefulButton(it) }
 
       }
     }
   }
 
+  private fun clickUsefulButton(type: UsefulLinksItem.IconType) {
+    when (type) {
+      UsefulLinksItem.IconType.business_kyc -> baseActivity.startBusinessKycBoost(session)
+    }
+  }
+
+  private fun clickAppActionButton(type: AboutAppSectionItem.IconType) {
+    when (type) {
+      AboutAppSectionItem.IconType.whats_new_version -> baseActivity.startAboutBoostActivity(session!!)
+
+    }
+
+  }
 }
