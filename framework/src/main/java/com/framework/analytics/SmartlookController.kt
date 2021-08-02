26553db993e7bedd041578cd7402ff750c8a1619
suman mondal <sum20156@gmail.com>
2021-08-02 11:29:37 +0530

    smartlook api key added

--- framework/src/main/java/com/framework/analytics/SmartlookController.kt
+++ framework/src/main/java/com/framework/analytics/SmartlookController.kt
@@ -4,7 +4,7 @@ import com.smartlook.sdk.smartlook.Smartlook
 
 object SmartlookController {
 
-    val YOUR_API_KEY=""
+    val YOUR_API_KEY="2976fda0fc620d3186eca19884bd55ac125b56e4"
     fun setupAndStartRecording(){
         Smartlook.setupAndStartRecording(YOUR_API_KEY);
 
