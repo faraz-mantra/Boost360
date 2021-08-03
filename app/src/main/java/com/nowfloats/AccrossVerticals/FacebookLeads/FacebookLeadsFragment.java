ee11a37d1072343ebfc95f10275f817e41c1a662
Azhar Ansari <ansari@patchinfotech.com>
2021-08-03 10:35:04 +0530

    replaced vmn with users primary contact number

--- app/src/main/java/com/nowfloats/AccrossVerticals/FacebookLeads/FacebookLeadsFragment.java
+++ app/src/main/java/com/nowfloats/AccrossVerticals/FacebookLeads/FacebookLeadsFragment.java
@@ -98,8 +98,8 @@ public class FacebookLeadsFragment extends Fragment {
         } else {
             intent.putExtra("email", "ria@nowfloats.com");
         }
-        if (session.getFPPrimaryContactNumber() != null) {
-            intent.putExtra("mobileNo", session.getFPPrimaryContactNumber());
+        if (session.getUserPrimaryMobile() != null) {
+            intent.putExtra("mobileNo", session.getUserPrimaryMobile());
         } else {
             intent.putExtra("mobileNo", "9160004303");
         }
--- app/src/main/java/com/nowfloats/hotel/placesnearby/PlacesNearByActivity.java
+++ app/src/main/java/com/nowfloats/hotel/placesnearby/PlacesNearByActivity.java
@@ -255,8 +255,8 @@ public class PlacesNearByActivity extends AppCompatActivity implements PlaceNear
         } else {
             intent.putExtra("email", "ria@nowfloats.com");
         }
-        if (session.getFPPrimaryContactNumber() != null) {
-            intent.putExtra("mobileNo", session.getFPPrimaryContactNumber());
+        if (session.getUserPrimaryMobile() != null) {
+            intent.putExtra("mobileNo", session.getUserPrimaryMobile());
         } else {
             intent.putExtra("mobileNo", "9160004303");
         }
