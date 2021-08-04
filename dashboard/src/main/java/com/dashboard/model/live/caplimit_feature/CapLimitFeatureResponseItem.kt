804f8193bfe42074cbab61a98cf4bdae22a4191c
Sarvare Alam <sarvare@cantech.co>
2021-08-04 15:14:26 +0530

    Cap limit Api integration

---
+++ dashboard/src/main/java/com/dashboard/model/live/caplimit_feature/CapLimitFeatureResponseItem.kt
@@ -0,0 +1,38 @@
+package com.dashboard.model.live.caplimit_feature
+
+import com.framework.base.BaseResponse
+import com.framework.utils.*
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
+const val CAP_LIMIT_PROPERTIES = "cap_limit_properties"
+
+data class CapLimitFeatureResponseItem(
+  @SerializedName("activatedDate")
+  var activatedDate: String? = null,
+  @SerializedName("createdDate")
+  var createdDate: String? = null,
+  @SerializedName("expiryDate")
+  var expiryDate: String? = null,
+  @SerializedName("featureKey")
+  var featureKey: String? = null,
+  @SerializedName("featureState")
+  var featureState: Int? = null,
+  @SerializedName("featureType")
+  var featureType: Int? = null,
+  @SerializedName("properties")
+  var properties: ArrayList<PropertiesItem>? = null
+) : BaseResponse(), Serializable {
+
+  enum class FeatureType {
+    UNLIMITED_CONTENT
+  }
+
+  fun saveCapData() {
+    PreferencesUtils.instance.saveData(CAP_LIMIT_PROPERTIES, convertObjToString(this))
+  }
+
+  fun getCapData(): CapLimitFeatureResponseItem? {
+    return convertStringToObj(PreferencesUtils.instance.getData(CAP_LIMIT_PROPERTIES, "") ?: "")
+  }
+}
