f14be5d6a629124f3a0231950b6ce61b513f370e
Sarvare Alam <sarvare@cantech.co>
2021-08-30 14:08:52 +0530

    merge and conflict resolve

--- upgrades/src/main/java/com/boost/upgrades/interfaces/HomeListener.kt
+++ upgrades/src/main/java/com/boost/upgrades/interfaces/HomeListener.kt
@@ -1,20 +1,22 @@
 package com.boost.upgrades.interfaces
 
 import android.view.View
+import android.widget.ImageView
 import com.boost.upgrades.data.api_model.GetAllFeatures.response.Bundles
+import com.boost.upgrades.data.api_model.GetAllFeatures.response.Image
 import com.boost.upgrades.data.api_model.GetAllFeatures.response.PartnerZone
 import com.boost.upgrades.data.api_model.GetAllFeatures.response.PromoBanners
 import com.boost.upgrades.data.model.FeaturesModel
 import com.boost.upgrades.data.model.YoutubeVideoModel
 
 interface HomeListener {
-  fun onPackageClicked(item: Bundles?)
-  fun onPromoBannerClicked(item: PromoBanners?)
-  fun onShowHidePromoBannerIndicator(status: Boolean)
-  fun onPartnerZoneClicked(item: PartnerZone?)
-  fun onShowHidePartnerZoneIndicator(status: Boolean)
-  fun onAddFeatureDealItemToCart(item: FeaturesModel?, minMonth: Int)
-  fun onAddonsCategoryClicked(categoryType: String)
-  fun onPlayYouTubeVideo(videoItem: YoutubeVideoModel)
-  fun onPackageAddToCart(item: Bundles?)
+    fun onPackageClicked(item: Bundles?)
+    fun onPromoBannerClicked(item: PromoBanners?)
+    fun onShowHidePromoBannerIndicator(status: Boolean)
+    fun onPartnerZoneClicked(item: PartnerZone?)
+    fun onShowHidePartnerZoneIndicator(status: Boolean)
+    fun onAddFeatureDealItemToCart(item: FeaturesModel?, minMonth: Int)
+    fun onAddonsCategoryClicked(categoryType: String)
+    fun onPlayYouTubeVideo(videoItem: YoutubeVideoModel)
+    fun onPackageAddToCart(item: Bundles?,image: ImageView)
 }
