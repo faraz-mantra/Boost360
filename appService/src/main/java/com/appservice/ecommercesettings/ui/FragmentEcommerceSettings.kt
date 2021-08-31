9bc2fb4172a2e18a56bb03ba108dcc99835fc9a7
Azhar Ansari <ansari@patchinfotech.com>
2021-08-31 10:29:01 +0530

    ecommerce local search

--- appService/src/main/java/com/appservice/ecommercesettings/ui/FragmentEcommerceSettings.kt
+++ appService/src/main/java/com/appservice/ecommercesettings/ui/FragmentEcommerceSettings.kt
@@ -1,61 +1,105 @@
 package com.appservice.ecommercesettings.ui
 
-import android.view.View
+import android.widget.SearchView
 import com.appservice.R
 import com.appservice.base.AppBaseFragment
 import com.appservice.constant.FragmentType
 import com.appservice.databinding.FragmentEcommerceSettingsBinding
+import com.appservice.model.catalog.*
+import com.appservice.recyclerView.AppBaseRecyclerViewAdapter
+import com.appservice.recyclerView.BaseRecyclerViewItem
+import com.appservice.recyclerView.RecyclerItemClickListener
 import com.appservice.ui.catalog.startFragmentActivity
-import com.framework.models.BaseViewModel
+import com.appservice.viewmodel.AppointmentSettingsViewModel
+import com.framework.extensions.observeOnce
+import java.util.*
 
-class FragmentEcommerceSettings : AppBaseFragment<FragmentEcommerceSettingsBinding, BaseViewModel>() {
-    override fun getLayout(): Int {
-        return R.layout.fragment_ecommerce_settings
+class FragmentEcommerceSettings : AppBaseFragment<FragmentEcommerceSettingsBinding, AppointmentSettingsViewModel>(), RecyclerItemClickListener {
+  private var adapter: AppBaseRecyclerViewAdapter<EcommerceTilesItem>? = null
+  private var copyList: ArrayList<EcommerceTilesItem>? = arrayListOf()
+  private var finalList: ArrayList<EcommerceTilesItem>? = arrayListOf()
+  var filteredList: ArrayList<EcommerceTilesItem>? = arrayListOf()
+  override fun getLayout(): Int {
+    return R.layout.fragment_ecommerce_settings
+  }
+
+  override fun getViewModelClass(): Class<AppointmentSettingsViewModel> {
+    return AppointmentSettingsViewModel::class.java
+  }
+
+  companion object {
+    fun newInstance(): FragmentEcommerceSettings {
+      return FragmentEcommerceSettings()
     }
+  }
 
-    override fun getViewModelClass(): Class<BaseViewModel> {
-        return BaseViewModel::class.java
-    }
+  override fun onCreateView() {
+    super.onCreateView()
+    setUpRecyclerView()
+    clearSearchFocus()
+    binding?.svSettings?.setOnQueryTextListener(object : SearchView.OnQueryTextListener, androidx.appcompat.widget.SearchView.OnQueryTextListener {
+      override fun onQueryTextSubmit(query: String?): Boolean {
+        return false
+      }
 
-    companion object {
-        fun newInstance(): FragmentEcommerceSettings {
-            return FragmentEcommerceSettings()
+      override fun onQueryTextChange(newText: String?): Boolean {
+        filterList(newText)
+        return false
+      }
+
+    })
+  }
+
+  private fun clearSearchFocus() {
+    // closes the soft keyboard when this fragment loads
+    binding?.svSettings?.clearFocus()
+  }
+
+  private fun filterList(newText: String?) {
+    filteredList?.clear()
+    if (newText?.isNotEmpty() == true) {
+      copyList?.forEach {
+        if (it.title?.toLowerCase(Locale.ROOT)?.contains(newText.toLowerCase(Locale.ROOT)) == true) {
+          filteredList?.add(it)
         }
+      }
+      adapter?.updateList(filteredList ?: arrayListOf())
+    } else {
+      adapter?.updateList(finalList ?: arrayListOf())
+
     }
+  }
 
-    override fun onCreateView() {
-        super.onCreateView()
-        setOnClickListener(binding?.catalogSetup, binding?.paymentCollectionSetup, binding?.customerInvoiceSetup, binding?.policiesForCustomer, binding?.deliverySetup)
-        clearSearchFocus()
-    }
-
-    private fun clearSearchFocus() {
-        // closes the soft keyboard when this fragment loads
-        binding?.svSettings?.clearFocus()
-    }
-
-    override fun onClick(v: View) {
-        super.onClick(v)
-        when (v) {
-            binding?.catalogSetup -> {
-                startFragmentActivity(FragmentType.ECOMMERCE_CATALOG_SETTINGS)
-            }
-            binding?.paymentCollectionSetup -> {
-                startFragmentActivity(FragmentType.ECOMMERCE_PAYMENT_SETTINGS)
-
-            }
-            binding?.customerInvoiceSetup -> {
-                startFragmentActivity(FragmentType.ECOMMERCE_FRAGMENT_CUSTOMER_INVOICE)
-
-            }
-            binding?.policiesForCustomer -> {
-                startFragmentActivity(FragmentType.ECOMMERCE_FRAGMENT_CUSTOMER_POLICIES)
-
-            }
-            binding?.deliverySetup -> {
-                startFragmentActivity(FragmentType.ECOMMERCE_DELIVERY_CONFIG)
-
-            }
+  private fun setUpRecyclerView() {
+    viewModel?.getAppointmentSettingsTiles(baseActivity.baseContext)?.observeOnce(viewLifecycleOwner, {
+      val data = it as? EcommerceCatalogTileModel
+      when (data != null) {
+        true -> {
+          this.copyList?.addAll(data.tiles!!)
+          this.finalList?.addAll(data.tiles!!)
+          this.adapter = AppBaseRecyclerViewAdapter(baseActivity, data.tiles as ArrayList<EcommerceTilesItem>, this@FragmentEcommerceSettings)
+          binding?.rvTiles?.setHasFixedSize(true)
+          binding?.rvTiles?.adapter = adapter
         }
+      }
+    })
+
+  }
+
+  override fun onItemClick(position: Int, item: BaseRecyclerViewItem?, actionType: Int) {
+    val data = item as? EcommerceTilesItem
+    data?.icon?.let { EcommerceIconType.fromName(it) }?.let { clickActionButton(it) }
+
+
+  }
+
+  private fun clickActionButton(type: EcommerceIconType) {
+    when (type) {
+      EcommerceIconType.catalog_setup -> startFragmentActivity(FragmentType.ECOMMERCE_CATALOG_SETTINGS)
+      EcommerceIconType.delivery_setup -> startFragmentActivity(FragmentType.ECOMMERCE_DELIVERY_CONFIG)
+      EcommerceIconType.customer_invoice_setup -> startFragmentActivity(FragmentType.ECOMMERCE_FRAGMENT_CUSTOMER_INVOICE)
+      EcommerceIconType.payment_collection -> startFragmentActivity(FragmentType.ECOMMERCE_PAYMENT_SETTINGS)
+      EcommerceIconType.policies -> startFragmentActivity(FragmentType.ECOMMERCE_FRAGMENT_CUSTOMER_POLICIES)
     }
+  }
 }
