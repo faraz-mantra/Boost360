d6cd5086fe1914e5314021f372880dbcfb215101
Azhar Ansari <ansari@patchinfotech.com>
2021-09-08 15:00:22 +0530

    ecommerce settings product verb

--- appService/src/main/java/com/appservice/ecommercesettings/ui/FragmentEcommerceCatalogSettings.kt
+++ appService/src/main/java/com/appservice/ecommercesettings/ui/FragmentEcommerceCatalogSettings.kt
@@ -2,42 +2,72 @@ package com.appservice.ecommercesettings.ui
 
 import android.view.View
 import com.appservice.R
+import com.appservice.appointment.model.UserFpDetailsResponse
 import com.appservice.appointment.widgets.BottomSheetCatalogDisplayName
 import com.appservice.base.AppBaseFragment
 import com.appservice.databinding.FragmentEcomCatalogSettingBinding
-import com.framework.models.BaseViewModel
+import com.appservice.viewmodel.AppointmentSettingsViewModel
+import com.framework.extensions.observeOnce
+import com.framework.pref.UserSessionManager
+import com.framework.pref.clientId
+import java.util.*
 
-class FragmentEcommerceCatalogSettings : AppBaseFragment<FragmentEcomCatalogSettingBinding, BaseViewModel>() {
-    override fun getLayout(): Int {
-        return R.layout.fragment_ecom_catalog_setting
+class FragmentEcommerceCatalogSettings : AppBaseFragment<FragmentEcomCatalogSettingBinding, AppointmentSettingsViewModel>() {
+  override fun getLayout(): Int {
+    return R.layout.fragment_ecom_catalog_setting
+  }
+
+  private var response: UserFpDetailsResponse? = null
+
+
+  override fun getViewModelClass(): Class<AppointmentSettingsViewModel> {
+    return AppointmentSettingsViewModel::class.java
+  }
+
+  companion object {
+    fun newInstance(): FragmentEcommerceCatalogSettings {
+      return FragmentEcommerceCatalogSettings()
     }
+  }
 
-    override fun getViewModelClass(): Class<BaseViewModel> {
-        return BaseViewModel::class.java
-    }
+  override fun onCreateView() {
+    super.onCreateView()
+    sessionLocal = UserSessionManager(requireActivity())
+    setOnClickListener(binding?.ctvChangeServices)
+    getProductVerb()
+  }
 
-    companion object {
-        fun newInstance(): FragmentEcommerceCatalogSettings {
-            return FragmentEcommerceCatalogSettings()
+  private fun getProductVerb() {
+      getFpDetails()
+  }
+
+  private fun getFpDetails() {
+    showProgress()
+    val map = HashMap<String, String>()
+    map["clientId"] = clientId
+    viewModel?.getFpDetails(sessionLocal?.fPID ?: "", map)
+      ?.observeOnce(viewLifecycleOwner, {
+        hideProgress()
+        this.response = it as? UserFpDetailsResponse
+        if (it.isSuccess() && response != null) {
+          binding?.ctvProductVerb?.text = response?.productCategoryVerb
+          binding?.ctvProductVerbUrl?.text = "${sessionLocal.rootAliasURI}/${response?.productCategoryVerb}"
+        } else {
         }
-    }
+      })
+  }
 
-    override fun onCreateView() {
-        super.onCreateView()
-        setOnClickListener(binding?.ctvChangeServices)
+  override fun onClick(v: View) {
+    super.onClick(v)
+    when (v) {
+      binding?.ctvChangeServices -> {
+        showCatalogDisplayName()
+      }
     }
+  }
 
-    override fun onClick(v: View) {
-        super.onClick(v)
-        when (v) {
-            binding?.ctvChangeServices -> {
-                showCatalogDisplayName()
-            }
-        }
-    }
-
-    private fun showCatalogDisplayName() {
-        val bottomSheetCatalogDisplayName = BottomSheetCatalogDisplayName()
-        bottomSheetCatalogDisplayName.show(this.parentFragmentManager, BottomSheetCatalogDisplayName::class.java.name)
-    }
+  private fun showCatalogDisplayName() {
+    val bottomSheetCatalogDisplayName = BottomSheetCatalogDisplayName()
+    bottomSheetCatalogDisplayName.show(this.parentFragmentManager, BottomSheetCatalogDisplayName::class.java.name)
+  }
 }
