c27f10d4ddbccf2259ab43fae16b69fc919e5e1c
Azhar Ansari <ansari@patchinfotech.com>
2021-09-10 16:47:13 +0530

    product listing logic changes

--- appService/src/main/java/com/appservice/holder/ProductListingViewHolder.kt
+++ appService/src/main/java/com/appservice/holder/ProductListingViewHolder.kt
@@ -1,5 +1,6 @@
 package com.appservice.holder
 
+import android.graphics.Color
 import android.graphics.Paint
 import androidx.core.content.ContextCompat
 import com.appservice.R
@@ -13,28 +14,34 @@ import com.framework.extensions.visible
 import com.framework.glide.util.glideLoad
 
 class ProductListingViewHolder(binding: RecyclerItemProductListingBinding) : AppBaseRecyclerViewHolder<RecyclerItemProductListingBinding>(binding) {
-    override fun bind(position: Int, item: BaseRecyclerViewItem) {
-        super.bind(position, item)
-        val data = (item as? CatalogProduct) ?: return
-        binding.labelName.text = data.brandName
-        binding.labelCategory.text = data.category
-        when {
-          data.availableUnits == -1 -> {binding.ctvStock.text="Stock"}
-          data.availableUnits > 0 -> { binding.ctvStock.setCompoundDrawables(null,null,ContextCompat.getDrawable(binding.root.context,R.drawable.ic_dot_green),null);
-              binding.ctvStock.text="${data.availableUnits} In stock"}
-          else -> {
-              binding.ctvStock.setCompoundDrawables(null,null,ContextCompat.getDrawable(binding.root.context,R.drawable.ic_dot_red),null)
-              binding.ctvStock.text = "Out of stock" }
-        }
-        binding.ctvStock.text = "${data.availableUnits ?: 0}"
-        if (data.Price ?: 0.0 <= data.DiscountAmount ?: 0.0) binding.labelBasePrice.gone() else binding.labelBasePrice.visible()
-        binding.labelPrice.text = "${data.CurrencyCode ?: "INR"} ${data.DiscountAmount}"
-        binding.labelBasePrice.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
-        binding.labelBasePrice.text = "${data.CurrencyCode ?: "INR"} ${data.Price}"
-        binding.labelDescription.text = "${data.Description}"
-        apply { activity?.glideLoad(binding.cardThumbnail, data.TileImageUri, R.drawable.placeholder_image_n) }
-        binding.root.setOnClickListener { listener?.onItemClick(position, data, RecyclerViewActionType.PRODUCT_ITEM_CLICK.ordinal) }
-        binding.shareData.setOnClickListener { listener?.onItemClick(position, data, RecyclerViewActionType.PRODUCT_DATA_SHARE_CLICK.ordinal) }
-        binding.shareWhatsapp.setOnClickListener { listener?.onItemClick(position, data, RecyclerViewActionType.PRODUCT_WHATS_APP_SHARE.ordinal) }
+  override fun bind(position: Int, item: BaseRecyclerViewItem) {
+    super.bind(position, item)
+    val data = (item as? CatalogProduct) ?: return
+    binding.labelName.text = data.brandName
+    binding.labelCategory.text = data.category
+    when {
+      data.availableUnits == -1 -> {
+        binding.ctvStock.text = activity?.getString(R.string.stock); binding?.ctvStock.setTextColor(getColor(R.color.secondary_text)!!)
+      }
+      data.availableUnits > 0 -> {
+        binding.ctvStock.setCompoundDrawablesWithIntrinsicBounds(null, null, ContextCompat.getDrawable(binding.root.context, R.drawable.ic_dot_green), null);
+        binding.ctvStock.text = "${data.availableUnits} In stock"
+        binding?.ctvStock.setTextColor(getColor(R.color.secondary_text)!!)
+      }
+      else -> {
+        binding.ctvStock.setCompoundDrawablesWithIntrinsicBounds(null, null, ContextCompat.getDrawable(binding.root.context, R.drawable.ic_dot_red), null)
+        binding?.ctvStock.setTextColor(Color.RED)
+        binding.ctvStock.text = activity?.getString(R.string.out_of_stock)
+      }
     }
+    if (data.Price ?: 0.0 <= data.DiscountAmount ?: 0.0) binding.labelBasePrice.gone() else binding.labelBasePrice.visible()
+    binding.labelPrice.text = "${data.CurrencyCode ?: "INR"} ${data.DiscountAmount}"
+    binding.labelBasePrice.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
+    binding.labelBasePrice.text = "${data.CurrencyCode ?: "INR"} ${data.Price}"
+    binding.labelDescription.text = "${data.Description}"
+    apply { activity?.glideLoad(binding.cardThumbnail, data.TileImageUri, R.drawable.placeholder_image_n) }
+    binding.root.setOnClickListener { listener?.onItemClick(position, data, RecyclerViewActionType.PRODUCT_ITEM_CLICK.ordinal) }
+    binding.shareData.setOnClickListener { listener?.onItemClick(position, data, RecyclerViewActionType.PRODUCT_DATA_SHARE_CLICK.ordinal) }
+    binding.shareWhatsapp.setOnClickListener { listener?.onItemClick(position, data, RecyclerViewActionType.PRODUCT_WHATS_APP_SHARE.ordinal) }
+  }
 }
