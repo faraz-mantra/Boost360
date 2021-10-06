7e64c015bfff8e40a4df697e45551f574fcb76af
shubhamgupta <shubham.gupta@cantech.co>
2021-10-07 01:05:48 +0530

    search & create flow create with API implementation

--- appService/src/main/java/com/appservice/ui/domainbooking/SearchDomainActivity.kt
+++ appService/src/main/java/com/appservice/ui/domainbooking/SearchDomainActivity.kt
@@ -1,9 +1,11 @@
 package com.appservice.ui.domainbooking
 
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.util.Log
+import android.text.Html
+import android.view.KeyEvent
+import android.view.inputmethod.EditorInfo
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.appservice.R
@@ -14,13 +16,16 @@ import com.appservice.recyclerView.AppBaseRecyclerViewAdapter
 import com.appservice.recyclerView.BaseRecyclerViewItem
 import com.appservice.recyclerView.RecyclerItemClickListener
 import com.appservice.ui.domainbooking.model.SimilarDomainSuggestionModel
+import com.appservice.utils.Validations
+import com.appservice.utils.getDomainSplitValues
 import com.appservice.viewmodel.SearchDomainViewModel
-import com.framework.base.BaseActivity
 import com.framework.extensions.afterTextChanged
+import com.framework.extensions.gone
 import com.framework.extensions.observeOnce
+import com.framework.extensions.visible
 import com.framework.pref.clientId
+import com.framework.pref.getDomainName
 import com.framework.utils.showKeyBoard
-import com.framework.webengageconstant.LOGGED_IN
 import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class SearchDomainActivity : AppBaseActivity<ActivitySearchDomainBinding, SearchDomainViewModel>(),
@@ -44,29 +49,61 @@ class SearchDomainActivity : AppBaseActivity<ActivitySearchDomainBinding, Search
         }
 
         binding?.edSearchBox?.afterTextChanged {
-            //setRecyclerData()
-            searchDomain()
+            binding?.layputEtError?.gone()
         }
+
+        binding?.edSearchBox?.setOnEditorActionListener(object : TextView.OnEditorActionListener {
+            override fun onEditorAction(v: TextView?, actionId: Int, event: KeyEvent?): Boolean {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    if (Validations.isDomainValid(binding?.edSearchBox?.text.toString())) {
+                        searchDomain(binding?.edSearchBox?.text.toString())
+                        return true
+                    } else {
+                        showShortToast(getString(R.string.error_wrong_domain_entered))
+                    }
+                }
+                return false
+            }
+
+        })
 
         this.showKeyBoard(binding?.edSearchBox)
     }
 
-    private fun searchDomain(){
-        viewModel.searchDomain("SURESH", clientId, ".COM").observeOnce(lifecycleOwner = this, {
+    private fun searchDomain(domainString: String) {
+        val splitDomain = getDomainSplitValues(domainString)
+        viewModel.searchDomain(splitDomain.domainName.lowercase(), clientId, splitDomain.domainExtension.uppercase()).observeOnce(lifecycleOwner = this, {
             if (!it.isSuccess() || it == null) {
                 showShortToast(getString(R.string.something_went_wrong))
                 return@observeOnce
             }
 
             val isAvailable = it.anyResponse as Boolean
-            showShortToast(if (isAvailable) "Yes available!" else "Not available!")
+            binding?.layputEtError?.visible()
 
+            val color: Int
+            val stringAvailability: Int
+            if (isAvailable) {
+                color = R.color.green_dark
+                stringAvailability = R.string.is_available_new
+                binding?.btnContinue?.visible()
+            } else {
+                color = R.color.red_dark
+                stringAvailability = R.string.is_not_available
+                binding?.btnContinue?.gone()
+            }
+            binding?.tvIsDomainAvailable?.text = "$domainString "
+            binding?.tvIsDomainAvailable?.setTextColor(ContextCompat.getColor(this, color))
+            binding?.tvAvailabilitySuffix?.text = getString(stringAvailability)
         })
-        /*viewModel.createDomain(clientId, "SURESH", ".IN",
+    }
+
+    private fun createDomain(domainSelected: String, domainType: String) {
+        viewModel.createDomain(
+            clientId, domainSelected.lowercase(), domainType.uppercase(),
             session.fpTag!!, "1", 0
-        ).observeOnce(lifecycleOwner =  this, {
-            Log.i("djkne", it.toString())
-        })*/
+        ).observeOnce(lifecycleOwner = this, {
+        })
     }
 
     private fun setRecyclerData() {
@@ -96,6 +133,15 @@ class SearchDomainActivity : AppBaseActivity<ActivitySearchDomainBinding, Search
         )
         bSheet.setContentView(sheetBinding.root)
         bSheet.setCancelable(false)
+        val enteredDomainName = binding?.edSearchBox?.text.toString()
+
+        sheetBinding.tvWbAddress.text = enteredDomainName
+        sheetBinding.customTextView4.text = Html.fromHtml(
+            "${getString(R.string.domain_confirm_msg_1)} " +
+                    "<u>${session.getDomainName(true)}</u>. " +
+                    "${getString(R.string.domain_confirm_msg_2)}"
+        )
+
         sheetBinding.btnConfirm.setOnClickListener {
             startFragmentDomainBookingActivity(
                 activity = this,
@@ -111,6 +157,11 @@ class SearchDomainActivity : AppBaseActivity<ActivitySearchDomainBinding, Search
 
         sheetBinding.ivClose.setOnClickListener {
             bSheet.dismiss()
+        }
+
+        sheetBinding.btnConfirm.setOnClickListener {
+            val domainSplit = getDomainSplitValues(enteredDomainName)
+            createDomain(domainSplit.domainName, domainSplit.domainExtension)
         }
 
         bSheet.show()
