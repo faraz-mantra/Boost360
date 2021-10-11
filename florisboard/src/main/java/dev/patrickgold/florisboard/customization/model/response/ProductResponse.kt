b291c1eb5404eaea10c0c4a750aee0715026549d
Sarvare Alam <sarvare@cantech.co>
2021-10-11 17:37:38 +0530

    Boost360-Android build: Product sliding is in the loop

--- florisboard/src/main/java/dev/patrickgold/florisboard/customization/model/response/ProductResponse.kt
+++ florisboard/src/main/java/dev/patrickgold/florisboard/customization/model/response/ProductResponse.kt
@@ -1,7 +1,8 @@
 package dev.patrickgold.florisboard.customization.model.response
 
+import java.io.Serializable
+
 class ProductResponse(
     val Products:List<Product>?=null,
     val TotalCount:Int?=null
-) {
-}
+):Serializable
