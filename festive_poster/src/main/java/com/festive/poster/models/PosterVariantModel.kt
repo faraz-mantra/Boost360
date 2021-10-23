4cdc9cb8e25df41c85418f81822efee3bdf7ee97
Shyam Gopalsamy <shyam.gopalsamy@nowfloats.com>
2021-10-24 02:27:50 +0530

    Bug Fixes

--- festive_poster/src/main/java/com/festive/poster/models/PosterVariantModel.kt
+++ festive_poster/src/main/java/com/festive/poster/models/PosterVariantModel.kt
@@ -1,7 +1,12 @@
 package com.festive.poster.models
 
+import com.google.gson.annotations.SerializedName
+
 data class PosterVariantModel(
+    @SerializedName("active")
     val active: Boolean,
+    @SerializedName("svgUrl")
     val svgUrl: String,
+    @SerializedName("name")
     val name: String
 )
--- festive_poster/src/main/java/com/festive/poster/models/response/GetTemplatesResponse.kt
+++ festive_poster/src/main/java/com/festive/poster/models/response/GetTemplatesResponse.kt
@@ -2,7 +2,9 @@ package com.festive.poster.models.response
 
 import com.festive.poster.models.GetTemplatesResult
 import com.framework.base.BaseResponse
+import com.google.gson.annotations.SerializedName
 
 data class GetTemplatesResponse(
+    @SerializedName("Result")
     val Result: GetTemplatesResult,
 ):BaseResponse()
