940f11db4d63d0c2e03ff7d088bd30d11a0658e5
shubhamgupta <shubham.gupta@cantech.co>
2021-10-23 22:20:05 +0530

    domain validation and some optimizations done

--- appService/src/main/java/com/appservice/utils/RegularExpressions.kt
+++ appService/src/main/java/com/appservice/utils/RegularExpressions.kt
@@ -1,5 +1,3 @@
 package com.appservice.utils
 
-const val DOMAIN_FORMAT = ("^((?!-)[A-Za-z0-9-]"
-        + "{1,63}(?<!-)\\.)"
-        + "+[A-Za-z]{2,6}")
+const val DOMAIN_FORMAT = ("^((?!-)[A-Za-z0-9-]{1,63}(?<!-)\\.)+[A-Za-z]{2,6}")
