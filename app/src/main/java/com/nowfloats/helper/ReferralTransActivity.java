8a2f71ed14f1c0d34b85f62144ec364c2030e04b
Sarvare Alam <sarvare@cantech.co>
2021-10-28 14:37:26 +0530

    Some dependency update and Log add invite referral

--- app/src/main/java/com/nowfloats/helper/ReferralTransActivity.java
+++ app/src/main/java/com/nowfloats/helper/ReferralTransActivity.java
@@ -18,52 +18,54 @@ import org.json.JSONException;
 import static com.nowfloats.util.Constants.REFERRAL_CAMPAIGN_CODE;
 
 public class ReferralTransActivity extends AppCompatActivity {
-    UserSessionManager session;
+  private UserSessionManager session;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        session = new UserSessionManager(getApplicationContext(), ReferralTransActivity.this);
-        if (!TextUtils.isEmpty(session.getFPEmail())) {
-            InviteReferralsApi.getInstance(getApplicationContext()).userDetails(
-                    session.getUserProfileName(),
-                    session.getFPEmail(),
-                    session.getUserPrimaryMobile(),
-                    REFERRAL_CAMPAIGN_CODE, null, null
-            );
-            inviteReferralLogin();
-        } else if (!TextUtils.isEmpty(session.getUserProfileEmail())) {
-            InviteReferralsApi.getInstance(getApplicationContext()).userDetails(
-                    session.getUserProfileName(),
-                    session.getUserProfileEmail(),
-                    session.getUserPrimaryMobile(),
-                    REFERRAL_CAMPAIGN_CODE, null, null
-            );
-            inviteReferralLogin();
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    session = new UserSessionManager(getApplicationContext(), ReferralTransActivity.this);
+    if (!TextUtils.isEmpty(session.getFPEmail())) {
+      InviteReferralsApi.getInstance(getApplicationContext()).userDetails(
+          session.getUserProfileName(),
+          session.getFPEmail(),
+          session.getUserPrimaryMobile(),
+          REFERRAL_CAMPAIGN_CODE, null, null
+      );
+      Log.d("ReferralTransActivity", "Email: " + session.getFPEmail() + "Number: " + session.getUserPrimaryMobile());
+      inviteReferralLogin();
+    } else if (!TextUtils.isEmpty(session.getUserProfileEmail())) {
+      InviteReferralsApi.getInstance(getApplicationContext()).userDetails(
+          session.getUserProfileName(),
+          session.getUserProfileEmail(),
+          session.getUserPrimaryMobile(),
+          REFERRAL_CAMPAIGN_CODE, null, null
+      );
+      Log.d("ReferralTransActivity", "Email: " + session.getUserProfileEmail() + "Number: " + session.getUserPrimaryMobile());
+      inviteReferralLogin();
+    } else {
+      Toast.makeText(getApplicationContext(), R.string.an_unexpacted_error, Toast.LENGTH_LONG).show();
+    }
+    finish();
+  }
+
+  private void inviteReferralLogin() {
+    InviteReferralsApi.getInstance(getApplicationContext()).userDetailListener(jsonObject -> {
+      Log.d("Referral Details", jsonObject.toString());
+      try {
+        String status = jsonObject.get("Authentication").toString();
+        if (status.toLowerCase().equals("success")) {
+          InviteReferralsApi.getInstance(getApplicationContext()).inline_btn(REFERRAL_CAMPAIGN_CODE);
         } else {
-            Toast.makeText(getApplicationContext(), R.string.an_unexpacted_error, Toast.LENGTH_LONG).show();
+          Toast.makeText(getApplicationContext(), getString(R.string.auth_failed_try_again), Toast.LENGTH_SHORT).show();
         }
-        finish();
-    }
+      } catch (JSONException e) {
+        Toast.makeText(getApplicationContext(), getString(R.string.auth_failed_try_again), Toast.LENGTH_SHORT).show();
+      }
+    });
+  }
 
-    private void inviteReferralLogin() {
-        InviteReferralsApi.getInstance(getApplicationContext()).userDetailListener(jsonObject -> {
-            Log.d("Referral Details", jsonObject.toString());
-            try {
-                String status = jsonObject.get("Authentication").toString();
-                if (status.toLowerCase().equals("success")) {
-                    InviteReferralsApi.getInstance(getApplicationContext()).inline_btn(REFERRAL_CAMPAIGN_CODE);
-                } else {
-                    Toast.makeText(getApplicationContext(), getString(R.string.auth_failed_try_again), Toast.LENGTH_SHORT).show();
-                }
-            } catch (JSONException e) {
-                Toast.makeText(getApplicationContext(), getString(R.string.auth_failed_try_again), Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-    }
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+  }
 }
