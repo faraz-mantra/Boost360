6b143a851daa85a4cf7a4b2c3269c0875957eeed
shubhamgupta <shubham.gupta@cantech.co>
2021-11-12 23:43:16 +0530

    AppZeroCases supportr applied in catalogue, testimonial, custom page, image gallery,etc. Testing done. Help menu added

--- onboarding/src/main/java/com/onboarding/nowfloats/constant/SupportVideoType.kt
+++ onboarding/src/main/java/com/onboarding/nowfloats/constant/SupportVideoType.kt
@@ -1,14 +1,14 @@
 package com.onboarding.nowfloats.constant
 
 enum class SupportVideoType (val value :String) {
-    CUSTOM_PAGES("feature.CUSTOMPAGES"),   //Custom Pages
-    GALLERY_IMAGE("feature.GALLERYIMAGE"), //Done
+    CUSTOM_PAGES("feature.CUSTOMPAGES"),
+    GALLERY_IMAGE("feature.GALLERYIMAGE"),
     LATEST_UPDATES("feature.LATESTUPDATES"), //Pending
     PAYMENT_GATEWAY("feature.PAYMENTGATEWAY"), //hold, doubts
     ANALYTICS("feature.ANALYTICS"), //hold, doubts
     TOB("feature.TOB"),             //hold, doubts
-    TESTIMONIALS("feature.TESTIMONIALS"), //Done
-    PRODUCT_CATALOGUE("feature.PRODUCTCATALOGUE"),  //Product & service both
+    TESTIMONIALS("feature.TESTIMONIALS"),
+    PRODUCT_CATALOGUE("feature.PRODUCTCATALOGUE"),
     FEATURED_IMAGE("feature.FEATUREDIMAGE"),
     CONTACT_DETAILS("feature.CONTACTDETAILS")  //hold, doubts
 }
