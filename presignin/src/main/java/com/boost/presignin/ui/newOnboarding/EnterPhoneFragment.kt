d15e41a8edbd871ac55ef11a7b750e00be9decc1
suman <sum20156@gmail.com>
2021-11-18 16:20:51 +0530

    otp verify and sending api added

--- presignin/src/main/java/com/boost/presignin/ui/newOnboarding/EnterPhoneFragment.kt
+++ presignin/src/main/java/com/boost/presignin/ui/newOnboarding/EnterPhoneFragment.kt
@@ -1,8 +1,15 @@
 package com.boost.presignin.ui.newOnboarding
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.Paint
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.*
+import android.widget.ImageView
+import androidx.activity.OnBackPressedCallback
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import com.appservice.base.AppBaseFragment
 import com.appservice.constant.FragmentType
@@ -10,22 +17,35 @@ import com.appservice.databinding.FragmentBookADomainSslBinding
 import com.appservice.ui.catalog.startFragmentActivity
 import com.appservice.ui.domainbooking.BookDomainSslFragment
 import com.boost.presignin.R
+import com.boost.presignin.constant.IntentConstant
 import com.boost.presignin.databinding.FragmentEnterPhoneBinding
 import com.boost.presignin.dialog.WebViewDialog
+import com.boost.presignin.extensions.isPhoneValid
 import com.boost.presignin.helper.WebEngageController
+import com.boost.presignin.ui.login.LoginActivity
+import com.boost.presignin.ui.mobileVerification.MobileVerificationActivity
+import com.boost.presignin.ui.mobileVerification.OTP_FRAGMENT
+import com.boost.presignin.viewmodel.LoginSignUpViewModel
+import com.framework.analytics.SentryController
+import com.framework.base.FRAGMENT_TYPE
 import com.framework.extensions.afterTextChanged
+import com.framework.extensions.observeOnce
+import com.framework.extensions.onTextChanged
 import com.framework.models.BaseViewModel
+import com.framework.pref.clientId
 import com.framework.utils.fromHtml
+import com.framework.utils.hideKeyBoard
 import com.framework.utils.makeLinks
-import com.framework.webengageconstant.BOOST_360_CONDITIONS_CLICK
-import com.framework.webengageconstant.BOOST_360_TERMS_CLICK
-import com.framework.webengageconstant.CLICKED
-import com.framework.webengageconstant.NO_EVENT_VALUE
+import com.framework.webengageconstant.*
+import com.google.android.gms.auth.api.credentials.Credential
+import com.google.android.gms.auth.api.credentials.Credentials
+import com.google.android.gms.auth.api.credentials.HintRequest
 
-class EnterPhoneFragment : AppBaseFragment<FragmentEnterPhoneBinding, BaseViewModel>() {
+class EnterPhoneFragment : AppBaseFragment<FragmentEnterPhoneBinding, LoginSignUpViewModel>() {
 
     private val urlTermsAndCond = "https://www.getboost360.com/tnc?src=android&stage=presignup"
-
+    val NUMBER_PICKER_RC=100
+    private val TAG = "EnterPhoneFragment"
     companion object {
         @JvmStatic
         fun newInstance(bundle: Bundle? = null): EnterPhoneFragment {
@@ -39,14 +59,87 @@ class EnterPhoneFragment : AppBaseFragment<FragmentEnterPhoneBinding, BaseViewMo
         return R.layout.fragment_enter_phone
     }
 
-    override fun getViewModelClass(): Class<BaseViewModel> {
-        return BaseViewModel::class.java
+    override fun getViewModelClass(): Class<LoginSignUpViewModel> {
+        return LoginSignUpViewModel::class.java
     }
 
     override fun onCreateView() {
         setOnListeners()
         initUI()
+
+        requestPhonePicker()
+        WebEngageController.trackEvent(PS_LOGIN_NUMBER_PAGE_LOAD, PAGE_VIEW, NO_EVENT_VALUE)
+
+        setOnClickListener(binding?.tvRequestOtp)
+
+
+
     }
+
+    override fun onClick(v: View) {
+        super.onClick(v)
+        when(v){
+            binding?.tvRequestOtp->{
+                sendOtp(binding?.phoneEt?.text.toString())
+
+            }
+        }
+    }
+
+
+    private fun sendOtp(phoneNumber: String?) {
+        WebEngageController.trackEvent(PS_LOGIN_NUMBER_CLICK, NEXT_CLICK, NO_EVENT_VALUE)
+        baseActivity.hideKeyBoard()
+        showProgress(getString(R.string.sending_otp))
+        viewModel?.sendOtpIndia(phoneNumber?.toLong(), clientId)?.observeOnce(viewLifecycleOwner, {
+            if (it.isSuccess() && it.parseResponse()) {
+                startFragmentFromNewOnBoardingActivity(
+                    activity = requireActivity(),
+                    type = com.boost.presignin.constant.FragmentType.VERIFY_PHONE_FRAGMENT,
+                    bundle = Bundle().apply {
+                                            putString(IntentConstant.EXTRA_PHONE_NUMBER.name,phoneNumber)
+                    },
+                    clearTop = false
+                )
+            } else showShortToast(getString(R.string.otp_not_sent))
+            hideProgress()
+        })
+    }
+
+
+
+        private fun requestPhonePicker() {
+            val hintRequest = HintRequest.Builder()
+                .setPhoneNumberIdentifierSupported(true)
+                .build()
+            val credentialsClient = Credentials.getClient(requireActivity())
+            val intent = credentialsClient.getHintPickerIntent(hintRequest)
+            try {
+                startIntentSenderForResult(intent.intentSender, NUMBER_PICKER_RC, null, 0, 0, 0,null)
+            } catch (e: Exception) {
+                SentryController.captureException(e)
+
+                e.printStackTrace()
+            }
+        }
+
+        override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+            super.onActivityResult(requestCode, resultCode, data)
+            if (requestCode === NUMBER_PICKER_RC) {
+                if (resultCode === Activity.RESULT_OK) {
+                    val cred = data?.getParcelableExtra<Credential>(Credential.EXTRA_KEY)
+                    Log.i(TAG, "onActivityResult: "+cred?.id)
+                    binding?.phoneEt?.setText(cred?.id.toString().replace("+91",""))
+                    val isPhoneValid = binding?.phoneEt?.text.toString().isPhoneValid()
+                    binding?.tvRequestOtp?.isEnabled = isPhoneValid
+
+                    if (isPhoneValid){
+                        binding?.tvRequestOtp?.performClick()
+                    }
+                }
+            }
+        }
+
 
     private fun initUI() {
         initTncString()
@@ -54,17 +147,9 @@ class EnterPhoneFragment : AppBaseFragment<FragmentEnterPhoneBinding, BaseViewMo
 
     private fun setOnListeners() {
         binding?.phoneEt?.afterTextChanged {
-            binding?.tvRequestOtp?.isEnabled = !it.isNullOrEmpty()
+            binding?.tvRequestOtp?.isEnabled = it.isPhoneValid()
         }
 
-        binding?.tvRequestOtp?.setOnClickListener {
-            startFragmentFromNewOnBoardingActivity(
-                activity = requireActivity(),
-                type = com.boost.presignin.constant.FragmentType.VERIFY_PHONE_FRAGMENT,
-                bundle = Bundle(),
-                clearTop = false
-            )
-        }
     }
 
     private fun initTncString() {
@@ -89,11 +174,10 @@ class EnterPhoneFragment : AppBaseFragment<FragmentEnterPhoneBinding, BaseViewMo
 
     private fun openTNCDialog(url: String, title: String) {
         showShortToast(getString(R.string.coming_soon))
-       /* WebViewDialog().apply {
-            setData(false, url, title)
-            onClickType = { }
-            show(activity.supportFragmentManager, title)
-        }*/
+        val webViewDialog = WebViewDialog()
+        webViewDialog.setData(false, url, title)
+        webViewDialog.onClickType={}
+        webViewDialog.show(requireActivity().supportFragmentManager, title)
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
--- presignin/src/main/java/com/boost/presignin/ui/newOnboarding/VerifyPhoneFragment.kt
+++ presignin/src/main/java/com/boost/presignin/ui/newOnboarding/VerifyPhoneFragment.kt
@@ -1,19 +1,66 @@
 package com.boost.presignin.ui.newOnboarding
 
 import android.os.Bundle
+import android.os.Handler
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
 import android.view.*
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.boost.presignin.R
 import com.boost.presignin.base.AppBaseFragment
+import com.boost.presignin.constant.IntentConstant
 import com.boost.presignin.databinding.FragmentSetupMyWebsiteBinding
 import com.boost.presignin.databinding.FragmentVerifyPhoneBinding
+import com.boost.presignin.helper.WebEngageController
+import com.boost.presignin.model.authToken.AuthTokenDataItem
+import com.boost.presignin.model.login.VerificationRequestResult
+import com.boost.presignin.model.login.VerifyOtpResponse
+import com.boost.presignin.ui.mobileVerification.AuthBaseFragment
+import com.boost.presignin.ui.mobileVerification.FP_LIST_FRAGMENT
+import com.boost.presignin.ui.mobileVerification.MobileVerificationActivity
+import com.boost.presignin.ui.registration.RegistrationActivity
 import com.boost.presignin.views.otptextview.OTPListener
+import com.framework.base.FRAGMENT_TYPE
 import com.framework.extensions.gone
+import com.framework.extensions.observeOnce
 import com.framework.extensions.visible
 import com.framework.models.BaseViewModel
+import com.framework.pref.UserSessionManager
+import com.framework.pref.clientId
+import com.framework.pref.clientId2
+import com.framework.smsVerification.SMSReceiver
+import com.framework.smsVerification.SmsManager
+import com.framework.webengageconstant.*
 
-class VerifyPhoneFragment : AppBaseFragment<FragmentVerifyPhoneBinding, BaseViewModel>() {
+class VerifyPhoneFragment : AuthBaseFragment<FragmentVerifyPhoneBinding>(), SMSReceiver.OTPReceiveListener {
 
+    private var isCounterRunning = false
+
+    private lateinit var countDown: com.boost.presignin.timer.CountDownTimer
+
+    private var resultLogin: VerificationRequestResult? = null
+
+    private val phoneNumber by lazy {
+        arguments?.getString(IntentConstant.EXTRA_PHONE_NUMBER.name)
+    }
+
+
+    override fun resultLogin(): VerificationRequestResult? {
+        return resultLogin
+    }
+
+    override fun authTokenData(): AuthTokenDataItem? {
+        return if (resultLogin()?.authTokens.isNullOrEmpty()
+                .not()
+        ) resultLogin()?.authTokens!![0] else null
+    }
     companion object {
         @JvmStatic
         fun newInstance(bundle: Bundle? = null): VerifyPhoneFragment {
@@ -27,17 +74,48 @@ class VerifyPhoneFragment : AppBaseFragment<FragmentVerifyPhoneBinding, BaseView
         return R.layout.fragment_verify_phone
     }
 
-    override fun getViewModelClass(): Class<BaseViewModel> {
-       return BaseViewModel::class.java
-    }
 
     override fun onCreateView() {
+        this.session = UserSessionManager(baseActivity)
         setOnListeners()
         initUI()
+        setOnClickListener(binding?.tvVerifyOtp,binding?.tvResendOtpIn)
+        WebEngageController.trackEvent(PS_VERIFY_OTP_PAGE_LOAD, PAGE_VIEW, NO_EVENT_VALUE)
+        binding?.tvPhoneNumber?.text = phoneNumber.toString()
+
+
+        Handler().postDelayed({ onCodeSent() }, 500)
+
+
+        // init SMS Manager
+        SmsManager.initManager(activity?.baseContext!!, this);
     }
 
+    override fun onClick(v: View) {
+        super.onClick(v)
+        when(v){
+            binding?.tvVerifyOtp->{
+                verify()
+            }
+            binding?.tvResendOtpIn->{
+                binding?.pinOtpVerify?.setOTP("")
+                sendOtp(phoneNumber)
+            }
+        }
+    }
     private fun initUI() {
 
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (this::countDown.isInitialized) countDown.resume()
+        SmsManager.register()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        SmsManager.unregister()
     }
 
     private fun setOnListeners() {
@@ -51,6 +129,7 @@ class VerifyPhoneFragment : AppBaseFragment<FragmentVerifyPhoneBinding, BaseView
             }
 
             override fun onOTPComplete(otp: String) {
+                verify()
                 binding?.linearWhatsApp?.visible()
             }
         }
@@ -62,6 +141,14 @@ class VerifyPhoneFragment : AppBaseFragment<FragmentVerifyPhoneBinding, BaseView
                 bundle = Bundle(),
                 clearTop = false
             )
+        }
+
+        binding?.chkWhatsapp?.setOnCheckedChangeListener { compoundButton, b ->
+            if (b){
+                WebEngageController.trackEvent(Ps_Login_WhatsApp_Consent_click, CLICK, NO_EVENT_VALUE)
+            }else{
+                WebEngageController.trackEvent(Ps_Login_WhatsApp_Consent_not_clicked,CLICK, NO_EVENT_VALUE)
+            }
         }
     }
 
@@ -79,4 +166,107 @@ class VerifyPhoneFragment : AppBaseFragment<FragmentVerifyPhoneBinding, BaseView
             else -> super.onOptionsItemSelected(item)
         }
     }
+
+    private fun onCodeSent() {
+        binding?.tvResendOtpIn?.setTextColor(getColor(R.color.gray_DADADA))
+        countDown = object : com.boost.presignin.timer.CountDownTimer(30 * 1000, 1000) {
+            override fun onTick(p0: Long) {
+                binding?.tvResendOtpIn?.isEnabled =false
+
+                val resendIn = getString(R.string.psn_resend_in);
+                binding?.tvResendOtpIn?.text = String.format(resendIn, (p0 / 1000).toInt());
+            }
+
+            override fun onFinish() {
+                binding?.tvResendOtpIn?.isEnabled =true
+                isCounterRunning = false
+                val clickableSpan = object : ClickableSpan() {
+                    override fun updateDrawState(ds: TextPaint) {
+                        super.updateDrawState(ds)
+                        ds.isUnderlineText = true
+                        ds.color = ContextCompat.getColor(requireContext(), R.color.colorAccent)
+                        ds.linkColor = ContextCompat.getColor(requireContext(), R.color.colorAccent)
+                    }
+
+                    override fun onClick(widget: View) {
+                        view?.invalidate()
+
+                    }
+                }
+                val resendString = getString(R.string.psn_resend_hint)
+                binding?.tvResendOtpIn?.text = resendString
+                binding?.tvResendOtpIn?.setTextColor(getColor(R.color.colorAccent))
+            }
+        }
+        startOperation()
+    }
+
+    private fun startOperation() {
+        if (!isCounterRunning) {
+            isCounterRunning = true
+            countDown.start()
+        } else {
+            countDown.cancel() // cancel
+            countDown.start() // then restart
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (this::countDown.isInitialized) countDown.pause()
+    }
+
+    private fun sendOtp(phoneNumber: String?) {
+        showProgress(getString(R.string.sending_otp))
+        WebEngageController.trackEvent(PS_VERIFY_OTP_RESEND, OTP_RESEND_CLICK, NO_EVENT_VALUE)
+        viewModel?.sendOtpIndia(phoneNumber?.toLong(), clientId)?.observeOnce(viewLifecycleOwner, {
+            if (it.isSuccess() && it.parseResponse()) {
+                onCodeSent()
+            } else showShortToast(getString(R.string.otp_not_sent))
+            hideProgress()
+        })
+    }
+
+    fun verify() {
+        showProgress(getString(R.string.verify_otp))
+        WebEngageController.trackEvent(PS_VERIFY_OTP_VERIFY, OTP_VERIFY_CLICK, NO_EVENT_VALUE)
+        val otp = binding?.pinOtpVerify?.otp
+        viewModel?.verifyLoginOtp(number = phoneNumber, otp, clientId2)
+            ?.observeOnce(viewLifecycleOwner, {
+                hideProgress()
+                if (it.isSuccess()) {
+                    val result = it as? VerifyOtpResponse
+                    if (result?.Result?.authTokens.isNullOrEmpty().not()) {
+                        if (result?.Result?.authTokens!!.size == 1) {
+                            this.resultLogin = result.Result
+                            authTokenData()?.createAccessTokenAuth()
+                        } else {
+                            startFragmentFromNewOnBoardingActivity(
+                                activity = requireActivity(),
+                                type = com.boost.presignin.constant.FragmentType.WELCOME_FRAGMENT,
+                                bundle = Bundle().apply {
+                                },
+                                clearTop = true
+                            )
+                        }
+                    } else {
+                        startFragmentFromNewOnBoardingActivity(
+                            activity = requireActivity(),
+                            type = com.boost.presignin.constant.FragmentType.WELCOME_FRAGMENT,
+                            bundle = Bundle().apply {
+                            },
+                            clearTop = true
+                        )
+                    }
+                } else {
+                    showLongToast(getString(R.string.wrong_otp_tv))
+                }
+            })
+    }
+
+    override fun onOTPReceived(otp: String?) {
+        binding?.pinOtpVerify?.post { binding?.pinOtpVerify?.setOTP(otp ?: "") }
+    }
+
+
 }
