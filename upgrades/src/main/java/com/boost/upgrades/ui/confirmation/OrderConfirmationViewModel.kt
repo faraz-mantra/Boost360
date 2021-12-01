61e13fe674e3c8821e86f75e2a5757ba2745e2aa
Anurag-MantraLabs <anurag.j@mantralabsglobal.com>
2021-12-01 14:12:51 +0530

    Renewal History Page Bug Fixes

--- upgrades/src/main/java/com/boost/upgrades/ui/confirmation/OrderConfirmationViewModel.kt
+++ upgrades/src/main/java/com/boost/upgrades/ui/confirmation/OrderConfirmationViewModel.kt
@@ -1,8 +1,12 @@
 package com.boost.upgrades.ui.confirmation
 
 import android.app.Application
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.biz2.nowfloats.boost.updates.persistance.local.AppDatabase
+import com.boost.presignin.rest.repository.WithFloatTwoRepository
+import com.framework.base.BaseResponse
+import com.framework.models.toLiveData
 import io.reactivex.Completable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
@@ -21,5 +25,9 @@ class OrderConfirmationViewModel : ViewModel() {
         //if any action post empty_cart
       }
       .subscribe()
+  }
+
+  fun getFpDetails(fpId: String, map: Map<String, String>): LiveData<BaseResponse> {
+    return WithFloatTwoRepository.getFpDetails(fpId, map).toLiveData()
   }
 }
