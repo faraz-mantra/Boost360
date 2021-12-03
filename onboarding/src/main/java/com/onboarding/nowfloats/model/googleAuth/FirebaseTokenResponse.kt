64e4be2088e88bef9b97389aa123b53a0484a9e0
Shyam Gopalsamy <shyam91s@gmail.com>
2021-12-03 10:14:43 +0530

    dr score obfuscation issue fix

--- onboarding/src/main/java/com/onboarding/nowfloats/model/googleAuth/FirebaseTokenResponse.kt
+++ onboarding/src/main/java/com/onboarding/nowfloats/model/googleAuth/FirebaseTokenResponse.kt
@@ -1,8 +1,10 @@
 package com.onboarding.nowfloats.model.googleAuth
 
 import com.framework.base.BaseResponse
+import com.google.gson.annotations.SerializedName
 
 class FirebaseTokenResponse(
+    @SerializedName("Result")
     val Result:String?
 ):BaseResponse() {
 }
