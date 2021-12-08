ecc329d4aea7f0eeda10ca75b0618d5a9dcedead
Anurag-MantraLabs <anurag.j@mantralabsglobal.com>
2021-12-08 11:09:01 +0530

    New Classes Created

--- marketplace/src/main/java/com/boost/marketplace/ui/home/MarketPlaceActivity.kt
+++ marketplace/src/main/java/com/boost/marketplace/ui/home/MarketPlaceActivity.kt
@@ -1,18 +1,17 @@
 package com.boost.marketplace.ui.home
 
-import android.os.Bundle
 import com.boost.marketplace.R
 import com.boost.marketplace.base.AppBaseActivity
 import com.boost.marketplace.databinding.ActivityMarketplaceBinding
 
-class MarketPlaceActivity : AppBaseActivity<ActivityMarketplaceBinding, MarketPlaceViewModel>() {
+class MarketPlaceActivity : AppBaseActivity<ActivityMarketplaceBinding, MarketPlaceHomeViewModel>() {
 
     override fun getLayout(): Int {
         return R.layout.activity_marketplace
     }
 
-    override fun getViewModelClass(): Class<MarketPlaceViewModel> {
-        return MarketPlaceViewModel::class.java
+    override fun getViewModelClass(): Class<MarketPlaceHomeViewModel> {
+        return MarketPlaceHomeViewModel::class.java
     }
 
 
--- marketplace/src/main/java/com/boost/marketplace/ui/home/MarketPlaceHomeViewModel.kt
+++ marketplace/src/main/java/com/boost/marketplace/ui/home/MarketPlaceHomeViewModel.kt
@@ -2,5 +2,5 @@ package com.boost.marketplace.ui.home
 
 import com.framework.models.BaseViewModel
 
-class MarketPlaceViewModel: BaseViewModel() {
+class MarketPlaceHomeViewModel: BaseViewModel() {
 }
