42354b6e480891f76ec4a5d2baf9d507fdc234f7
shubhamgupta <shubham.gupta@cantech.co>
2021-12-11 00:26:31 +0530

    error handling UI completed

---
+++ framework/src/main/java/com/framework/errorHandling/ThankYouResponseBottomSheet.kt
@@ -0,0 +1,32 @@
+package com.framework.errorHandling
+
+import android.os.Bundle
+import com.framework.R
+import com.framework.base.BaseBottomSheetDialog
+import com.framework.databinding.BsheetErrorOccurredBinding
+import com.framework.databinding.BsheetThankYouResponseBinding
+import com.framework.models.BaseViewModel
+
+class ThankYouResponseBottomSheet : BaseBottomSheetDialog<BsheetThankYouResponseBinding, BaseViewModel>() {
+
+    companion object {
+        @JvmStatic
+        fun newInstance(): ThankYouResponseBottomSheet {
+            val bundle = Bundle().apply {}
+            val fragment = ThankYouResponseBottomSheet()
+            fragment.arguments = bundle
+            return fragment
+        }
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.bsheet_thank_you_response
+    }
+
+    override fun getViewModelClass(): Class<BaseViewModel> {
+        return BaseViewModel::class.java
+    }
+
+    override fun onCreateView() {
+    }
+}
