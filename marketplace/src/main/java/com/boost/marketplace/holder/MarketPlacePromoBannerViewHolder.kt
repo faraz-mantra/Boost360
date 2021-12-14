83822b0d22620be73ea569eed6cb2feb57d7e394
Anurag-MantraLabs <anurag.j@mantralabsglobal.com>
2021-12-14 12:56:29 +0530

    Ui changes

--- marketplace/src/main/java/com/boost/marketplace/holder/MarketPlacePromoBannerViewHolder.kt
+++ marketplace/src/main/java/com/boost/marketplace/holder/MarketPlacePromoBannerViewHolder.kt
@@ -13,14 +13,14 @@ class MarketPlacePromoBannerViewHolder(binding: ItemPromoBannerBinding) : AppBas
   override fun bind(position: Int, item: BaseRecyclerViewItem) {
     super.bind(position, item)
     val data = item as? PromoBanners ?: return
-//    activity?.glideLoad(binding.packagePrimaryImage, data.image?.url ?: "", R.drawable.placeholder_image_n)
-//    binding.packagePrimaryImage.setOnClickListener {
-//      listener?.onItemClick(
-//        position,
-//        data,
-//        RecyclerViewActionType.MARKETPLACE_PROMO_BANNER_CLICK.ordinal
-//      )
-//    }
+    activity?.glideLoad(binding.packagePrimaryImage, data.image?.url ?: "", R.drawable.placeholder_image_n)
+    binding.packagePrimaryImage.setOnClickListener {
+      listener?.onItemClick(
+        position,
+        data,
+        RecyclerViewActionType.MARKETPLACE_PROMO_BANNER_CLICK.ordinal
+      )
+    }
 
   }
 
