7ba2cbdf3a8afd3d44c40937a8e6976eb4ca3b29
Faraz <faraz78626@outlook.com>
2021-12-20 02:26:39 +0530

    MarketPlaceOffers_Activity updated

--- marketplace/src/main/java/com/boost/marketplace/ui/Marketplace_Offers/MarketPlaceOffersActivity.kt
+++ marketplace/src/main/java/com/boost/marketplace/ui/Marketplace_Offers/MarketPlaceOffersActivity.kt
@@ -1,5 +1,7 @@
 package com.boost.marketplace.ui.Marketplace_Offers
 
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.boost.marketplace.R
 import com.boost.marketplace.base.AppBaseActivity
 import com.boost.marketplace.databinding.ActivityMarketplaceoffersBinding
@@ -9,20 +11,53 @@ class MarketPlaceOffersActivity : AppBaseActivity<ActivityMarketplaceoffersBindi
 
     override fun getLayout(): Int {
         return R.layout.activity_marketplaceoffers
-
-
-
     }
 
     override fun getViewModelClass(): Class<MarketPlaceOffersViewModel> {
         return MarketPlaceOffersViewModel::class.java
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
+        initializeDetailsRecycler()
+        initializeTermsRecycler()
 
+        binding!!.dateFromToLayout.setOnClickListener {
 
+        }
+        binding!!.availCouponSubmit.setOnClickListener {
 
+        }
+
+        binding!!.packageBack.setOnClickListener {
+
+        }
+        binding!!.help.setOnClickListener {
+
+        }
+
+    }
+
+    private fun initializeTermsRecycler() {
+        val gridLayoutManager = LinearLayoutManager(MarketPlaceOffersActivity())
+        gridLayoutManager.orientation = LinearLayoutManager.VERTICAL
+        binding?.recyclerTerms?.apply {
+            layoutManager = gridLayoutManager
+        }
+    }
+
+    fun initializeDetailsRecycler() {
+        val gridLayoutManager = LinearLayoutManager(MarketPlaceOffersActivity())
+        gridLayoutManager.orientation = LinearLayoutManager.VERTICAL
+        binding?.recyclerOfferDetails?.apply {
+            layoutManager = gridLayoutManager
+        }
+    }
 }
+
+
+
 
 
 
