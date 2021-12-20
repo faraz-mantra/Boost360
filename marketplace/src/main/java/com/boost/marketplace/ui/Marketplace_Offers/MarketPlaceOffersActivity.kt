fb4da2c68a34b6e531ed4021d680cc8f7e7a2d17
Faraz <faraz78626@outlook.com>
2021-12-20 15:55:52 +0530

    MarketPlaceOffers_Activity & ViewModel

--- marketplace/src/main/java/com/boost/marketplace/ui/Marketplace_Offers/MarketPlaceOffersActivity.kt
+++ marketplace/src/main/java/com/boost/marketplace/ui/Marketplace_Offers/MarketPlaceOffersActivity.kt
@@ -1,13 +1,39 @@
 package com.boost.marketplace.ui.Marketplace_Offers
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.TextPaint
+import android.text.style.ClickableSpan
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.boost.dbcenterapi.data.api_model.GetAllFeatures.response.MarketPlaceOffers
+import com.boost.dbcenterapi.data.api_model.GetPurchaseOrder.Result
+import com.boost.dbcenterapi.utils.SharedPrefs
 import com.boost.marketplace.R
 import com.boost.marketplace.base.AppBaseActivity
 import com.boost.marketplace.databinding.ActivityMarketplaceoffersBinding
+import com.boost.marketplace.infra.recyclerView.AppBaseRecyclerViewAdapter
+import com.bumptech.glide.Glide
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import kotlinx.android.synthetic.main.activity_marketplaceoffers.*
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class MarketPlaceOffersActivity : AppBaseActivity<ActivityMarketplaceoffersBinding, MarketPlaceOffersViewModel>() {
+
+    var marketOffersData: MarketPlaceOffers? = null
+    lateinit var marketOfferDetailAdapter: AppBaseRecyclerViewAdapter<MarketPlaceOffers>
+    lateinit var marketOfferTermsAdapter: AppBaseRecyclerViewAdapter<MarketPlaceOffers>
+    lateinit var prefs: SharedPrefs
+
 
     override fun getLayout(): Int {
         return R.layout.activity_marketplaceoffers
@@ -20,23 +46,180 @@ class MarketPlaceOffersActivity : AppBaseActivity<ActivityMarketplaceoffersBindi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+      //  val jsonString = requireArguments().getString("marketOffersData")
+     //   marketOffersData = Gson().fromJson(this!!.getString("MarketPlaceOffers"), MarketPlaceOffers::class.java)
+     //   marketOffersData = Gson().fromJson<MarketPlaceOffers>(jsonString, object : TypeToken<MarketPlaceOffers>() {}.type)
+        prefs = SharedPrefs(this)
+
         initializeDetailsRecycler()
         initializeTermsRecycler()
+        loadData()
+     //   initMvvm()
+
+
+
+       binding?.offerTitle?.setText(marketOffersData!!.title)
+        avail_coupon_txt?.setText(marketOffersData!!.coupon_code)
+        offer_validity_txt?.setText(getConvertedExpiryDateFormat(marketOffersData!!.expiry_date))
+        if (marketOffersData?.extra_information != null) {
+            var list: ArrayList<String> = arrayListOf()
+            var listTerms: ArrayList<String> = arrayListOf()
+
+            var extra = marketOffersData?.extra_information?.trim()
+            extra = extra?.replace("S2coupon_code", marketOffersData!!.coupon_code)
+            extra = extra?.replace("S1coupon_code", marketOffersData!!.coupon_code)
+            extra = extra?.replace("S2discount_amt", "null")
+            extra = extra!!.replace("S1discount_amt", "null")
+            extra = extra?.replace("S2from_date", getConvertedDateFormat(marketOffersData!!.createdon))
+            extra =
+                extra?.replace("S2to_date", getConvertedExpiryDateFormat(marketOffersData!!.expiry_date))
+            Log.v("marketOffersDataValue", " " + extra)
+            var extraInfoTermsNCond = extra.trim()?.split("Terms and Conditions")?.toTypedArray()
+
+            Log.v("extraInfoTermsNCond", " " + extraInfoTermsNCond?.get(1))
+            var termsContent = extraInfoTermsNCond?.get(1)?.split("s2")?.toTypedArray()
+            var offerContent = extraInfoTermsNCond?.get(0)?.split("s1")?.toTypedArray()
+            for (terms in termsContent!!) {
+                if (terms.isNotEmpty()) {
+                    Log.v(
+                        "listTermsBoolean",
+                        " " + terms.contains("S2coupon_code") + terms.contains("S2from_date") + marketOffersData!!.coupon_code
+                    )
+
+                    listTerms.add(terms)
+                    Log.v("listTerms", " " + terms)
+                }
+
+
+            }
+            updateTermsRecycler(listTerms)
+
+            for (extraInfo in offerContent!!) {
+                if (extraInfo.isNotEmpty() && !extraInfo.equals("") && extraInfo.length > 1) {
+                    extraInfo.replace("@coupon_code", marketOffersData!!.coupon_code)
+                    extraInfo.replace("@discount_amt", "null")
+                    list.add(extraInfo)
+                    Log.v("extraInfoList", " " + extraInfo)
+                }
+
+
+            }
+            updateRecycler(list)
+//            marketOfferDetailAdapter = MarketOfferDetailAdapter(list, this)
+        }
+
+        val ss = SpannableString(
+            resources.getString(
+                R.string.marketoffer_termSix,
+                getConvertedDateFormat(marketOffersData!!.createdon),
+                getConvertedExpiryDateFormat(marketOffersData!!.expiry_date)
+            )
+        )
+        val clickableSpan: ClickableSpan = object : ClickableSpan() {
+            override fun onClick(textView: View) {
+                Toast.makeText(this@MarketPlaceOffersActivity, "On Clicked ", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun updateDrawState(ds: TextPaint) {
+                super.updateDrawState(ds)
+                ds.color = resources.getColor(R.color.common_text_color)
+                ds.isUnderlineText = true
+            }
+        }
+
+//        if (requireArguments().containsKey("showCartIcon")) {
+//            offer_info_icon.visibility = View.INVISIBLE
+//            avail_coupon_submit.visibility = View.GONE
+//        }
+
+        if (marketOffersData!!.image != null && !marketOffersData!!.image!!.url.isNullOrEmpty()) {
+            Glide.with(this).load(marketOffersData!!.image!!.url).into(binding?.marketoffersImage!!)
+        } else {
+            binding?.marketoffersImage?.setImageResource(R.drawable.rectangle_copy_18)
+        }
+
+        binding!!.packageBack.setOnClickListener {
+            TODO("Not yet implemented")
+        }
+
+        binding!!.help.setOnClickListener {
+            TODO("Not yet implemented")
+        }
 
         binding!!.dateFromToLayout.setOnClickListener {
+            val clipboard: ClipboardManager =
+                this.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clip = ClipData.newPlainText(avail_coupon_txt.text, avail_coupon_txt.text)
+            clipboard.setPrimaryClip(clip)
+            Toast.makeText(this, "Coupon " + avail_coupon_txt.text + " copied!!", Toast.LENGTH_SHORT)
+                .show()
 
         }
         binding!!.availCouponSubmit.setOnClickListener {
 
+            val clipboard: ClipboardManager =
+                this.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clip = ClipData.newPlainText(avail_coupon_txt.text, avail_coupon_txt.text)
+            clipboard.setPrimaryClip(clip)
+//            (activity as UpgradeActivity).popActivityFromBackStack()
+            Toast.makeText(this, "Coupon " + avail_coupon_txt.text + " copied!!", Toast.LENGTH_SHORT)
+                .show()
         }
 
-        binding!!.packageBack.setOnClickListener {
+    }
 
+    private fun loadData() {
+        Log.d("marketOffersCoupon", " " + marketOffersData!!.coupon_code)
+        viewModel.getOffersByCouponId(marketOffersData!!.coupon_code)
+    }
+
+    private fun initMvvm() {
+        viewModel.marketOffersCouponResult().observe(this, androidx.lifecycle.Observer {
+            Log.v("marketOffersCoupon", " " + it.coupon_code) })
+    }
+
+    fun getConvertedDateFormat(textDate: String): String {
+        val input = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+        val output = SimpleDateFormat("dd'th' MMM yyyy ")
+
+        var d: Date? = null
+        try {
+            d = input.parse(textDate)
+        } catch (e: ParseException) {
+            e.printStackTrace()
         }
-        binding!!.help.setOnClickListener {
+        return output.format(d)
+    }
 
+    fun getConvertedExpiryDateFormat(textDate: String): String {
+        Log.v("getConvertedExpir", " " + textDate)
+//        val input = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+        val input = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
+//        val output = SimpleDateFormat("dd'th' MMM yyyy ")
+        val output = SimpleDateFormat("dd-MMM-yy ")
+
+        var d: Date? = null
+        try {
+            d = input.parse(textDate)
+        } catch (e: ParseException) {
+            e.printStackTrace()
         }
+        return output.format(d)
+    }
 
+    fun updateRecycler(list: List<String>) {
+        Log.v("updateRecycler", " " + list)
+        marketOfferDetailAdapter.addupdates(list)
+        recyclerOfferDetails.adapter = marketOfferDetailAdapter
+//        marketOfferDetailAdapter.notifyDataSetChanged()
+    }
+
+    fun initializeDetailsRecycler() {
+        val gridLayoutManager = LinearLayoutManager(MarketPlaceOffersActivity())
+        gridLayoutManager.orientation = LinearLayoutManager.VERTICAL
+        binding?.recyclerOfferDetails?.apply {
+            layoutManager = gridLayoutManager
+        }
     }
 
     private fun initializeTermsRecycler() {
@@ -47,59 +230,11 @@ class MarketPlaceOffersActivity : AppBaseActivity<ActivityMarketplaceoffersBindi
         }
     }
 
-    fun initializeDetailsRecycler() {
-        val gridLayoutManager = LinearLayoutManager(MarketPlaceOffersActivity())
-        gridLayoutManager.orientation = LinearLayoutManager.VERTICAL
-        binding?.recyclerOfferDetails?.apply {
-            layoutManager = gridLayoutManager
-        }
+    fun updateTermsRecycler(list: List<String>) {
+        Log.v("updateRecycler", " " + list)
+        marketOfferTermsAdapter.addupdates(list)
+        recyclerTerms.adapter = marketOfferTermsAdapter
+//        marketOfferDetailAdapter.notifyDataSetChanged()
     }
+
 }
-
-
-
-
-
-
-
-
-//    lateinit var binding: MarketPlaceOffersActivity
-
-  //  override fun onCreate(savedInstanceState: Bundle?) {
-     //   super.onCreate(savedInstanceState)
-    //  binding= DataBindingUtil.setContentView(this, R.layout.activity_marketplaceoffers)
-//
-//        initializeDetailsRecycler()
-//        initializeTermsRecycler()
-//
-//        binding.dateFromToLayout.setOnClickListener {
-//
-//        }
-//        binding.availCouponSubmit.setOnClickListener {
-//
-//        }
-//
-//        binding.package_back.setOnClickListener {
-//
-//        }
-//        binding.help.setOnClickListener {
-//
-//        }
-//    }
-//
-//    private fun initializeDetailsRecycler() {
-//        val gridLayoutManager = LinearLayoutManager(MarketPlaceOffersActivity())
-//        gridLayoutManager.orientation = LinearLayoutManager.VERTICAL
-//        recyclerOfferDetails.apply {
-//            layoutManager = gridLayoutManager
-//        }
-//    }
-//
-//    private fun initializeTermsRecycler() {
-//        val gridLayoutManager = LinearLayoutManager(MarketPlaceOffersActivity())
-//        gridLayoutManager.orientation = LinearLayoutManager.VERTICAL
-//        recyclerTerms.apply {
-//            layoutManager = gridLayoutManager
-//        }
-//    }
-//}
