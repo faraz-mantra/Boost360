2bc4435797f4d1956b6886401685871cb65d49c6
Anurag-MantraLabs <anurag.j@mantralabsglobal.com>
2021-12-28 15:59:44 +0530

    cart to payment ui

--- marketplace/src/main/java/com/boost/marketplace/ui/home/MarketPlaceActivity.kt
+++ marketplace/src/main/java/com/boost/marketplace/ui/home/MarketPlaceActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
 import com.boost.dbcenterapi.data.api_model.GetAllFeatures.response.*
 import com.boost.dbcenterapi.recycleritem.BaseRecyclerViewItem
