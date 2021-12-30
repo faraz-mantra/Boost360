789a467b96a83ed15af93219b8c2b70c368042fa
Sarvare Alam <sarvare@cantech.co>
2021-12-31 00:12:14 +0530

    bug fixed

--- framework/src/main/java/com/framework/errorHandling/ErrorFlowInvokeObject.kt
+++ framework/src/main/java/com/framework/errorHandling/ErrorFlowInvokeObject.kt
@@ -7,7 +7,6 @@ object ErrorFlowInvokeObject {
   fun errorOccurred(errorCode: Int, errorMessage: String, correlationId: String) {
     when (errorCode) {
       400, in 500..599 -> {
-        currentActivity()
         currentActivity()?.supportFragmentManager?.let {
           ErrorOccurredBottomSheet(correlationId, errorMessage).show(it, ErrorOccurredBottomSheet::class.java.name)
         }
