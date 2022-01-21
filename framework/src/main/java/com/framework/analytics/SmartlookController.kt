a01a65020433bab259de88d6b593e3cbd8b5b24c
Sarvare Alam <sarvare@cantech.co>
2022-01-21 18:52:30 +0530

    Keys add in gradle file

--- framework/src/main/java/com/framework/analytics/SmartlookController.kt
+++ framework/src/main/java/com/framework/analytics/SmartlookController.kt
@@ -1,12 +1,12 @@
 package com.framework.analytics
 
+import com.framework.BuildConfig
 import com.smartlook.sdk.smartlook.Smartlook
 
 object SmartlookController {
 
-    val YOUR_API_KEY="2976fda0fc620d3186eca19884bd55ac125b56e4"
-    fun setupAndStartRecording(){
-        Smartlook.setupAndStartRecording(YOUR_API_KEY);
+  fun setupAndStartRecording() {
+    Smartlook.setupAndStartRecording(BuildConfig.SMARTLOOK_API_KEY);
 
-    }
+  }
 }
