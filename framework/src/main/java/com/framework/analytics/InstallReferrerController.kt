80dabad570aab239d69ef041ff61ad17aa7d64cd
Shyam Gopalsamy <shyam91s@gmail.com>
2022-02-11 12:28:53 +0530

    install referral init

---
+++ framework/src/main/java/com/framework/analytics/InstallReferrerController.kt
@@ -0,0 +1,35 @@
+package com.framework.analytics
+
+import com.android.installreferrer.api.InstallReferrerClient
+import com.android.installreferrer.api.InstallReferrerStateListener
+
+object InstallReferrerController {
+
+    private lateinit var referrerClient: InstallReferrerClient
+
+    fun getInstallReferrerClient() {
+        referrerClient = InstallReferrerClient.newBuilder(this).build()
+        referrerClient.startConnection(object : InstallReferrerStateListener {
+
+            override fun onInstallReferrerSetupFinished(responseCode: Int) {
+                when (responseCode) {
+                    InstallReferrerClient.InstallReferrerResponse.OK -> {
+                        // Connection established.
+                    }
+                    InstallReferrerClient.InstallReferrerResponse.FEATURE_NOT_SUPPORTED -> {
+                        // API not available on the current Play Store app.
+                    }
+                    InstallReferrerClient.InstallReferrerResponse.SERVICE_UNAVAILABLE -> {
+                        // Connection couldn't be established.
+                    }
+                }
+            }
+
+            override fun onInstallReferrerServiceDisconnected() {
+                // Try to restart the connection on the next request to
+                // Google Play by calling the startConnection() method.
+            }
+        })
+    }
+
+}
