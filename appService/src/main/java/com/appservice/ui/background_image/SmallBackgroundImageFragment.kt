d388e6f53e10e954995dd675dab2ddff57da9068
silpi.mukherjee2 <silpi.mukherjee2@religare.com>
2022-02-24 21:04:47 +0530

    background image ui changes done and migration in progress

--- appService/src/main/java/com/appservice/ui/background_image/SmallBackgroundImageFragment.kt
+++ appService/src/main/java/com/appservice/ui/background_image/SmallBackgroundImageFragment.kt
@@ -3,15 +3,15 @@ package com.appservice.ui.background_image
 import com.appservice.R
 import com.appservice.base.AppBaseFragment
 import com.appservice.databinding.FragmentBackgroundImageBinding
-import com.appservice.databinding.FragmentCropZoomBinding
+import com.appservice.databinding.FragmentSmallBackgroundImageBinding
 import com.framework.models.BaseViewModel
 
-class CropZoomImageFragment : AppBaseFragment<FragmentCropZoomBinding,BaseViewModel>() {
+class SmallBackgroundImageFragment : AppBaseFragment<FragmentSmallBackgroundImageBinding,BaseViewModel>() {
     override fun getLayout(): Int {
-        return R.layout.fragment_crop_zoom
+        return R.layout.fragment_small_background_image
     }
 
     override fun getViewModelClass(): Class<BaseViewModel> {
-        return BaseViewModel::class.java
+        TODO("Not yet implemented")
     }
 }
