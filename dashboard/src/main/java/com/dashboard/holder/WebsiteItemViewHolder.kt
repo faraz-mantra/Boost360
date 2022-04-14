7ea969bd52f9be8a0afc417f9b6f4b2846b37ff2
suman <sum20156@gmail.com>
2022-04-14 17:02:05 +0530

    website item text fix

--- dashboard/src/main/java/com/dashboard/holder/WebsiteItemViewHolder.kt
+++ dashboard/src/main/java/com/dashboard/holder/WebsiteItemViewHolder.kt
@@ -9,13 +9,14 @@ import com.dashboard.recyclerView.AppBaseRecyclerViewHolder
 import com.dashboard.recyclerView.BaseRecyclerViewItem
 import com.framework.extensions.invisible
 import com.framework.extensions.visible
+import com.framework.utils.capitalized
 
 class WebsiteItemViewHolder(binding: ItemWebsiteItemBinding) : AppBaseRecyclerViewHolder<ItemWebsiteItemBinding>(binding) {
 
   override fun bind(position: Int, item: BaseRecyclerViewItem) {
     super.bind(position, item)
     val data = item as? WebsiteActionItem ?: return
-    binding.textViewName.text = data.title
+    binding.textViewName.text = data.title?.split(' ')?.joinToString(" ") { it.capitalized() }
     binding.txtDesc.text = data.desc
     binding.tvCount.text = data.getCountN().toString()
     getColor(if (data.getCountN() == 0) R.color.gray_e2e2e2e2 else R.color.black_4a4a4a)?.let { binding.tvCount.setTextColor(it) }
