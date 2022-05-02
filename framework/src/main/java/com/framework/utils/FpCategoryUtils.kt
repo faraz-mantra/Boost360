50f6b619ce3c8ec9694ad3d8addb0def5ce12637
suman <sum20156@gmail.com>
2022-05-02 18:20:55 +0530

    client logo zero case added

--- framework/src/main/java/com/framework/utils/FpCategoryUtils.kt
+++ framework/src/main/java/com/framework/utils/FpCategoryUtils.kt
@@ -4,6 +4,10 @@ fun isHotelProfile(category_code: String?): Boolean {
     return category_code.equals("HOT", true)
 }
 
+fun isManufacturingProfile(category_code: String?): Boolean {
+    return category_code.equals("MFG", true)
+}
+
 fun isRestaurantProfile(category_code: String?): Boolean {
     return category_code.equals("CAF", true)
 }
