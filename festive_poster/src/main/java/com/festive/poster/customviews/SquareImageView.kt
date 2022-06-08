6d9c90e075c21146845dbeb0cd4b580af5662aea
suman <sum20156@gmail.com>
2022-06-08 21:41:43 +0530

    poster view adjusted for differnet screen sizes

---
+++ festive_poster/src/main/java/com/festive/poster/customviews/SquareImageView.kt
@@ -0,0 +1,34 @@
+package com.festive.poster.customviews
+
+import android.content.Context
+import android.util.AttributeSet
+import com.framework.R
+import com.framework.views.customViews.CustomImageView
+
+class SquareImageView: CustomImageView {
+
+    constructor(context: Context) : super(context) {
+        setCustomAttrs(context, null)
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        setCustomAttrs(context, attrs)
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle) {
+        setCustomAttrs(context, attrs)
+    }
+
+    private fun setCustomAttrs(context: Context, attrs: AttributeSet?) {
+        if (attrs == null) return
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.CustomImageView)
+        typedArray.recycle()
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val width = measuredWidth
+        setMeasuredDimension(width, width)
+    }
+
+}
