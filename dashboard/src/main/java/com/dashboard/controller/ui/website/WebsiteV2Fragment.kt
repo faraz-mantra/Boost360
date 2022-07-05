7cb8125e379c18c2252a9017f35542a3bf26afe8
shubhamgupta <shubham.gupta@cantech.co>
2022-07-05 17:25:17 +0530

    "click here" at website bottom done

--- dashboard/src/main/java/com/dashboard/controller/ui/website/WebsiteV2Fragment.kt
+++ dashboard/src/main/java/com/dashboard/controller/ui/website/WebsiteV2Fragment.kt
@@ -3,11 +3,17 @@ package com.dashboard.controller.ui.website
 import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Typeface.*
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
 import android.util.Log
 import android.view.*
 import android.widget.PopupWindow
 import android.widget.TextView
 import androidx.appcompat.widget.LinearLayoutCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
@@ -25,8 +31,6 @@ import com.dashboard.databinding.FragmentWebsiteV2Binding
 import com.dashboard.model.live.websiteItem.WebsiteActionItem
 import com.dashboard.utils.*
 import com.dashboard.viewmodel.DashboardViewModel
-import com.framework.extensions.gone
-import com.framework.extensions.visible
 import com.framework.glide.util.glideLoad
 import com.framework.pref.BASE_IMAGE_URL
 import com.framework.pref.Key_Preferences
@@ -38,6 +42,7 @@ import com.framework.views.customViews.CustomImageView
 import com.framework.webengageconstant.DASHBOARD_WEBSITE_PAGE
 import com.framework.webengageconstant.PAGE_VIEW
 import com.google.android.material.tabs.TabLayout
+
 
 private val TAB_TITLES = arrayOf("Content", "Features")
 
@@ -64,13 +69,13 @@ class WebsiteV2Fragment : AppBaseFragment<FragmentWebsiteV2Binding, DashboardVie
         session = UserSessionManager(baseActivity)
         setOnClickListener(
             binding?.txtDomainName, binding?.btnProfileLogo, binding?.editProfileWrapper,
-            binding?.websiteThemeCustomization, binding?.businessTiming, binding?.layoutContent, binding?.layoutSettings,
-            binding?.tvClickHere)
+            binding?.websiteThemeCustomization, binding?.businessTiming, binding?.layoutContent, binding?.layoutSettings)
         WebEngageController.trackEvent(DASHBOARD_WEBSITE_PAGE, PAGE_VIEW, session?.fpTag)
         setupViewPager()
         this.websiteLink = fromHtml("<u>${session?.getDomainName()}</u>").toString()
         businessName = session?.fPName!!
         businessContact = session?.fPPrimaryContactNumber ?: ""
+        setSpannable()
     }
 
     private fun setupViewPager() {
@@ -165,44 +170,22 @@ class WebsiteV2Fragment : AppBaseFragment<FragmentWebsiteV2Binding, DashboardVie
 
     private fun clickActionButton(type: WebsiteActionItem.IconType) {
         when (type) {
-            WebsiteActionItem.IconType.service_product_catalogue -> baseActivity.startListServiceProduct(
-                session
-            )
-            WebsiteActionItem.IconType.latest_update_tips -> session?.let {
-                baseActivity.startUpdateLatestStory(
-                    it
-                )
-            }
-            WebsiteActionItem.IconType.all_images -> startBackgroundActivity(
-                session,
-                FragmentType.BACKGROUND_IMAGE_FRAGMENT
-            )
-            WebsiteActionItem.IconType.business_profile -> baseActivity.startFragmentsFactory(
-                session,
-                fragmentType = "Business_Profile_Fragment_V2"
-            )
+            WebsiteActionItem.IconType.service_product_catalogue -> baseActivity.startListServiceProduct(session)
+            WebsiteActionItem.IconType.latest_update_tips -> session?.let { baseActivity.startUpdateLatestStory(it) }
+            WebsiteActionItem.IconType.all_images -> startBackgroundActivity(session, FragmentType.BACKGROUND_IMAGE_FRAGMENT)
+            WebsiteActionItem.IconType.business_profile -> baseActivity.startFragmentsFactory(session, fragmentType = "Business_Profile_Fragment_V2")
             WebsiteActionItem.IconType.testimonials -> baseActivity.startTestimonial(session)
             WebsiteActionItem.IconType.custom_page -> baseActivity.startCustomPage(session)
-            WebsiteActionItem.IconType.project_teams -> baseActivity.startListProjectAndTeams(
-                session
-            )
-            WebsiteActionItem.IconType.unlimited_digital_brochures -> baseActivity.startListDigitalBrochure(
-                session
-            )
+            WebsiteActionItem.IconType.project_teams -> baseActivity.startListProjectAndTeams(session)
+            WebsiteActionItem.IconType.unlimited_digital_brochures -> baseActivity.startListDigitalBrochure(session)
             WebsiteActionItem.IconType.toppers_institute -> baseActivity.startListToppers(session)
             WebsiteActionItem.IconType.upcoming_batches -> baseActivity.startListBatches(session)
             WebsiteActionItem.IconType.faculty_management -> baseActivity.startFacultyMember(session)
             WebsiteActionItem.IconType.places_look_around -> baseActivity.startNearByView(session)
-            WebsiteActionItem.IconType.trip_adviser_ratings -> baseActivity.startListTripAdvisor(
-                session
-            )
-            WebsiteActionItem.IconType.seasonal_offers -> baseActivity.startListSeasonalOffer(
-                session
-            )
+            WebsiteActionItem.IconType.trip_adviser_ratings -> baseActivity.startListTripAdvisor(session)
+            WebsiteActionItem.IconType.seasonal_offers -> baseActivity.startListSeasonalOffer(session)
             WebsiteActionItem.IconType.website_theme -> baseActivity.startWebsiteTheme(session)
-            WebsiteActionItem.IconType.doctor_e_profile_listing -> baseActivity.startListDoctors(
-                session
-            )
+            WebsiteActionItem.IconType.doctor_e_profile_listing -> baseActivity.startListDoctors(session)
         }
     }
 
@@ -235,7 +218,6 @@ class WebsiteV2Fragment : AppBaseFragment<FragmentWebsiteV2Binding, DashboardVie
             binding?.businessTiming -> baseActivity.startBusinessHours(session)
             binding?.layoutContent -> setUpButtonSelectedUI()
             binding?.layoutSettings -> setUpButtonSelectedUI(false)
-            binding?.tvClickHere -> showShortToast(getString(R.string.coming_soon))
         }
 
     }
@@ -246,33 +228,21 @@ class WebsiteV2Fragment : AppBaseFragment<FragmentWebsiteV2Binding, DashboardVie
     }
 
     private fun shareWhatsAppText() {
-        ContentSharing.shareWebsiteTheme(
-            requireActivity(),
-            businessName!!,
-            websiteLink!!,
-            businessContact!!,
-            isWhatsApp = true
-        )
+        ContentSharing.shareWebsiteTheme(requireActivity(), businessName!!, websiteLink!!, businessContact!!, isWhatsApp = true)
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
         inflater.inflate(R.menu.menu_website_theme, menu)
         val item = menu.findItem(R.id.menu_whatsapp_share)?.actionView
-        item?.findViewById<LinearLayoutCompat>(R.id.share_whatsapp)
-            ?.setOnClickListener { shareWhatsAppText() }
+        item?.findViewById<LinearLayoutCompat>(R.id.share_whatsapp)?.setOnClickListener { shareWhatsAppText() }
         item?.findViewById<CustomImageView>(R.id.more_share)?.setOnClickListener { openPopUp(it) }
     }
 
     private fun showPopupWindow(anchor: View) {
         val view = LayoutInflater.from(baseActivity)
             .inflate(R.layout.popup_window_website_menu_share, null)
-        this.popupWindow = PopupWindow(
-            view,
-            WindowManager.LayoutParams.WRAP_CONTENT,
-            WindowManager.LayoutParams.WRAP_CONTENT,
-            true
-        )
+        this.popupWindow = PopupWindow(view, WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT, true)
         val more = this.popupWindow?.contentView?.findViewById<LinearLayoutCompat>(R.id.ll_share)
         more?.setOnClickListener {
             shareMore()
@@ -283,10 +253,7 @@ class WebsiteV2Fragment : AppBaseFragment<FragmentWebsiteV2Binding, DashboardVie
         republishWebsiteBtn?.setOnClickListener {
             RepublishProgressBottomSheet().apply {
                 onRepublishSuccess = { openSuccessDialog() }
-                show(
-                    baseActivity.supportFragmentManager,
-                    RepublishProgressBottomSheet::javaClass.name
-                )
+                show(baseActivity.supportFragmentManager, RepublishProgressBottomSheet::javaClass.name)
             }
             this.popupWindow?.dismiss()
         }
@@ -295,12 +262,7 @@ class WebsiteV2Fragment : AppBaseFragment<FragmentWebsiteV2Binding, DashboardVie
     }
 
     private fun shareMore() {
-        ContentSharing.shareWebsiteTheme(
-            baseActivity,
-            businessName ?: "",
-            websiteLink ?: "",
-            businessContact ?: ""
-        )
+        ContentSharing.shareWebsiteTheme(baseActivity, businessName ?: "", websiteLink ?: "", businessContact ?: "")
     }
 
     private fun openSuccessDialog() {
@@ -337,7 +299,8 @@ class WebsiteV2Fragment : AppBaseFragment<FragmentWebsiteV2Binding, DashboardVie
             binding?.ivContent?.setImageResource(R.drawable.ic_note_white)
             binding?.ivSettings?.setImageResource(R.drawable.ic_slider_grey)
             binding?.pager?.currentItem = 0
-            binding?.tvClickHere?.text = getString(R.string.content_click_here_text)
+            setSpannable(true)
+
         } else {
             binding?.layoutContent?.setBackgroundResource(R.drawable.rounded_rect_white_18)
             binding?.layoutSettings?.setBackgroundResource(R.drawable.rounded_rect_grey_18)
@@ -346,8 +309,32 @@ class WebsiteV2Fragment : AppBaseFragment<FragmentWebsiteV2Binding, DashboardVie
             binding?.ivContent?.setImageResource(R.drawable.ic_note_grey)
             binding?.ivSettings?.setImageResource(R.drawable.ic_slider_white)
             binding?.pager?.currentItem = 1
-            binding?.tvClickHere?.text = getString(R.string.setting_click_here_text)
+            setSpannable(false)
         }
+    }
+
+    private fun setSpannable(isContentSelected: Boolean = true) {
+        val spannableString: SpannableString = if (isContentSelected) {
+            SpannableString(getString(R.string.content_click_here_text))
+        } else {
+            SpannableString(getString(R.string.setting_click_here_text))
+        }
+
+        spannableString.setSpan(
+            object : ClickableSpan() {
+                override fun onClick(p0: View) {
+                    if (isContentSelected) binding?.layoutSettings?.performClick() else binding?.layoutContent?.performClick()
+                }
+
+                override fun updateDrawState(ds: TextPaint) {
+                    super.updateDrawState(ds)
+                    ds.color = ContextCompat.getColor(baseActivity, R.color.color_747474)
+                    ds.typeface = DEFAULT_BOLD
+                }
+            },
+            spannableString.indexOf(getString(R.string.click_here)), spannableString.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        binding?.tvClickHere?.text = spannableString
+        binding?.tvClickHere?.movementMethod = LinkMovementMethod.getInstance()
     }
 }
 
