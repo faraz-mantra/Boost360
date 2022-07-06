ea6c454d4087c5c076c04445608ddb5eebfb7be9
Silpi <silpi.m@mantralabsglobal.com>
2022-07-07 00:50:43 +0530

    pack_details ui in progress

---
+++ marketplace/src/main/java/com/boost/marketplace/ui/pack_details/PackDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.boost.marketplace.ui.pack_details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.boost.marketplace.R
+
+class PackDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pack_details)
+    }
+}
