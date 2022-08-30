9fcd0c98b0b4d250c8c20c47d2c57e6d0d093273
suhassasuke <suhassasuke4090@gmail.com>
2022-08-30 10:02:54 +0530

    payment screen changes for auto renew

--- payment/src/main/java/com/boost/payment/ui/payment/PaymentFragment.kt
+++ payment/src/main/java/com/boost/payment/ui/payment/PaymentFragment.kt
@@ -503,6 +503,15 @@ class PaymentFragment : BaseFragment(), PaymentListener, BusinessDetailListener,
         auto_renew_switch.setOnClickListener {
             if (autoRenewState) {
                 autoRenewState = false
+                netbanking_layout.visibility = View.VISIBLE
+                feature_validity.text = ""
+                validity_months.setText(
+                    prefs.getValidityMonths() + Utils.yearOrMonthText(prefs.getValidityMonths()!!.toInt(), requireActivity(), true)
+                )
+                validity_period.text = "Validity period"
+                validity_period_value.setText(
+                    nowFormat.format(Calendar.getInstance().time) + " - " + nowFormat.format(oneMonthFromNow.time)
+                )
                 WebEngageController.trackEvent(
                         MARKETPLACE_AUTO_RENEWAL_OFF,
                         PAYMENT_SCREEN,
@@ -562,6 +571,13 @@ class PaymentFragment : BaseFragment(), PaymentListener, BusinessDetailListener,
                 updateAutoRenewState()
             } else {
                 autoRenewState = true
+                netbanking_layout.visibility = View.GONE
+                feature_validity.text = "Renewal period"
+                validity_months.setText(
+                    "Every "+prefs.getValidityMonths() + Utils.yearOrMonthText(prefs.getValidityMonths()!!.toInt(), requireActivity(), true)
+                )
+                validity_period.text = "Next billing date"
+                validity_period_value.setText(nowFormat.format(oneMonthFromNow.time))
                 WebEngageController.trackEvent(
                         MARKETPLACE_AUTO_RENEWAL_ON,
                         PAYMENT_SCREEN,
