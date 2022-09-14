19f0243090cbccb0ec6869126f5383ee6cc3fb1f
Faraz <faraz78626@outlook.com>
2022-09-14 12:15:39 +0530

    popup fixes

--- cart/src/main/java/com/boost/cart/ui/popup/FeatureDetailsPopup.kt
+++ cart/src/main/java/com/boost/cart/ui/popup/FeatureDetailsPopup.kt
@@ -62,13 +62,6 @@ class FeatureDetailsPopup : DialogFragment() {
     var originalBundlePrice = 0.0
     lateinit var singleAddon: FeaturesModel
     lateinit var viewModel: CartViewModel
-//    override fun getLayout(): Int {
-//        return R.layout.feature_details_bottomsheet
-//    }
-
-//    override fun getViewModelClass(): Class<CartViewModel> {
-//        return CartViewModel::class.java
-//    }
 
     override fun onStart() {
         super.onStart()
@@ -82,7 +75,6 @@ class FeatureDetailsPopup : DialogFragment() {
         val view  =  inflater.inflate(R.layout.feature_details_bottomsheet, container,false)
         viewModel = ViewModelProviders.of(this).get(CartViewModel::class.java)
         prefs = SharedPrefs(requireActivity())
-
         fpid = requireArguments().getString("fpid")
         isDeepLink = requireArguments().getBoolean("isDeepLink", false)
         deepLinkViewType = requireArguments().getString("deepLinkViewType") ?: ""
@@ -103,10 +95,6 @@ class FeatureDetailsPopup : DialogFragment() {
         widgetFeatureCode = requireArguments().getString("buyItemKey")
         userPurchsedWidgets =
             requireArguments().getStringArrayList("userPurchsedWidgets") ?: ArrayList()
-//        singleAddon = Gson().fromJson<FeaturesModel>(
-//            requireArguments().getString("featuresData"),
-//            object : TypeToken<FeaturesModel>() {}.type
-//        )
 
         val jsonString = requireArguments().getString("bundleData")
         bundleData = Gson().fromJson<Bundles>(jsonString, object : TypeToken<Bundles>() {}.type)
@@ -178,7 +166,6 @@ class FeatureDetailsPopup : DialogFragment() {
                     .observeOn(AndroidSchedulers.mainThread())
                     .subscribe(
                         {
-//                                            featuresList = it
                             var bundleMonthlyMRP = 0.0
                             val minMonth: Int =
                                 if (!prefs.getYearPricing() && bundleData!!.min_purchase_months != null && bundleData!!.min_purchase_months!! > 1) bundleData!!.min_purchase_months!! else 1
@@ -266,116 +253,12 @@ class FeatureDetailsPopup : DialogFragment() {
         dismiss()
     }
 
-    private fun skipToCart() {
-        if (bundleData != null) {
-            prefs.storeAddedPackageDesc(bundleData!!.desc ?: "")
-
-            val itemIds = arrayListOf<String>()
-            for (i in bundleData!!.included_features) {
-                itemIds.add(i.feature_code)
-            }
-
-            CompositeDisposable().add(
-                AppDatabase.getInstance(Application())!!
-                    .featuresDao()
-                    .getallFeaturesInList(itemIds)
-                    .subscribeOn(Schedulers.io())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe(
-                        {
-//                                            featuresList = it
-                            var bundleMonthlyMRP = 0.0
-                            val minMonth: Int =
-                                if (!prefs.getYearPricing() && bundleData!!.min_purchase_months != null && bundleData!!.min_purchase_months!! > 1) bundleData!!.min_purchase_months!! else 1
-
-                            for (singleItem in it) {
-                                for (item in bundleData!!.included_features) {
-                                    if (singleItem.feature_code == item.feature_code) {
-                                        bundleMonthlyMRP += RootUtil.round(
-                                            singleItem.price - ((singleItem.price * item.feature_price_discount_percent) / 100.0),
-                                            2
-                                        )
-                                    }
-                                }
-                            }
-                            offeredBundlePrice = (bundleMonthlyMRP * minMonth)
-                            originalBundlePrice = (bundleMonthlyMRP * minMonth)
-
-                            if (bundleData!!.overall_discount_percent > 0)
-                                offeredBundlePrice = RootUtil.round(
-                                    originalBundlePrice - (originalBundlePrice * bundleData!!.overall_discount_percent / 100),
-                                    2
-                                )
-                            else
-                                offeredBundlePrice = originalBundlePrice
-
-                            //clear cartOrderInfo from SharedPref to requestAPI again
-                            prefs.storeCartOrderInfo(null)
-
-                            viewModel?.addItemToCartPackage1(
-                                CartModel(
-                                    bundleData!!._kid,
-                                    null,
-                                    null,
-                                    bundleData!!.name,
-                                    "",
-                                    bundleData!!.primary_image!!.url,
-                                    offeredBundlePrice.toDouble(),
-                                    originalBundlePrice.toDouble(),
-                                    bundleData!!.overall_discount_percent,
-                                    1,
-                                    if (!prefs.getYearPricing() && bundleData!!.min_purchase_months != null) bundleData!!.min_purchase_months!! else 1,
-                                    "bundles",
-                                    null,
-                                    ""
-                                )
-                            )
-                        },
-                        {
-                            it.printStackTrace()
-                        }
-                    )
-            )
-        }
-
-        val intent = Intent(
-            requireContext(),
-            CartActivity::class.java
-        )
-        intent.putExtra("fpid", fpid)
-        intent.putExtra("expCode", experienceCode)
-        intent.putExtra("isDeepLink", isDeepLink)
-        intent.putExtra("deepLinkViewType", deepLinkViewType)
-        intent.putExtra("deepLinkDay", deepLinkDay)
-        intent.putExtra("isOpenCardFragment", isOpenCardFragment)
-        intent.putExtra(
-            "accountType",
-            accountType
-        )
-        intent.putStringArrayListExtra(
-            "userPurchsedWidgets",
-            userPurchsedWidgets
-        )
-        if (email != null) {
-            intent.putExtra("email", email)
-        } else {
-            intent.putExtra("email", "ria@nowfloats.com")
-        }
-        if (mobileNo != null) {
-            intent.putExtra("mobileNo", mobileNo)
-        } else {
-            intent.putExtra("mobileNo", "9160004303")
-        }
-        intent.putExtra("profileUrl", profileUrl)
-        startActivity(intent)
-    }
-
     private fun loadData() {
         try {
             val pref = activity?.getSharedPreferences("nowfloatsPrefs", Context.MODE_PRIVATE)
             val fpTag = pref?.getString("GET_FP_DETAILS_TAG", null)
             fpTag?.let { DomainRequest(Constants.clientid, it) }
-                ?.let { viewModel?.getSuggestedDomains(it) }
+                ?.let { viewModel.getSuggestedDomains(it) }
         } catch (e: Exception) {
             SentryController.captureException(e)
         }
@@ -413,7 +296,7 @@ class FeatureDetailsPopup : DialogFragment() {
             )
         }
 
-        viewModel?.updateCustomDomainsResultResult()?.observe(this) {
+        viewModel.updateCustomDomainsResultResult().observe(this) {
             for (singleDomain in it.domains) {
                 if (singleDomain.isAvailable) {
                     domainName = singleDomain.name
@@ -424,7 +307,7 @@ class FeatureDetailsPopup : DialogFragment() {
 
         }
 
-        viewModel?.updatesLoader?.observe(this)  {
+        viewModel.updatesLoader.observe(this)  {
             if (it) {
                 mainll.visibility=View.GONE
                 shimmer_anim.visibility=View.VISIBLE
@@ -435,98 +318,98 @@ class FeatureDetailsPopup : DialogFragment() {
             }
         }
 
-        viewModel?.getCallTrackingDetails()?.observe(this) {
+        viewModel.getCallTrackingDetails().observe(this) {
             if (it != null) {
 
                 System.out.println("numberList" + it)
                 val selectedNum = it[0]
                 tv_empty_select_number.text = selectedNum
-//                val dialogCard = SelectNumberBottomSheet()
-//                val bundle = Bundle()
+    //                val dialogCard = SelectNumberBottomSheet()
+    //                val bundle = Bundle()
 
-//                val content = SpannableString("Claim the above number\n@ ${numberprice}")
-//                content.setSpan(
-//                    StyleSpan(Typeface.BOLD),
-//                    0,
-//                    22,
-//                    0
-//                )
-//                claim_button.setText(content)
-//                claim_button.setOnClickListener {
-//                    if (selectedNum != null) {
-//                        if (!itemInCartStatus) {
-//                            if (addonDetails != null) {
-//                                prefs.storeCartOrderInfo(null)
-//                                viewModel!!.addItemToCart1(addonDetails!!, this, selectedNum)
-//                                val event_attributes: HashMap<String, Any> = HashMap()
-//                                addonDetails!!.name?.let { it1 ->
-//                                    event_attributes.put(
-//                                        "Addon Name",
-//                                        it1
-//                                    )
-//                                }
-//                                event_attributes.put("Addon Price", addonDetails!!.price)
-//                                event_attributes.put(
-//                                    "Addon Discounted Price",
-//                                    getDiscountedPrice(
-//                                        addonDetails!!.price,
-//                                        addonDetails!!.discount_percent
-//                                    )
-//                                )
-//                                event_attributes.put(
-//                                    "Addon Discount %",
-//                                    addonDetails!!.discount_percent
-//                                )
-//                                event_attributes.put("Addon Validity", 1)
-//                                event_attributes.put(
-//                                    "Addon Feature Key",
-//                                    addonDetails!!.boost_widget_key
-//                                )
-//                                addonDetails!!.target_business_usecase?.let { it1 ->
-//                                    event_attributes.put(
-//                                        "Addon Tag",
-//                                        it1
-//                                    )
-//                                }
-//                                WebEngageController.trackEvent(
-//                                    ADDONS_MARKETPLACE_FEATURE_ADDED_TO_CART,
-//                                    ADDONS_MARKETPLACE,
-//                                    event_attributes
-//                                )
-//                                itemInCartStatus = true
-//                            }
-//                        }
-//
-//                    } else {
-//                        Toasty.error(
-//                            this,
-//                            "Number not available, please select other",
-//                            Toast.LENGTH_SHORT
-//                        ).show()
-//                    }
-//                    val intent = Intent(this, CartActivity::class.java)
-//                    intent.putExtra("fpid", fpid)
-//                    intent.putExtra("expCode", experienceCode)
-//                    intent.putExtra("isDeepLink", isDeepLink)
-//                    intent.putExtra("deepLinkViewType", deepLinkViewType)
-//                    intent.putExtra("deepLinkDay", deepLinkDay)
-//                    intent.putExtra("isOpenCardFragment", isOpenCardFragment)
-//                    intent.putExtra("accountType", accountType)
-//                    intent.putStringArrayListExtra("userPurchsedWidgets", userPurchsedWidgets)
-//                    if (email != null) {
-//                        intent.putExtra("email", email)
-//                    } else {
-//                        intent.putExtra("email", "ria@nowfloats.com")
-//                    }
-//                    if (mobileNo != null) {
-//                        intent.putExtra("mobileNo", mobileNo)
-//                    } else {
-//                        intent.putExtra("mobileNo", "9160004303")
-//                    }
-//                    intent.putExtra("profileUrl", profileUrl)
-//                    startActivity(intent)
-//                }
-//                choose_different_value.setText("Pick another number")
+    //                val content = SpannableString("Claim the above number\n@ ${numberprice}")
+    //                content.setSpan(
+    //                    StyleSpan(Typeface.BOLD),
+    //                    0,
+    //                    22,
+    //                    0
+    //                )
+    //                claim_button.setText(content)
+    //                claim_button.setOnClickListener {
+    //                    if (selectedNum != null) {
+    //                        if (!itemInCartStatus) {
+    //                            if (addonDetails != null) {
+    //                                prefs.storeCartOrderInfo(null)
+    //                                viewModel!!.addItemToCart1(addonDetails!!, this, selectedNum)
+    //                                val event_attributes: HashMap<String, Any> = HashMap()
+    //                                addonDetails!!.name?.let { it1 ->
+    //                                    event_attributes.put(
+    //                                        "Addon Name",
+    //                                        it1
+    //                                    )
+    //                                }
+    //                                event_attributes.put("Addon Price", addonDetails!!.price)
+    //                                event_attributes.put(
+    //                                    "Addon Discounted Price",
+    //                                    getDiscountedPrice(
+    //                                        addonDetails!!.price,
+    //                                        addonDetails!!.discount_percent
+    //                                    )
+    //                                )
+    //                                event_attributes.put(
+    //                                    "Addon Discount %",
+    //                                    addonDetails!!.discount_percent
+    //                                )
+    //                                event_attributes.put("Addon Validity", 1)
+    //                                event_attributes.put(
+    //                                    "Addon Feature Key",
+    //                                    addonDetails!!.boost_widget_key
+    //                                )
+    //                                addonDetails!!.target_business_usecase?.let { it1 ->
+    //                                    event_attributes.put(
+    //                                        "Addon Tag",
+    //                                        it1
+    //                                    )
+    //                                }
+    //                                WebEngageController.trackEvent(
+    //                                    ADDONS_MARKETPLACE_FEATURE_ADDED_TO_CART,
+    //                                    ADDONS_MARKETPLACE,
+    //                                    event_attributes
+    //                                )
+    //                                itemInCartStatus = true
+    //                            }
+    //                        }
+    //
+    //                    } else {
+    //                        Toasty.error(
+    //                            this,
+    //                            "Number not available, please select other",
+    //                            Toast.LENGTH_SHORT
+    //                        ).show()
+    //                    }
+    //                    val intent = Intent(this, CartActivity::class.java)
+    //                    intent.putExtra("fpid", fpid)
+    //                    intent.putExtra("expCode", experienceCode)
+    //                    intent.putExtra("isDeepLink", isDeepLink)
+    //                    intent.putExtra("deepLinkViewType", deepLinkViewType)
+    //                    intent.putExtra("deepLinkDay", deepLinkDay)
+    //                    intent.putExtra("isOpenCardFragment", isOpenCardFragment)
+    //                    intent.putExtra("accountType", accountType)
+    //                    intent.putStringArrayListExtra("userPurchsedWidgets", userPurchsedWidgets)
+    //                    if (email != null) {
+    //                        intent.putExtra("email", email)
+    //                    } else {
+    //                        intent.putExtra("email", "ria@nowfloats.com")
+    //                    }
+    //                    if (mobileNo != null) {
+    //                        intent.putExtra("mobileNo", mobileNo)
+    //                    } else {
+    //                        intent.putExtra("mobileNo", "9160004303")
+    //                    }
+    //                    intent.putExtra("profileUrl", profileUrl)
+    //                    startActivity(intent)
+    //                }
+    //                choose_different_value.setText("Pick another number")
             }
         }
     }
