8ec9669b75c8ae964cc45bac77348554dd01f5cf
faraz <faraz.m@mantralabsglobal.comfaraz-mantra>
2023-02-13 14:54:00 +0530

    Bug fixed-49805

--- festive_poster/src/main/java/com/festive/poster/ui/promoUpdates/intro/UpdateStudioIntroActivity.kt
+++ festive_poster/src/main/java/com/festive/poster/ui/promoUpdates/intro/UpdateStudioIntroActivity.kt
@@ -75,7 +75,7 @@ class UpdateStudioIntroActivity : AppBaseActivity<ActivityUpdateStudioIntroBindi
       ),
 
       IntroUpdateStudioItem(
-        getString(R.string.post_free_updates_using_create_tab), R.drawable.update_studio_intro3_v2, getString(R.string.you_can_continue_to_post_good_old_updates_using_the_create_tab_on_top)
+        getString(R.string.post_free_updates_using_create_tab), R.drawable.update_into3, getString(R.string.you_can_continue_to_post_good_old_updates_using_the_create_tab_on_top)
       ),
     )
 
